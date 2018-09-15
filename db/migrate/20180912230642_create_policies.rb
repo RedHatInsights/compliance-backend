8ba200105860c27bba2ec1b82b15441de3531d7c
Daniel Lobato Garcia <me@daniellobato.me>
2018-09-15 16:26:44 +0200

    Change migrations to use UUID

--- db/migrate/20180912230642_create_policies.rb
+++ db/migrate/20180912230642_create_policies.rb
@@ -3,7 +3,7 @@
 # Create policies table, meant to store policy attributes from OpenSCAP
 class CreatePolicies < ActiveRecord::Migration[5.2]
   def change
-    create_table :policies do |t|
+    create_table :policies, id: :uuid do |t|
       t.string :name
 
       t.timestamps
--- db/migrate/20180912230653_create_profiles.rb
+++ db/migrate/20180912230653_create_profiles.rb
@@ -3,7 +3,7 @@
 # Create profiles table, meant to store profile attributes from OpenSCAP
 class CreateProfiles < ActiveRecord::Migration[5.2]
   def change
-    create_table :profiles do |t|
+    create_table :profiles, id: :uuid do |t|
       t.string :name
       t.references :policy, type: :uuid, index: true
 
