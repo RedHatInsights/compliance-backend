230038c5c7c6ebc84ddd6c6373f4bbb679b80720
Daniel Lobato Garcia <me@daniellobato.me>
2018-09-21 23:41:50 +0200

    Create test report and get the score

--- app/services/xccdf_report_parser.rb
+++ app/services/xccdf_report_parser.rb
@@ -15,7 +15,39 @@ class XCCDFReportParser
     @benchmark = ::OpenSCAP::Xccdf::Benchmark.new(@source)
   end
 
+  def test_result
+    return @test_result if @test_result.present?
+
+    xccdf_report = File.open(@report_path) { |f| Nokogiri::XML(f) }
+    source = ::OpenSCAP::Source.new(
+      content: create_test_result(xccdf_report).to_xml
+    )
+    begin
+      @test_result = ::OpenSCAP::Xccdf::TestResult.new(source)
+    rescue ::OpenSCAP::OpenSCAPError => e
+      Rails.logger.error('Error: ', e)
+    end
+  end
+
+  def score
+    test_result.score['urn:xccdf:scoring:default'][:value]
+  end
+
   def profiles
     @benchmark.profiles.map { |id, oscap_profile| [id, oscap_profile.title] }
+  end
+
+  private
+
+  def find_namespace(report_xml)
+    report_xml.namespaces['xmlns']
+  end
+
+  def create_test_result(report_xml)
+    test_result_node = report_xml.search('TestResult')
+    test_result_doc = Nokogiri::XML::Document.parse(test_result_node.to_xml)
+    test_result_doc.root.default_namespace = find_namespace(report_xml)
+    test_result_doc.namespace = test_result_doc.root.namespace
+    test_result_doc
   end
 end
--- test/services/xccdf_report_parser_test.rb
+++ test/services/xccdf_report_parser_test.rb
@@ -3,12 +3,20 @@
 require 'test_helper'
 
 class XCCDFReportParserTest < ActiveSupport::TestCase
-  test 'policy can be parsed' do
+  test 'profile can be parsed' do
     fake_report = file_fixture('xccdf_report.xml').to_path
     assert_equal(
       [['xccdf_org.ssgproject.content_profile_standard',
         'Standard System Security Profile for Fedora']],
       ::XCCDFReportParser.new(fake_report).profiles
+    )
+  end
+
+  test 'score can be parsed' do
+    fake_report = file_fixture('xccdf_report.xml').to_path
+    assert_equal(
+      16.220237731933594,
+      ::XCCDFReportParser.new(fake_report).score
     )
   end
 end
