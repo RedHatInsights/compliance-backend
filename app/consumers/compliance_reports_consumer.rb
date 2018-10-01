0fe91be1010c8d5ae491d79dfe7a5190d1dc3207
Daniel Lobato Garcia <me@daniellobato.me>
2018-10-01 18:54:42 +0200

    Listen to 'compliance' topic and parse report

--- app/consumers/compliance_reports_consumer.rb
+++ app/consumers/compliance_reports_consumer.rb
@@ -1,7 +1,14 @@
 class ComplianceReportsConsumer < Racecar::Consumer
-  subscribes_to 'testareno'
+  subscribes_to 'compliance'
 
   def process(message)
+    # Handle URL differently if it looks like HTTPS URL vs filepath
+    parser = XCCDFReportParser.new(message.value['url'])
+    parser.save_host
+    parser.save_profiles
+    parser.save_rule_results
     puts "Received message: #{message.value}"
+  rescue OpenSCAP::OpenSCAPError => e
+    puts "Failed to process message in #{message.topic}/#{message.partition} at offset #{message.offset}: #{e}"
   end
 end
