fcc71f3c66a7c91aa293b92e8d48aeffe55df7ae
Daniel Lobato Garcia <me@daniellobato.me>
2018-11-27 13:45:07 +0100

    Profile details API

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,12 @@
 # API for Profiles
 class ProfilesController < ApplicationController
   def index
-    render json: ProfileSerializer.new(Profile.includes(:rules, :hosts).all.to_a)
+    render json: ProfileSerializer.new(
+      Profile.includes(:rules, :hosts).all.to_a
+    )
+  end
+
+  def show
+    render json: ProfileSerializer.new(Profile.find(params[:id]))
   end
 end
