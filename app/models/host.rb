f96980decb9dc43ef1ac6284a598d71f01600760
Daniel Lobato Garcia <me@daniellobato.me>
2018-12-13 10:56:10 +0100

    Basic CSV exporter

--- app/models/host.rb
+++ app/models/host.rb
@@ -3,6 +3,7 @@
 # Host representation in insights compliance backend. Most of the times
 # these hosts will also show up in the insights-platform host inventory.
 class Host < ApplicationRecord
+  scoped_search on: %i[id]
   has_many :rule_results, dependent: :destroy
   has_many :rules, through: :rule_results, source: :rule
   has_many :profile_hosts, dependent: :destroy
