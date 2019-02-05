539d57b67255048a6f35de2d622d01940c3640be
Daniel Lobato García <me@daniellobato.me>
2019-02-05 10:42:59 +0100

    Accept SETTINGS__PROMETHEUS_EXPORTER_HOST as env var (#59)

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,11 +1,14 @@
 if Rails.env != "test"
   require 'prometheus_exporter/client'
+  require 'prometheus_exporter/metric'
   require 'prometheus_exporter/middleware'
   require 'prometheus_exporter/instrumentation'
   client = PrometheusExporter::Client.new(
     host: Settings.prometheus_exporter_host,
     port: Settings.prometheus_exporter_port
+    # Add custom_labels for app_name here (consumer and webserver)
   )
+  PrometheusExporter::Metric::Base.default_prefix = 'compliance'
   PrometheusExporter::Client.default = client
 
   # This reports stats per request like HTTP status and timings
