e333320cd01324e1d9c32af8a5fd29d9ed7378c5
Daniel Lobato García <me@daniellobato.me>
2019-04-15 10:10:08 +0200

    Substitute filecache by redis (#107)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_cache_store, { url: "redis://#{Settings.redis_url}/0" }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
