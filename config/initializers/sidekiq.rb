123c481b6758c80844aa8f3861eb4a869c9b25cc
Daniel Lobato Garcia <me@daniellobato.me>
2019-04-17 15:30:14 +0200

    Setup reliable fetch after setting up config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,19 +1,17 @@
-Settings.reload! # ensure settings are loaded according to these settings
-
 Sidekiq.configure_server do |config|
-  Sidekiq::ReliableFetch.setup_reliable_fetch!(config)
   config.redis = {
     url: "redis://#{Settings.redis_url}",
     network_timeout: 5 # Default is 1 second, let's be more lenient
   }
+  Sidekiq::ReliableFetch.setup_reliable_fetch!(config)
 end
 
 Sidekiq.configure_client do |config|
-  Sidekiq::ReliableFetch.setup_reliable_fetch!(config)
   config.redis = {
     url: "redis://#{Settings.redis_url}",
     network_timeout: 5
   }
+  Sidekiq::ReliableFetch.setup_reliable_fetch!(config)
 end
 
 Sidekiq.default_worker_options = { 'backtrace' => true, 'retry' => 3, 'unique' => true }
