b0a21bf919ba61e18acf964e78073ee0fc96c085
Andrew Kofink <akofink@redhat.com>
2019-06-03 16:36:40 -0400

    Fix and add tests for exception_notifier

--- app/controllers/concerns/exception_notifier_custom_data.rb
+++ app/controllers/concerns/exception_notifier_custom_data.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Module to add any info to ExceptionNotifier
-module ExceptionNotifier
+module ExceptionNotifierCustomData
   extend ActiveSupport::Concern
 
   included do
