85ca307c5ae439f047254af3fd143ca19a81d943
Sebastian Gräßl <bastilian@users.noreply.github.com>
2019-06-06 10:00:20 +0200

    RHICOMPL-199 - Cache failed rules results (#163)
    * RHICOMPL-199 - Cache failed rules results
    
    * Expire cached failed rule objects
    
    * Do not offend rubocop
    
    * Correct using @host
    
    * Address minor rubocop complaint

--- app/models/schema.rb
+++ app/models/schema.rb
@@ -127,12 +127,15 @@ SystemType = GraphQL::ObjectType.define do
     type types[RuleType]
     description 'Rules failed by a system'
     resolve lambda { |host, _args, _ctx|
-      Rule.where(
-        id: RuleResult.includes(:rule).where(
-          host: host,
-          result: %w[error fail notchecked]
-        ).pluck(:rule_id).uniq
-      )
+      Rails.cache.fetch("#{host.id}/failed_rule_objects_result",
+                        expires_in: 1.week) do
+        Rule.where(
+          id: RuleResult.includes(:rule).where(
+            host: host,
+            result: %w[error fail notchecked]
+          ).pluck(:rule_id).uniq
+        )
+      end
     }
   end
 
