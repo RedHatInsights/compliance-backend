4fba0d159b3fa6e40026023aa1b64ba0a9dfd4ea
Daniel Lobato García <me@daniellobato.me>
2019-07-15 14:21:03 +0200

    RHICOMPL-207 - Destroy business objectives when they are orphaned (#182)

--- app/models/business_objective.rb
+++ app/models/business_objective.rb
@@ -2,6 +2,6 @@
 
 # Business objectives are arbitrary strings to tag profiles in the UI
 class BusinessObjective < ApplicationRecord
-  has_many :profiles, dependent: :destroy
+  has_many :profiles, dependent: :nullify
   validates :title, presence: true
 end
