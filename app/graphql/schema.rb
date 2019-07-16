b01d9dca84cc15950024bd12c8744a729bc038ca
Andrew Kofink <akofink@redhat.com>
2019-07-16 13:12:43 -0400

    Disable prometheus exporter in test environment
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- app/graphql/schema.rb
+++ app/graphql/schema.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require 'prometheus_exporter/client'
+require 'prometheus_exporter/client' unless Rails.env.test?
 
 # Definition for the GraphQL schema - read the
 # GraphQL-ruby documentation to find out what to add or
 # remove here.
 class Schema < GraphQL::Schema
-  use GraphQL::Tracing::PrometheusTracing
+  use GraphQL::Tracing::PrometheusTracing unless Rails.env.test?
   query Types::Query
   mutation Types::Mutation
 end
