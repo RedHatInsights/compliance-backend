0293df8df31a6a80cf2e6fa1b3c6b71d5aa7676c
Sebastian Gräßl <mail@bastilian.me>
2019-07-24 13:05:35 +0200

    Remove waiting for redis from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,4 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
-while ! nc -z redis 6379;
-do
-  echo "Waiting for redis";
-  sleep 1;
-done;
-echo Ready!;
-
 exec "$@"
