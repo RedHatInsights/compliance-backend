9eb50b653aa31ee80754896f46fcd4851790ab45
Andrew Kofink <akofink@redhat.com>
2019-08-26 15:52:33 -0400

    Fixes 323: Include associations in graphql query
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 module Types
+  # Definition of the BaseObject type in GraphQL
   class BaseObject < GraphQL::Schema::Object
+    protected
+
+    def lookahead_includes(lookahead, object, selects_includes)
+      selects_includes.each do |selects, includes|
+        object = object.includes(includes) if lookahead.selects?(selects)
+      end
+
+      object
+    end
   end
 end
