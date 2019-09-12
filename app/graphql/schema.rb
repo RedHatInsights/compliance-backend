490af5ee6fd8024e310957d65821a641eb4af093
Andrew Kofink <akofink@redhat.com>
2019-09-12 04:46:42 -0400

    Enforce a 10 second timeout in graphql (#217)
    10 seconds is terribly slow - anything more than this is in danger of
    timing out at the gateway (30 seconds total response time). This graphql
    timeout is not the total response time, but just for graphql query
    execution.
    
    https://graphql-ruby.org/queries/timeout.html
    
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- app/graphql/schema.rb
+++ app/graphql/schema.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 require 'prometheus_exporter/client' unless Rails.env.test?
+require 'compliance_timeout'
 
 # Definition for the GraphQL schema - read the
 # GraphQL-ruby documentation to find out what to add or
 # remove here.
 class Schema < GraphQL::Schema
   use GraphQL::Tracing::PrometheusTracing unless Rails.env.test?
+  use ComplianceTimeout, max_seconds: 10
   query Types::Query
   mutation Types::Mutation
 end
