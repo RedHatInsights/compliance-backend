a9630ccef32d5be53e13b4e948b1f33c2081fe10
Daniel Lobato García <me@daniellobato.me>
2019-10-16 15:49:20 +0200

    RHICOMPL-364 - Migrate broken report ref IDs (#244)

---
+++ test/services/xccdf_report_migration_test.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class XCCDFReportMigrationTest < ActiveSupport::TestCase
+  setup do
+    @profile = Profile.create(
+      account: accounts(:test),
+      name: 'footitle',
+      ref_id: 'foorefid'
+    )
+    @rules = 10.times.map do |i|
+      rule = Rule.create(title: "bar#{i}", ref_id: "ruleref#{i}",
+                         description: 'baz',
+                         severity: 'low')
+      @profile.rules << rule
+      rule
+    end
+    @host = hosts(:one)
+    @rule_results = @rules.map do |rule|
+      RuleResult.create(rule: rule, host: @host, result: 'pass')
+    end
+  end
+
+  test 'migration should just rename rules/profiles if no conflict' do
+    original_profile_ref_id = @profile.ref_id
+    XCCDFReportMigration.new(accounts(:test), false).run
+    @profile.reload
+    @rules.map(&:reload)
+    @rule_results.map(&:reload)
+    assert_equal(
+      "xccdf_org.ssgproject.content_profile_#{original_profile_ref_id}",
+      @profile.ref_id
+    )
+    10.times do |i|
+      assert_equal(
+        "xccdf_org.ssgproject.content_rule_ruleref#{i}",
+        @rules[i].ref_id
+      )
+    end
+    @rule_results.each_with_index do |rule_result, i|
+      assert_equal @rules[i].id, rule_result.rule_id
+    end
+  end
+
+  test 'migration should reassign results if conflict' do
+    conflict_profile = Profile.create(
+      account: accounts(:test),
+      name: 'footitle',
+      ref_id: "xccdf_org.ssgproject.content_profile_#{@profile.ref_id}"
+    )
+    conflict_rules = 10.times.map do |i|
+      rule = Rule.create(
+        title: "bar#{i}",
+        ref_id: "xccdf_org.ssgproject.content_rule_ruleref#{i}",
+        description: 'baz',
+        severity: 'low'
+      )
+      conflict_profile.rules << rule
+      rule
+    end
+    original_profile_ref_id = @profile.ref_id
+    XCCDFReportMigration.new(accounts(:test), false).run
+    assert Profile.where(ref_id: original_profile_ref_id).empty?
+    @rules.each do |rule|
+      assert Rule.where(ref_id: rule.ref_id).empty?
+    end
+    @rule_results.map(&:reload).each_with_index do |rule_result, i|
+      assert_equal conflict_rules[i].id, rule_result.rule_id
+    end
+  end
+end
