c1a5f61cb38f128f83d1a987b44ac2743b1ec88a
Andrew Kofink <akofink@redhat.com>
2019-10-24 09:53:56 -0400

    Add a phony default benchmark to profiles + rules
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- db/migrate/20190924173755_delete_policies.rb
+++ db/migrate/20190924173755_delete_policies.rb
@@ -1,5 +1,14 @@
 class DeletePolicies < ActiveRecord::Migration[5.2]
-  def change
+  def up
     drop_table :policies
+  end
+
+  def down
+    create_table :policies, id: :uuid do |t|
+      t.string :name
+
+      t.timestamps
+    end
+    add_index :policies, :name, unique: true
   end
 end
