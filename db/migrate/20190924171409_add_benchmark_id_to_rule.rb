c89c6da5c18e78ad121be0b191382f79f308426d
Andrew Kofink <akofink@redhat.com>
2019-10-24 12:00:44 -0400

    Add a phony default benchmark to profiles + rules (#255)
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- db/migrate/20190924171409_add_benchmark_id_to_rule.rb
+++ db/migrate/20190924171409_add_benchmark_id_to_rule.rb
@@ -1,5 +1,13 @@
 class AddBenchmarkIdToRule < ActiveRecord::Migration[5.2]
+  PHONY_BENCHMARK = Xccdf::Benchmark.find_or_create_by!(
+    ref_id: 'phony_ref_id',
+    version: 'v0.0.0',
+    title: 'phony title',
+    description: 'phony description'
+  )
+
   def change
-    add_column :rules, :benchmark_id, :uuid, null: false
+    add_column :rules, :benchmark_id, :uuid, null: false,
+      default: PHONY_BENCHMARK.id
   end
 end
