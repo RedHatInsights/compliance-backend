d100a951725eed19d66701d56409635002c7cf06
Daniel Lobato Garcia <me@daniellobato.me>
2019-12-03 18:38:06 +0100

    RHICOMPL-419 - Rules should be able to be queried by references

---
+++ app/graphql/record_loader.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# Class to batch record loading
+class RecordLoader < GraphQL::Batch::Loader
+  def initialize(model, column: model.primary_key, where: nil, order: nil,
+                 includes: nil)
+    @model = model
+    @column = column.to_s
+    @column_type = model.type_for_attribute(@column)
+    @where = where
+    @order = order
+    @includes = includes
+  end
+
+  def load(key)
+    super(@column_type.cast(key))
+  end
+
+  def perform(keys)
+    query(keys).each { |record| fulfill(record.public_send(@column), record) }
+    keys.each { |key| fulfill(key, nil) unless fulfilled?(key) }
+  end
+
+  private
+
+  def query(keys)
+    scope = @model
+    scope = scope.where(@where) if @where.present?
+    scope = scope.order(@order) if @order.present?
+    scope = scope.includes(@includes) if @includes.present?
+    scope.where(@column => keys)
+  end
+end
--- app/graphql/types/profile.rb
+++ app/graphql/types/profile.rb
@@ -22,29 +22,14 @@ module Types
                'Rule references to filter by', required: false
     end
 
-    # rubocop:disable AbcSize
-    def rules(args = {})
-      selected_columns = (args[:lookahead].selections.map(&:name) &
-                         ::Rule.column_names.map(&:to_sym)) << :id
-
-      rules = rules_for_system(args, selected_columns) if system_id(args)
-      rules = object.rules.select(selected_columns) if rules.blank?
-      rules = rules.with_identifier(args[:identifier]) if args.dig(:identifier)
-      rules = rules.with_references(args[:references]) if args.dig(:references)
-      rules = lookahead_includes(args[:lookahead], rules,
-                                 identifier: :rule_identifier)
-      parent_profile(rules)
-      rules
-    end
-    # rubocop:enable AbcSize
-
     field :hosts, [::Types::System], null: true
     field :business_objective, ::Types::BusinessObjective, null: true
     field :business_objective_id, ID, null: true
     field :total_host_count, Int, null: false
 
     field :compliant, Boolean, null: false do
-      argument :system_id, String, 'Is a system compliant?', required: false
+      argument :system_id, String, 'Is a system compliant with this profile?',
+               required: false
     end
 
     field :rules_passed, Int, null: false do
@@ -65,38 +50,79 @@ module Types
 
     field :compliant_host_count, Int, null: false
 
+    def rules(args = {})
+      context[:parent_profile_id] ||= {}
+      context[:rule_results] ||= {} if args[:lookahead].selects?(:compliant)
+      if system_id(args)
+        latest_test_result_batch(args).then do |latest_test_result|
+          ::CollectionLoader.for(::TestResult, :rule_results).load(latest_test_result).then do |rule_results|
+            if args[:lookahead].selects?(:references)
+              grouped_rules_references = ::RuleReferencesRule.where(rule_id: rule_results.pluck(:rule_id)).group_by(&:rule_id)
+              grouped_rules_references.each do |rule_id, references|
+                context[:"rule_references_#{rule_id}"] = references.pluck(:rule_reference_id)
+              end
+            end
+            if rule_results.blank?
+              []
+            else
+              ::RecordLoader.for(::Rule).load_many(rule_results.pluck(:rule_id)).then do |rules|
+                rules.each do |rule|
+                  context[:parent_profile_id][rule.id] = object.id
+                end
+                rule_results.each do |rule_result|
+                  context[:rule_results][rule_result.rule_id] ||= {}
+                  context[:rule_results][rule_result.rule_id][object.id] = rule_result.result
+                end if args[:lookahead].selects?(:compliant)
+                rules
+              end
+            end
+          end
+        end
+      else
+        ::CollectionLoader.for(::Profile, :rules).load(object).then do |rules|
+          rules
+        end
+      end
+    end
+
     def compliant_host_count
-      object.hosts.count { |host| object.compliant?(host) }
+      CollectionLoader.for(object.class, :hosts).load(object).then do |hosts|
+        hosts.count { |host| object.compliant?(host) }
+      end
     end
 
     def total_host_count
-      object.hosts.count
+      CollectionLoader.for(object.class, :hosts).load(object).then(&:count)
     end
 
     def compliant(args = {})
-      host_results = latest_test_result(args)&.rule_results
-      host_results.present? &&
-        (
-          host_results.where(result: 'pass').count /
-          host_results.count.to_f
-        ) >=
-          (object.compliance_threshold / 100.0)
+      latest_test_result_batch(args).then do |latest_test_result|
+        host_results = latest_test_result&.rule_results
+        host_results.present? &&
+          (
+            host_results.where(result: 'pass').count /
+            host_results.count.to_f
+          ) >=
+            (object.compliance_threshold / 100.0)
+      end
     end
 
     def rules_passed(args = {})
-      return 0 if (@latest_test_result = latest_test_result(args)).blank?
-
-      @latest_test_result.rule_results.passed.count
+      latest_test_result_batch(args).then do |latest_test_result|
+        latest_test_result.rule_results.passed.count
+      end
     end
 
     def rules_failed(args = {})
-      return 0 if (@latest_test_result = latest_test_result(args)).blank?
-
-      @latest_test_result.rule_results.failed.count
+      latest_test_result_batch(args).then do |latest_test_result|
+        latest_test_result.rule_results.failed.count
+      end
     end
 
     def last_scanned(args = {})
-      latest_test_result(args)&.end_time&.iso8601 || 'Never'
+      latest_test_result_batch(args).then do |latest_test_result|
+        latest_test_result.end_time&.iso8601 || 'Never'
+      end
     end
 
     private
@@ -105,18 +131,14 @@ module Types
       args[:system_id] || context[:parent_system_id]
     end
 
-    def parent_profile(rules)
-      context[:parent_profile_id] ||= {}
-      rules.each { |rule| context[:parent_profile_id][rule.id] = object.id }
-    end
-
-    def latest_test_result(args)
-      TestResult.latest(object.id, system_id(args))
-    end
-
-    def rules_for_system(args, selected_columns)
-      host = Host.find(system_id(args)) if system_id(args).present?
-      object.rules_for_system(host, selected_columns) if host.present?
+    def latest_test_result_batch(args)
+      ::RecordLoader.for(
+        ::TestResult,
+        column: :profile_id,
+        where: { host_id: system_id(args) },
+        order: 'created_at DESC',
+        includes: [:rule_results]
+      ).load(object.id)
     end
   end
 end
