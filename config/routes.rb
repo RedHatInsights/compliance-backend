4cebdc28d762ee27ce3499630d75bc0c757c41cd
Andrew Kofink <akofink@redhat.com>
2019-12-16 14:44:51 -0500

    Mount compliance app at both prefixes (#278)
    Mounted at both /api and /r/insights/platform
    
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,26 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  scope "#{ENV['PATH_PREFIX']}/#{ENV['APP_NAME']}" do
-    resources :profiles, only: [:index, :show]
-    resources :rule_results, only: [:index]
-    resources :systems, only: [:index, :destroy]
-    resources :rules, only: [:index, :show]
-    mount Rswag::Api::Engine => '/'
-    mount Rswag::Ui::Engine => '/'
-    get 'openapi' => 'application#openapi'
-    post 'graphql' => 'graphql#query'
-    if Rails.env.development?
-      mount GraphiQL::Rails::Engine, at: "/graphiql",
-        graphql_path: "#{ENV['PATH_PREFIX']}/#{ENV['APP_NAME']}/graphql"
+  def draw_routes(prefix)
+    scope "#{prefix}/#{ENV['APP_NAME']}" do
+      resources :profiles, only: [:index, :show]
+      resources :rule_results, only: [:index]
+      resources :systems, only: [:index, :destroy]
+      resources :rules, only: [:index, :show]
+      mount Rswag::Api::Engine => '/',
+        as: "#{prefix}/#{ENV['APP_NAME']}/rswag_api"
+      mount Rswag::Ui::Engine => '/',
+        as: "#{prefix}/#{ENV['APP_NAME']}/rswag_ui"
+      get 'openapi' => 'application#openapi'
+      post 'graphql' => 'graphql#query'
+      if Rails.env.development?
+        mount GraphiQL::Rails::Engine, at: "/graphiql",
+          graphql_path: "#{prefix}/#{ENV['APP_NAME']}/graphql",
+          as: "#{prefix}/#{ENV['APP_NAME']}/graphiql"
+      end
     end
   end
+
+  draw_routes(ENV['PATH_PREFIX'])
+  draw_routes(ENV['OLD_PATH_PREFIX'])
 end
