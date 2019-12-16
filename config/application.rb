11290c5ff07d3662ae5ed3eb3bbc8cc14fa1cfc7
Andrew Kofink <akofink@redhat.com>
2019-12-16 11:20:47 -0500

    Disable rails asset pipeline
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- config/application.rb
+++ config/application.rb
@@ -12,12 +12,7 @@ require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'action_cable/engine'
-# require "sprockets/railtie"
 require 'rails/test_unit/railtie'
-
-if Rails.env.development?
-  require 'sprockets/railtie'
-end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,8 +46,6 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  config.assets.prefix = "#{ENV['PATH_PREFIX']}/#{ENV['APP_NAME']}/assets"
-
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
