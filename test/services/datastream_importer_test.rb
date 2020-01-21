85d21c8834713fe71b90acd5177b08b93e690b39
Andrew Kofink <akofink@redhat.com>
2020-01-21 11:02:12 -0500

    Allow datastream import without any accounts
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- test/services/datastream_importer_test.rb
+++ test/services/datastream_importer_test.rb
@@ -14,4 +14,11 @@ class DatastreamImporterTest < ActiveSupport::TestCase
     ENV['JOBS_ACCOUNT_NUMBER'] ||= Account.first.account_number
     importer.import!
   end
+
+  test 'works without any accounts' do
+    importer = DatastreamImporter.new(DATASTREAM_FILE)
+    importer.expects(:save_all_benchmark_info)
+    ENV['JOBS_ACCOUNT_NUMBER'] ||= nil
+    importer.import!
+  end
 end
