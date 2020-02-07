d837592a69d773fce147fb9a34db33de8e489a9e
Daniel Lobato García <me@daniellobato.me>
2020-02-07 16:05:39 +0100

    RHICOMPL-446 - RBAC light implementation (#290) (#307)
    * RHICOMPL-446 - RBAC light implementation
    
    * Add webmock to Rspec

--- test/controllers/concerns/authentication_test.rb
+++ test/controllers/concerns/authentication_test.rb
@@ -11,182 +11,213 @@ class AuthenticationTest < ActionDispatch::IntegrationTest
     User.current = nil
   end
 
-  test 'rh-identity header not found' do
-    get profiles_url
-    assert_response :unauthorized
-  end
+  context 'unauthorized access' do
+    should 'rh-identity header not found' do
+      get profiles_url
+      assert_response :unauthorized
+    end
 
-  test 'error parsing rh-identity' do
-    get profiles_url, headers: { 'X-RH-IDENTITY': 'this should be a hash' }
-    assert_response :unauthorized
-  end
+    should 'error parsing rh-identity' do
+      get profiles_url, headers: { 'X-RH-IDENTITY': 'this should be a hash' }
+      assert_response :unauthorized
+    end
 
-  test 'account number not found, creates a new account' do
-    encoded_header = Base64.encode64(
-      {
-        'identity': {
-          'account_number': '1234',
-          'user': {
-            'username': 'testuser'
-          }
-        },
-        'entitlements':
+    should 'missing entitlement info completely' do
+      encoded_header = Base64.encode64(
         {
-          'smart_management': {
-            'is_entitled': true
+          'identity':
+          {
+            'account_number': '1234',
+            'type': 'User',
+            'user': {
+              'email': 'a@b.com',
+              'first_name': 'a',
+              'last_name': 'b',
+              'is_active': true,
+              'locale': 'en_US'
+            },
+            'internal': {
+              'org_id': '29329'
+            }
           }
-        }
-      }.to_json
-    )
-    get profiles_url, headers: { 'X-RH-IDENTITY': encoded_header }
-    assert_response :success
-    assert Account.find_by(account_number: '1234')
-    assert_equal(
-      User.find_by(username: 'testuser').account.account_number,
-      '1234'
-    )
-    assert_equal User.find_by(username: 'testuser'), User.current
-  end
+        }.to_json
+      )
+      get profiles_url, headers: { 'X-RH-IDENTITY': encoded_header }
+      assert_response :unauthorized
+      assert_not User.current
+    end
 
-  test 'user not found, creates a new user' do
-    encoded_header = Base64.encode64(
-      {
-        'identity':
+    should 'missing smart_management entitlement' do
+      encoded_header = Base64.encode64(
         {
-          'account_number': '1234',
-          'type': 'User',
-          'user': {
-            'email': 'a@b.com',
-            'username': 'a@b.com',
-            'first_name': 'a',
-            'last_name': 'b',
-            'is_active': true,
-            'locale': 'en_US'
+          'identity':
+          {
+            'account_number': '1234',
+            'type': 'User',
+            'user': {
+              'email': 'a@b.com',
+              'first_name': 'a',
+              'last_name': 'b',
+              'is_active': true,
+              'locale': 'en_US'
+            },
+            'internal': {
+              'org_id': '29329'
+            }
           },
-          'internal': {
-            'org_id': '29329'
+          'entitlements':
+          {
+            'smart_management': {
+              'is_entitled': false
+            }
           }
-        },
-        'entitlements':
-        {
-          'smart_management': {
-            'is_entitled': true
-          }
-        }
-      }.to_json
-    )
-    get profiles_url, headers: { 'X-RH-IDENTITY': encoded_header }
-    assert_response :success
-    assert_equal User.find_by(username: 'a@b.com'), User.current
-  end
+        }.to_json
+      )
+      get profiles_url, headers: { 'X-RH-IDENTITY': encoded_header }
+      assert_response :unauthorized
+      assert_not User.current
+    end
 
-  test 'user not found, creates a new account, username missing' do
-    encoded_header = Base64.encode64(
-      {
-        'identity':
+    should 'missing RBAC access for compliance' do
+      RbacApi.any_instance.expects(:check_user).returns(false)
+      encoded_header = Base64.encode64(
         {
-          'account_number': '1234',
-          'type': 'User',
-          'user': {
-            'email': 'a@b.com',
-            'first_name': 'a',
-            'last_name': 'b',
-            'is_active': true,
-            'locale': 'en_US'
+          'identity': {
+            'account_number': '1234',
+            'user': {
+              'username': 'shoulduser'
+            }
           },
-          'internal': {
-            'org_id': '29329'
+          'entitlements':
+          {
+            'smart_management': {
+              'is_entitled': true
+            }
           }
-        },
-        'entitlements':
-        {
-          'smart_management': {
-            'is_entitled': true
-          }
-        }
-      }.to_json
-    )
-    get profiles_url, headers: { 'X-RH-IDENTITY': encoded_header }
-    assert_response :unauthorized
-    assert_not User.current
+        }.to_json
+      )
+      get profiles_url, headers: { 'X-RH-IDENTITY': encoded_header }
+      assert_response :unauthorized
+      assert_not User.current
+    end
   end
 
-  test 'missing smart_management entitlement' do
-    encoded_header = Base64.encode64(
-      {
-        'identity':
+  context 'successful login' do
+    setup do
+      RbacApi.any_instance.expects(:check_user).returns(true)
+    end
+
+    should 'account number not found, creates a new account' do
+      encoded_header = Base64.encode64(
         {
-          'account_number': '1234',
-          'type': 'User',
-          'user': {
-            'email': 'a@b.com',
-            'first_name': 'a',
-            'last_name': 'b',
-            'is_active': true,
-            'locale': 'en_US'
+          'identity': {
+            'account_number': '1234',
+            'user': {
+              'username': 'shoulduser'
+            }
           },
-          'internal': {
-            'org_id': '29329'
+          'entitlements':
+          {
+            'smart_management': {
+              'is_entitled': true
+            }
           }
-        },
-        'entitlements':
-        {
-          'smart_management': {
-            'is_entitled': false
-          }
-        }
-      }.to_json
-    )
-    get profiles_url, headers: { 'X-RH-IDENTITY': encoded_header }
-    assert_response :unauthorized
-    assert_not User.current
-  end
+        }.to_json
+      )
+      get profiles_url, headers: { 'X-RH-IDENTITY': encoded_header }
+      assert_response :success
+      assert Account.find_by(account_number: '1234')
+      assert_equal(
+        User.find_by(username: 'shoulduser').account.account_number,
+        '1234'
+      )
+      assert_equal User.find_by(username: 'shoulduser'), User.current
+    end
 
-  test 'missing entitlement info completely' do
-    encoded_header = Base64.encode64(
-      {
-        'identity':
+    should 'user not found, creates a new user' do
+      encoded_header = Base64.encode64(
         {
-          'account_number': '1234',
-          'type': 'User',
-          'user': {
-            'email': 'a@b.com',
-            'first_name': 'a',
-            'last_name': 'b',
-            'is_active': true,
-            'locale': 'en_US'
+          'identity':
+          {
+            'account_number': '1234',
+            'type': 'User',
+            'user': {
+              'email': 'a@b.com',
+              'username': 'a@b.com',
+              'first_name': 'a',
+              'last_name': 'b',
+              'is_active': true,
+              'locale': 'en_US'
+            },
+            'internal': {
+              'org_id': '29329'
+            }
           },
-          'internal': {
-            'org_id': '29329'
+          'entitlements':
+          {
+            'smart_management': {
+              'is_entitled': true
+            }
           }
-        }
-      }.to_json
-    )
-    get profiles_url, headers: { 'X-RH-IDENTITY': encoded_header }
-    assert_response :unauthorized
-    assert_not User.current
-  end
+        }.to_json
+      )
+      get profiles_url, headers: { 'X-RH-IDENTITY': encoded_header }
+      assert_response :success
+      assert_equal User.find_by(username: 'a@b.com'), User.current
+    end
 
-  test 'successful authentication sets User.current' do
-    encoded_header = Base64.encode64(
-      {
-        'identity':
+    should 'user not found, creates a new account, username missing' do
+      encoded_header = Base64.encode64(
         {
-          'account_number': '1234',
-          'user': {
-            'username': 'fakeuser'
+          'identity':
+          {
+            'account_number': '1234',
+            'type': 'User',
+            'user': {
+              'email': 'a@b.com',
+              'first_name': 'a',
+              'last_name': 'b',
+              'is_active': true,
+              'locale': 'en_US'
+            },
+            'internal': {
+              'org_id': '29329'
+            }
+          },
+          'entitlements':
+          {
+            'smart_management': {
+              'is_entitled': true
+            }
           }
-        },
-        'entitlements':
+        }.to_json
+      )
+      get profiles_url, headers: { 'X-RH-IDENTITY': encoded_header }
+      assert_response :unauthorized
+      assert_not User.current
+    end
+
+    should 'successful authentication sets User.current' do
+      encoded_header = Base64.encode64(
         {
-          'smart_management': {
-            'is_entitled': true
+          'identity':
+          {
+            'account_number': '1234',
+            'user': {
+              'username': 'fakeuser'
+            }
+          },
+          'entitlements':
+          {
+            'smart_management': {
+              'is_entitled': true
+            }
           }
-        }
-      }.to_json
-    )
-    get profiles_url, headers: { 'X-RH-IDENTITY': encoded_header }
-    assert_response :success
-    assert_equal User.find_by(username: 'fakeuser'), User.current
+        }.to_json
+      )
+      get profiles_url, headers: { 'X-RH-IDENTITY': encoded_header }
+      assert_response :success
+      assert_equal User.find_by(username: 'fakeuser'), User.current
+    end
   end
 end
