221869826e1c62925978620cfee7bfffe75b6530
Andrew Kofink <akofink@redhat.com>
2020-02-26 12:33:56 -0500

    Create host if not exists on host-profile association (#330)
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- app/graphql/mutations/system/associate_profiles.rb
+++ app/graphql/mutations/system/associate_profiles.rb
@@ -11,26 +11,14 @@ module Mutations
       field :system, ::Types::System, null: true
 
       def resolve(args = {})
-        host = find_host(args[:id])
+        host = find_hosts([args[:id]]).first
         profiles = find_profiles(args[:profile_ids])
         host.update(profiles: profiles)
         { system: host }
       end
 
-      private
-
-      def find_host(host_id)
-        ::Pundit.authorize(
-          context[:current_user],
-          ::Host.find(host_id),
-          :edit?
-        )
-      end
-
-      def find_profiles(profile_ids)
-        ::Pundit.policy_scope(context[:current_user], ::Profile)
-                .where(id: profile_ids)
-      end
+      include HostHelper
+      include ProfileHelper
     end
   end
 end
