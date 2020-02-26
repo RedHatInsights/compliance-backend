dfa024c8f61ad205f65d74e4f3d00f0a5c808029
Andrew Kofink <akofink@redhat.com>
2020-02-26 16:28:45 -0500

    Migrate profiles using account_id to parent_profile_id
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- db/migrate/20200120203124_add_parent_profile_id_to_profiles.rb
+++ db/migrate/20200120203124_add_parent_profile_id_to_profiles.rb
@@ -1,5 +1,11 @@
 class AddParentProfileIdToProfiles < ActiveRecord::Migration[5.2]
-  def change
+  def down
+    remove_reference :profiles, :parent_profile
+  end
+
+  def up
     add_reference :profiles, :parent_profile, type: :uuid, foreign_key: { to_table: :profiles }
+
+    ParentProfileAssociator.run!
   end
 end
