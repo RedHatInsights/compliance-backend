ff42f22d11a648d38e140cafb41b1029b8f010c8
Andrew Kofink <akofink@redhat.com>
2020-02-27 02:55:09 -0500

    Don't delete orphaned profiles (#333)
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- test/models/profile_host_test.rb
+++ test/models/profile_host_test.rb
@@ -8,18 +8,4 @@ class ProfileHostTest < ActiveSupport::TestCase
     @host = hosts(:one)
     @profile_host = ProfileHost.new(profile: @profile, host: @host)
   end
-
-  test '#delete_orphaned_profiles when some other hosts still exist (noop)' do
-    @profile.expects(:destroy).never
-    @profile.stubs(:hosts).returns([hosts(:two)])
-
-    @profile_host.delete_orphaned_profiles
-  end
-
-  test '#delete_orphaned_profiles when only this host exists' do
-    @profile.expects(:destroy)
-    @profile.stubs(:hosts).returns([@host])
-
-    @profile_host.delete_orphaned_profiles
-  end
 end
