94fb62bd3c3a3c9e12e528ae80545226f91395bd
Daniel Lobato Garcia <me@daniellobato.me>
2020-03-03 15:56:33 +0100

    Migration is no-op when rollbacking

--- db/migrate/20200303102215_change_phony_benchmark_version.rb
+++ db/migrate/20200303102215_change_phony_benchmark_version.rb
@@ -1,7 +1,10 @@
 class ChangePhonyBenchmarkVersion < ActiveRecord::Migration[5.2]
   # The `.latest` method of ::Xccdf::Benchmark is using Gem::Version, which
   # expects the version to have the same versioning syntax as a gemspec.
-  def change
-    ::Xccdf::Benchmark.find_by(ref_id: 'phony_ref_id').update(version: '0.0.0')
+  def up
+    ::Xccdf::Benchmark.find_by(ref_id: 'phony_ref_id').update!(version: '0.0.0')
+  end
+
+  def down
   end
 end
