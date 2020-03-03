ccd1d38f83ee7a17292eb4b0e7d5f43f3b936abf
Daniel Lobato García <me@daniellobato.me>
2020-03-03 21:56:39 +0100

    Find all rules by slug using LIKE (#340)
    * Find all rules by slug using LIKE
    
    * Return canonical rules
    
    * Update tests
    
    * Change version of phony benchmark
    
    * Canonical scope and rubocop fix
    
    * Rename scope to latest
    
    * Migration is no-op when rollbacking
    
    * Update db/migrate/20200303102215_change_phony_benchmark_version.rb
    
    Co-Authored-By: Andrew Kofink <ajkofink@gmail.com>
    
    * Update Gemfile.lock to update puma patch version
    
    * Fix syntax error with &
    
    Co-authored-by: Andrew Kofink <ajkofink@gmail.com>

--- test/controllers/rules_controller_test.rb
+++ test/controllers/rules_controller_test.rb
@@ -7,10 +7,10 @@ class RulesControllerTest < ActionDispatch::IntegrationTest
     RulesController.any_instance.stubs(:authenticate_user)
     User.current = users(:test)
     users(:test).update account: accounts(:test)
-    profiles(:one).hosts = [hosts(:one)]
-    profiles(:one).account = accounts(:test)
-    rules(:one).profiles << profiles(:one)
-    profiles(:one).save
+    profiles(:one).update(
+      account: accounts(:test),
+      rules: [rules(:one)]
+    )
   end
 
   test 'index lists all rules' do
@@ -23,13 +23,29 @@ class RulesControllerTest < ActionDispatch::IntegrationTest
 
   test 'finds a rule within the user scope' do
     get rule_url(rules(:one).ref_id)
+    assert_response :success
+  end
+
+  test 'finds a rule with similar slug within the user scope' do
+    rules(:two).update(slug: "#{rules(:two).ref_id}-#{SecureRandom.uuid}")
+    profiles(:one).update rules: [rules(:two)]
+    get rule_url(rules(:two).ref_id)
 
     assert_response :success
   end
 
-  test 'does not find a rule outside of the user scope' do
+  test 'finds a rule by ID' do
+    get rule_url(rules(:one).id)
+
+    assert_response :success
+  end
+
+  test 'finds rules not within the user scope but within latest' do
+    assert_includes(Rule.latest, rules(:two))
+    assert_not_includes(::Pundit.policy_scope(users(:test), ::Rule),
+                        rules(:two))
     get rule_url(rules(:two).ref_id)
 
-    assert_response :not_found
+    assert_response :success
   end
 end
