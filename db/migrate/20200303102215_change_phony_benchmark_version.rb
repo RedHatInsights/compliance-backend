8b161b37cf7c7b376e0653861befc8c0a777d407
Daniel Lobato García <me@daniellobato.me>
2020-03-03 21:03:01 +0100

    Update db/migrate/20200303102215_change_phony_benchmark_version.rb
    Co-Authored-By: Andrew Kofink <ajkofink@gmail.com>

--- db/migrate/20200303102215_change_phony_benchmark_version.rb
+++ db/migrate/20200303102215_change_phony_benchmark_version.rb
@@ -2,7 +2,7 @@ class ChangePhonyBenchmarkVersion < ActiveRecord::Migration[5.2]
   # The `.latest` method of ::Xccdf::Benchmark is using Gem::Version, which
   # expects the version to have the same versioning syntax as a gemspec.
   def up
-    ::Xccdf::Benchmark.find_by(ref_id: 'phony_ref_id').update!(version: '0.0.0')
+    ::Xccdf::Benchmark.find_by(ref_id: 'phony_ref_id').&update!(version: '0.0.0')
   end
 
   def down
