d05da59d7a2291af9589903e9eb96d7178a0ec13
Daniel Lobato Garcia <me@daniellobato.me>
2020-03-18 11:00:03 +0100

    Associate parent profiles outside of own benchmark
    Some profiles cannot be associated to parents within its own benchmark,
    such as profiles that come from the phony benchmark (which has no
    canonical profiles).

--- app/services/parent_profile_associator.rb
+++ app/services/parent_profile_associator.rb
@@ -14,18 +14,46 @@ class ParentProfileAssociator
     private
 
     def find_parent(profile)
-      parents = Profile.where(ref_id: profile.ref_id,
-                              benchmark_id: profile.benchmark_id,
-                              account_id: nil)
-      raise not_found(profile) unless parents.one?
+      logger.info("Finding parents for profile #{profile.id}")
+      parents = find_in_same_benchmark(profile)
+      parents = find_in_other_benchmarks(profile) if parents.blank?
 
-      parents.first
+      raise not_found(profile) unless parents.any?
+
+      parent = find_most_recent_parent(parents)
+      logger.info("Found parent #{parent.id} for profile #{profile.id}")
+      parent
+    end
+
+    def find_in_same_benchmark(profile)
+      Profile.where(ref_id: profile.ref_id,
+                    benchmark_id: profile.benchmark_id,
+                    account_id: nil)
+    end
+
+    # Profiles in the phony-benchmark will have to be assigned to
+    # a real benchmark if it can be found.
+    def find_in_other_benchmarks(profile)
+      Profile.where(name: profile.name,
+                    ref_id: profile.ref_id,
+                    account_id: nil)
+    end
+
+    def find_most_recent_parent(parents)
+      latest_benchmark = ::Xccdf::Benchmark.find_latest(
+        parents.map(&:benchmark)
+      )
+      parents.find { |p| p.benchmark == latest_benchmark }
     end
 
     def not_found(profile)
       ActiveRecord::RecordNotFound.new(
         "Failed to find parent for profile with ID #{profile.id}."
       )
+    end
+
+    def logger
+      Rails.logger
     end
   end
 end
