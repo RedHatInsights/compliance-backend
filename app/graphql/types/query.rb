3f9eb9d0511b18ab4360b55a394a8b6f2e00036a
Daniel Lobato Garcia <me@daniellobato.me>
2020-03-26 10:09:56 +0100

    Remove legacy GQL queries not used anymore

--- app/graphql/types/query.rb
+++ app/graphql/types/query.rb
@@ -23,30 +23,13 @@ module Types
     record_field :profile, Types::Profile
     record_field :test_result, Types::TestResult
 
-    field :all_systems, [Types::System], null: true do
-      description 'All systems visible by the user'
-      argument :search, String, 'Search query', required: false
-      argument :per_page, Int, 'Page', required: false
-      argument :page, Int, 'Per page', required: false
-      argument :profile_id, String, 'Profile Id', required: false
-    end
-
     field :system, Types::System, null: true do
       description 'Details for a system'
       argument :id, String, required: true
     end
 
-    field :all_image_streams, [Types::System], null: true do
-      description 'All image streams visible by the user'
-      argument :search, String, 'Search query', required: false
-    end
-
     field :all_profiles, [Types::Profile], null: true do
       description 'All profiles visible by the user'
-    end
-
-    field :all_benchmarks, [Types::Benchmark], null: true do
-      description 'All benchmarks visible by the user'
     end
 
     field :latest_benchmarks, [Types::Benchmark], null: true do
@@ -65,18 +48,8 @@ module Types
       description 'All business objectives visible by the user'
     end
 
-    def all_systems(args = {})
-      Pundit.policy_scope(context[:current_user], ::Host)
-            .search_for(args[:search])
-            .paginate(page: args[:page], per_page: args[:per_page])
-    end
-
     def system(id:)
       Pundit.authorize(context[:current_user], ::Host.find(id), :show?)
-    end
-
-    def all_image_streams
-      []
     end
 
     def all_profiles
@@ -93,10 +66,6 @@ module Types
 
     def business_objectives
       Pundit.policy_scope(context[:current_user], ::BusinessObjective)
-    end
-
-    def all_benchmarks
-      Pundit.policy_scope(context[:current_user], ::Xccdf::Benchmark)
     end
 
     def latest_benchmarks
