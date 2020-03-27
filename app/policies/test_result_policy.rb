90e67d72112e97a905062cac7239de0bad15267b
Daniel Lobato Garcia <me@daniellobato.me>
2020-03-27 10:08:23 +0100

    Add Hosts to TestResults policy

--- app/policies/test_result_policy.rb
+++ app/policies/test_result_policy.rb
@@ -3,20 +3,21 @@
 # Policies for accessing TestResults
 class TestResultPolicy < ApplicationPolicy
   def index?
-    Pundit.policy(user, record.profile)
+    Pundit.policy(user, record.profile) || Pundit.policy(user, record.host)
   end
 
   def show?
-    Pundit.policy(user, record.profile)
+    Pundit.policy(user, record.profile) || Pundit.policy(user, record.host)
   end
 
   # Only show TestResults belonging to profiles owned by the current user
   class Scope < ::ApplicationPolicy::Scope
     def resolve
-      available_profiles = HostPolicy::Scope.new(
-        user, ::Profile
-      ).resolve.pluck(:id)
-      scope.where(profile_id: available_profiles)
+      available_profiles = ProfilePolicy::Scope.new(user, ::Profile)
+      available_hosts = HostPolicy::Scope.new(user, ::Host)
+      scope.where(profile_id: available_profiles.resolve).or(
+        scope.where(host_id: available_hosts.resolve)
+      )
     end
   end
 end
