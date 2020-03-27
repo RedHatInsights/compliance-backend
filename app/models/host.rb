d507c7ab17f0f28582290e5d4a6f3cb2db19d746
Daniel Lobato García <me@daniellobato.me>
2020-03-27 13:32:25 +0100

    RHICOMPL-292 - Filter by hosts with test results (#365)
    * RHICOMPL-292 - Filter by hosts with test results
    
    * Test for scoped_search
    
    * Update to use SQL exclusively

--- app/models/host.rb
+++ app/models/host.rb
@@ -8,6 +8,8 @@ class Host < ApplicationRecord
                 only_explicit: true
   scoped_search on: :compliance_score, ext_method: 'filter_by_compliance_score',
                 only_explicit: true
+  scoped_search on: :has_test_results, ext_method: 'test_results?',
+                only_explicit: true, operators: ['=']
   scoped_search relation: :profile_hosts, on: :profile_id
   has_many :rule_results, dependent: :delete_all
   has_many :rules, through: :rule_results, source: :rule
@@ -25,10 +27,10 @@ class Host < ApplicationRecord
         host.compliant.values.all?(ActiveModel::Type::Boolean.new.cast(value))
       end
       ids = ids.pluck(:id).map { |id| "'#{id}'" }
+
       return { conditions: '1=0' } if ids.empty?
 
       operator = operator == '<>' ? 'NOT' : ''
-
       { conditions: "hosts.id #{operator} IN(#{ids.join(',')})" }
     end
 
@@ -40,6 +42,15 @@ class Host < ApplicationRecord
       return { conditions: '1=0' } if ids.empty?
 
       { conditions: "hosts.id IN(#{ids.join(',')})" }
+    end
+
+    def test_results?(_filter, _operator, value)
+      operator = ActiveModel::Type::Boolean.new.cast(value) ? '' : 'NOT'
+      host_ids = TestResult.select(:host_id).distinct.where.not(host_id: nil)
+      {
+        conditions: "hosts.id #{operator} "\
+                    "IN(#{host_ids.to_sql})"
+      }
     end
   end
 end
