cff1fa87d19e87f16c7089b68fb13c897255db9e
Daniel Lobato García <me@daniellobato.me>
2020-04-02 22:26:02 +0200

    Performance improvements to SeedTestResults migration (#376)
    * Performance improvements to SeedTestResults migration
    
    * Host and Profile tables are not called anymore, instead we use host_id
      and profile_id directly.
    
    * TestResult inserts are wrapped in a transaction to avoid:
    
    ```
    BEGIN
      insert into ...
    COMMIT
    ... repeat for as many TestResult objects as needed
    ```
    
    instead we do
    
    ```
    BEGIN
      insert into ...
      insert into ...
      insert into ...
      insert into ...
    COMMIT
    ```
    
      which is more performant and just as safe as everything is already
      wrapped by an upper level nested transaction as it's a migration.
    
    * Also I added a counter to keep track of how many HostProfile objects
      are left and measure the progress.
    
    * Renamed ID variables to host_id/profile_id

--- db/migrate/20191126181009_seed_test_results.rb
+++ db/migrate/20191126181009_seed_test_results.rb
@@ -1,33 +1,35 @@
 class SeedTestResults < ActiveRecord::Migration[5.2]
   def up
     migrated = 0
-    ProfileHost.find_in_batches(batch_size: 200) do |profile_host_group|
+    ProfileHost.find_in_batches(batch_size: 30) do |profile_host_group|
       test_results = []
       rule_results_set = []
       profile_host_group.each do |profile_host|
-        host = profile_host.host
-        profile = profile_host.profile
-        if host.nil? || profile.nil?
+        host_id = profile_host.host_id
+        profile_id = profile_host.profile_id
+        if host_id.nil? || profile_id.nil?
           profile_host.destroy
           next
         end
-        puts(" - Migrating results for host #{host.name}"\
-             " - profile #{profile.name}")
-        rule_results_by_end_time = scan_results(host, profile)
+        puts(" - Migrating results for host #{host_id}"\
+             " - profile #{profile_id} - #{ProfileHost.count - migrated}")
+        rule_results_by_end_time = scan_results(host_id, profile_id)
           .group_by(&:end_time)
         rule_results_by_end_time.each do |end_time, rule_results|
           rules_passed = rule_results.count { |rr| RuleResult::PASSED.include?(rr.result) }
           rules_failed = rule_results.count { |rr| RuleResult::FAIL.include?(rr.result) }
           rule_results_set << rule_results
           test_results << {
-            host_id: host.id,
-            profile_id: profile.id,
+            host_id: host_id,
+            profile_id: profile_id,
             start_time: rule_results[0].start_time || Time.now.utc,
             end_time: rule_results[0].end_time || Time.now.utc,
             score: compliance_score(rules_passed, rules_failed)
           }
         end
         migrated += 1
+      end
+      TestResult.transaction do
         imported_test_results = TestResult.import test_results
         imported_test_results.ids.zip(rule_results_set).each do |imported_test_result, rule_results|
           RuleResult.where(id: rule_results.pluck(:id)).update_all(test_result_id: imported_test_result)
@@ -41,7 +43,7 @@ class SeedTestResults < ActiveRecord::Migration[5.2]
     TestResult.delete_all
   end
 
-  def scan_results(host, profile)
+  def scan_results(host_id, profile_id)
     RuleResult.find_by_sql(
       [
         'SELECT rule_results.* FROM (
@@ -57,7 +59,7 @@ class SeedTestResults < ActiveRecord::Migration[5.2]
                ON rules.id = profile_rules.rule_id
                WHERE profile_rules.profile_id = ?)
           ) rule_results',
-          host.id, RuleResult::SELECTED, profile.id
+          host_id, RuleResult::SELECTED, profile_id
       ]
     )
   end
