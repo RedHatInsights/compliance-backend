9cafd8f6ca6877e1b2a479309cfa745b6724dfdf
Andrew Kofink <akofink@redhat.com>
2020-04-07 10:35:02 -0400

    Use SQL only for BO pundit policy
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- app/policies/business_objective_policy.rb
+++ app/policies/business_objective_policy.rb
@@ -3,20 +3,17 @@
 # Policies for accessing BusinessObjectives
 class BusinessObjectivePolicy < ApplicationPolicy
   def index?
-    record.profiles.pluck(:account_id).include? user.account_id
+    record.account_ids.include? user.account_id
   end
 
   def show?
-    record.profiles.pluck(:account_id).include? user.account_id
+    record.account_ids.include? user.account_id
   end
 
   # Only show hosts in our user account
   class Scope < ::ApplicationPolicy::Scope
     def resolve
-      ids = scope.all.select do |business_objective|
-        business_objective.profiles.pluck(:account_id).include? user.account_id
-      end
-      scope.where(id: ids)
+      scope.in_account(user.account_id)
     end
   end
 end
