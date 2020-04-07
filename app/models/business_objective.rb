3c13e7bb1027655cada6f78738412666ba21b2f8
Andrew Kofink <akofink@redhat.com>
2020-04-07 11:28:21 -0400

    Use SQL only for BO pundit policy (#383)
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- app/models/business_objective.rb
+++ app/models/business_objective.rb
@@ -3,5 +3,11 @@
 # Business objectives are arbitrary strings to tag profiles in the UI
 class BusinessObjective < ApplicationRecord
   has_many :profiles, dependent: :nullify
+  has_many :accounts, through: :profiles
+
   validates :title, presence: true
+
+  scope :in_account, lambda { |account_or_account_id|
+    joins(:accounts).where(accounts: { id: account_or_account_id })
+  }
 end
