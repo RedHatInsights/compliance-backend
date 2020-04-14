47fd65a169fdcbe59b73c231e20b07737ba101b9
Andrew Kofink <akofink@redhat.com>
2020-04-14 10:44:38 -0400

    Remove default benchmark from rules and profiles
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

---
+++ db/migrate/20200414144209_remove_default_benchmarks_from_rules.rb
@@ -0,0 +1,16 @@
+class RemoveDefaultBenchmarksFromRules < ActiveRecord::Migration[5.2]
+  def up
+    change_column_default :rules, :benchmark_id, nil
+  end
+
+  def down
+    phony_benchmark = Xccdf::Benchmark.find_or_create_by!(
+      ref_id: 'phony_ref_id',
+      version: '0.0.0',
+      title: 'phony title',
+      description: 'phony description'
+    )
+
+    change_column_default :rules, :benchmark_id, phony_benchmark.id
+  end
+end
