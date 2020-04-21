65318e7ae23f77e2dbf5e42abfda52386dcb500d
Daniel Lobato García <me@daniellobato.me>
2020-04-21 16:31:53 +0200

    Hardcode latest RHEL supported versions (#408)
    * Hardcode latest RHEL supported versions
    
    * Add cache for latest supported versions
    
    * Freeze constant
    
    * Return an AR relation
    
    * Properly search for match ref_id/version

--- app/models/xccdf/benchmark.rb
+++ app/models/xccdf/benchmark.rb
@@ -11,6 +11,12 @@ module Xccdf
     validates :ref_id, uniqueness: { scope: %i[version] }, presence: true
     validates :version, presence: true
 
+    LATEST_SUPPORTED_VERSIONS = {
+      'xccdf_org.ssgproject.content_benchmark_RHEL-6': '0.1.28',
+      'xccdf_org.ssgproject.content_benchmark_RHEL-7': '0.1.46',
+      'xccdf_org.ssgproject.content_benchmark_RHEL-8': '0.1.46'
+    }.freeze
+
     class << self
       def from_openscap_parser(op_benchmark)
         benchmark = find_or_initialize_by(
@@ -41,6 +47,16 @@ module Xccdf
       def find_latest(benchmarks)
         benchmarks.max_by do |benchmark|
           Gem::Version.new(benchmark.version)
+        end
+      end
+
+      def latest_supported
+        Rails.cache.fetch('latest_supported_benchmarks') do
+          LATEST_SUPPORTED_VERSIONS.inject(
+            where('1=0')
+          ) do |acc, (ref_id, version)|
+            acc.or(where(ref_id: ref_id, version: version))
+          end.compact
         end
       end
     end
