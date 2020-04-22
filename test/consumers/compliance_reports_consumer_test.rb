1c7dd88235fd9f43dc0e82f88d502a4997ab07d9
Richard Brantley <rbrantle@redhat.com>
2020-04-22 15:56:35 -0400

    Remove duplicate test

--- test/consumers/compliance_reports_consumer_test.rb
+++ test/consumers/compliance_reports_consumer_test.rb
@@ -35,31 +35,6 @@ class ComplianceReportsConsumerTest < ActiveSupport::TestCase
     end
   end
 
-  test 'report message is parsed and job is enqueued with request_id' do
-    SafeDownloader.expects(:download).returns(['report'])
-    @consumer.expects(:identity).returns(OpenStruct.new(valid?: true))
-             .at_least_once
-    @message.expects(:value).returns(
-      '{"account": "000001", "principal": "default_principal", '\
-      '"validation":1,"request_id":"036738d6f4e541c4aa8cfc9f46f5a140",'\
-      '"size": 327, "service": "compliance", "url": "/tmp/uploads'\
-      '/insights-upload-quarantine/036738d6f4e541c4aa8cfc9f46f5a140"}'
-    ).at_least_once
-    begin
-      @consumer.expects(:validation_message).returns('success')
-      @consumer.expects(:produce).with(
-        {
-          'request_id': '036738d6f4e541c4aa8cfc9f46f5a140',
-          'service': 'compliance',
-          'validation': 'success'
-        }.to_json,
-        topic: Settings.platform_kafka_validation_topic
-      )
-      @consumer.process(@message)
-      assert_equal 1, ParseReportJob.jobs.size
-    end
-  end
-
   test 'file is deleted even when validation fails' do
     SafeDownloader.expects(:download).returns(['report'])
     @consumer.expects(:identity).returns(OpenStruct.new(valid?: true))
