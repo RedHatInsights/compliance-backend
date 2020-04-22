a359dafb0e9ae7148840fba2b3e87c3d19bc40ed
Richard Brantley <rbrantle@redhat.com>
2020-04-22 16:12:51 -0400

    Use request_id instead of payload_id (#420)
    * Use request_id instead of payload_id
    
    * Remove duplicate test

--- test/producers/payload_tracker_test.rb
+++ test/producers/payload_tracker_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 class PayloadTrackerTest < ActiveSupport::TestCase
   test 'handles missing kafka config' do
-    assert_nil PayloadTracker.deliver(payload_id: 'foo', status: 'received',
+    assert_nil PayloadTracker.deliver(request_id: 'foo', status: 'received',
                                       account: '000001', system_id: 'foo')
   end
 
@@ -12,7 +12,7 @@ class PayloadTrackerTest < ActiveSupport::TestCase
     kafka = mock('kafka')
     PayloadTracker.stubs(:kafka).returns(kafka)
     kafka.expects(:deliver_message)
-    PayloadTracker.deliver(payload_id: 'foo', status: 'received',
+    PayloadTracker.deliver(request_id: 'foo', status: 'received',
                            account: '000001', system_id: 'foo')
   end
 
@@ -23,7 +23,7 @@ class PayloadTrackerTest < ActiveSupport::TestCase
     kafka.expects(:deliver_message).raises(Kafka::DeliveryFailed.new(nil, nil))
 
     assert_nothing_raised do
-      PayloadTracker.deliver(payload_id: 'foo', status: 'received',
+      PayloadTracker.deliver(request_id: 'foo', status: 'received',
                              account: '000001', system_id: 'foo')
     end
   end
