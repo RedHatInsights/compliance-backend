56fc21dabe51dfdd2ba50ec7aac61ef506847a1c
Daniel Lobato García <me@daniellobato.me>
2020-04-22 18:03:11 +0200

    Revert GQL-cache (#417) (#419)
    * Revert "Introduce graphql-cache (#411)"
    
    This reverts commit 3854f855b2d80f2df6e0963abe567e09b686f74b.
    
    * Unify references JSON response

--- app/graphql/types/profile.rb
+++ app/graphql/types/profile.rb
@@ -13,16 +13,15 @@ module Types
     graphql_name 'Profile'
     description 'A Profile registered in Insights Compliance'
 
-    field :id, ID, null: false, cache: true
-    field :name, String, null: false, cache: true
-    field :description, String, null: true, cache: true
-    field :ref_id, String, null: false, cache: true
-    field :compliance_threshold, Float, null: false, cache: true
-    field :benchmark_id, ID, null: false, cache: true
-    field :account_id, ID, null: false, cache: true
-    field :policy, Types::Profile, null: true, cache: true
-    field :rules, [::Types::Rule], null: true, cache: true,
-                                   extras: [:lookahead] do
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :description, String, null: true
+    field :ref_id, String, null: false
+    field :compliance_threshold, Float, null: false
+    field :benchmark_id, ID, null: false
+    field :account_id, ID, null: false
+    field :policy, Types::Profile, null: true
+    field :rules, [::Types::Rule], null: true, extras: [:lookahead] do
       argument :system_id, String,
                'System ID to filter by', required: false
       argument :identifier, String,
@@ -30,44 +29,43 @@ module Types
       argument :references, [String],
                'Rule references to filter by', required: false
     end
-    field :hosts, [::Types::System], null: true, cache: true
-    field :benchmark, ::Types::Benchmark, null: true, cache: true
-    field :business_objective, ::Types::BusinessObjective, null: true,
-                                                           cache: true
-    field :business_objective_id, ID, null: true, cache: true
-    field :total_host_count, Int, null: false, cache: true
-    field :external, Boolean, null: false, cache: true
+    field :hosts, [::Types::System], null: true
+    field :benchmark, ::Types::Benchmark, null: true
+    field :business_objective, ::Types::BusinessObjective, null: true
+    field :business_objective_id, ID, null: true
+    field :total_host_count, Int, null: false
+    field :external, Boolean, null: false
 
-    field :score, Float, null: false, cache: true do
+    field :score, Float, null: false do
       argument :system_id, String,
                'Latest TestResult score for this system and profile',
                required: false
     end
 
-    field :compliant, Boolean, null: false, cache: true do
+    field :compliant, Boolean, null: false do
       argument :system_id, String, 'Is a system compliant with this profile?',
                required: false
     end
 
-    field :rules_passed, Int, null: false, cache: true do
+    field :rules_passed, Int, null: false do
       argument :system_id, String,
                'Rules passed for a system and a profile', required: false
     end
 
-    field :rules_failed, Int, null: false, cache: true do
+    field :rules_failed, Int, null: false do
       argument :system_id, String,
                'Rules failed for a system and a profile', required: false
     end
 
-    field :last_scanned, String, null: false, cache: true do
+    field :last_scanned, String, null: false do
       argument :system_id, String,
                'Last time this profile was scanned for a system',
                required: false
     end
 
-    field :compliant_host_count, Int, null: false, cache: true
+    field :compliant_host_count, Int, null: false
 
-    field :major_os_version, String, null: false, cache: true
+    field :major_os_version, String, null: false
 
     def compliant_host_count
       ::CollectionLoader.for(object.class, :hosts).load(object).then do |hosts|
