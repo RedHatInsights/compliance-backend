f9c67e1c8c13551e8f6fe343cc46b8ef30b36917
Daniel Lobato Garcia <me@daniellobato.me>
2020-04-29 17:31:33 +0200

    Add uniqueness to combined index

--- db/migrate/20200428110354_add_index_to_test_results.rb
+++ db/migrate/20200428110354_add_index_to_test_results.rb
@@ -1,5 +1,5 @@
 class AddIndexToTestResults < ActiveRecord::Migration[5.2]
   def change
-    add_index :test_results, [:profile_id, :host_id, :end_time]
+    add_index :test_results, [:profile_id, :host_id, :end_time], unique: true
   end
 end
