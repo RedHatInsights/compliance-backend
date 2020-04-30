9deadeff24aa45b11a1a5b88bf3b636ba701799f
Daniel Lobato García <me@daniellobato.me>
2020-04-30 17:37:09 +0200

    RHICOMPL-631 - Update hostnames based on 'update' message (#429)
    * RHICOMPL-631 - Update hostnames based on 'update' message
    
    On Apr 28th, there will be a new event to tell apps to update hostnames
    if the inventory hostname changed. On the UI, this shouldn't affect us
    we already always display the inventory hostname (unless the
    host is unsynced for whichever reason and is only on compliance,
    this should not happen in prod though).
    
    On the API, this would be visible as we don't contact the
    inventory for anything there. So we should update the names
    according to the message to serve the right name.
    
    https://platform-docs.cloud.paas.psi.redhat.com/backend/inventory.html#host-patching
    for more info on the message format
    
    * Clear jobs
    
    * Validate message format properly
    
    * Avoid silent fails of InventoryHostUpdated job

--- app/jobs/delete_host.rb
+++ app/jobs/delete_host.rb
@@ -3,16 +3,14 @@
 # Job meant to delete hosts and associated objects asynchronously
 class DeleteHost
   include Sidekiq::Worker
+  include RecordNotFound
 
   def perform(message)
     Host.transaction do
       RuleResult.where(host_id: message['id']).delete_all
-      Host.find_by(id: message['id'])&.destroy
+      rescue_not_found do
+        Host.find_by!(id: message['id']).destroy
+      end
     end
-  rescue ActiveRecord::RecordNotFound => e
-    Sidekiq.logger.info(
-      "#{e.message} (#{e.class}) "\
-      '- this host ID was not registered in Compliance'
-    )
   end
 end
