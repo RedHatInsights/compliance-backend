bb6bb5bed7388a54a1b4d0f7415809a941305a61
Andrew Kofink <akofink@redhat.com>
2020-04-30 11:10:13 -0400

    Add unique constraint on rules table (#396)
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

---
+++ db/migrate/20200415201445_add_unique_index_to_rules.rb
@@ -0,0 +1,11 @@
+class AddUniqueIndexToRules < ActiveRecord::Migration[5.2]
+  def up
+    DuplicateRuleResolver.run!
+
+    add_index(:rules, %i[ref_id benchmark_id], unique: true)
+  end
+
+  def down
+    remove_index(:rules, %i[ref_id benchmark_id])
+  end
+end
---
+++ test/services/duplicate_rule_resolver_test.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require './db/migrate/20200415201445_add_unique_index_to_rules.rb'
+
+class DuplicateRuleResolverTest < ActiveSupport::TestCase
+  setup do
+    # rubocop:disable Lint/SuppressedException
+    begin
+      AddUniqueIndexToRules.new.down
+    rescue ArgumentError # if index doesn't exist
+    end
+    # rubocop:enable Lint/SuppressedException
+
+    assert_difference('Rule.count' => 1) do
+      (@dup_rule = rules(:one).dup).save(validate: false)
+    end
+  end
+
+  test 'resolves identical rules' do
+    assert_difference('Rule.count' => -1) do
+      DuplicateRuleResolver.run!
+    end
+  end
+
+  test 'resolves profile_rules from a duplicate rule with '\
+       'different profiles' do
+    assert_difference('ProfileRule.count' => 2) do
+      rules(:one).profiles << profiles(:one)
+      @dup_rule.profiles << profiles(:two)
+    end
+
+    assert_difference('ProfileRule.count' => 0) do
+      DuplicateRuleResolver.run!
+    end
+  end
+
+  test 'resolves profile_rules from a duplicate rule with '\
+       'the same profiles' do
+    assert_difference('ProfileRule.count' => 2) do
+      rules(:one).profiles << profiles(:two)
+      @dup_rule.profiles << profiles(:two)
+    end
+
+    assert_difference('ProfileRule.count' => -1) do
+      DuplicateRuleResolver.run!
+    end
+  end
+
+  test 'resolves rule_results from a duplicate rule' do
+    assert_difference('RuleResult.count' => 2) do
+      (rr = rule_results(:one).dup).save(validate: false)
+      (rr2 = rule_results(:one).dup).save(validate: false)
+      rules(:one).rule_results << rr
+      @dup_rule.rule_results << rr2
+    end
+
+    assert_difference('RuleResult.count' => 0) do
+      DuplicateRuleResolver.run!
+    end
+  end
+end
