2418f3455b1a29afe3b4d14976e788f27b494416
Daniel Lobato Garcia <me@daniellobato.me>
2020-05-04 09:54:23 +0200

    RHICOMPL-645 - Prometheus collector for business metrics
    Adding a collector for prometheus_exporter. This runs every time
    /metrics is called, which is every 60s, from the prometheus_exporter
    deployment. Most other metrics are calculated from the data send to
    /send-metrics by the other pods, but this sort of data isn't really
    related to any pods runtime.
    
    Timeout has been greatly increased just to prevent any failures,
    although benchmarks show the `collect` method is really fast.
    
    [3] pry(#<BusinessCollector>)> Benchmark.measure { collect }
    => #<Benchmark::Tms:0x00007f22216bb288
            @cstime=0.0,
    	@cutime=0.0,
    	@label="",
    	@real=0.030418214002565946,
    	@stime=0.002605999999999997,
    	@total=0.021810000000000218,
    	@utime=0.01920400000000022>
    
    In addition to that, I added binding to 0.0.0.0 as the latest prometheus
    binds to 127.0.0.1, which makes the data only visible through the pod.
    The docker-compose also now contains the necessary variables to access
    the Rails database. --verbose helps us see which collectors are loaded
    when prometheus_exporter boots.

---
+++ lib/prometheus/business_collector.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+unless defined? Rails
+  require File.expand_path('../../config/environment', __dir__)
+end
+
+# rubocop:disable Metrics/MethodLength
+# rubocop:disable Metrics/AbcSize
+# Collects stats relevent for the business from the database
+# when /metrics is called. This runs directly on prometheus_exporter.
+class BusinessCollector < PrometheusExporter::Server::TypeCollector
+  def initialize
+    @total_accounts = PrometheusExporter::Metric::Gauge.new(
+      'total_accounts', 'Total accounts'
+    )
+    @client_accounts = PrometheusExporter::Metric::Gauge.new(
+      'client_accounts', 'Client accounts (excludes Red Hat)'
+    )
+    @client_accounts_with_hosts = PrometheusExporter::Metric::Gauge.new(
+      'client_accounts_with_hosts',
+      'Client accounts with 1 or more hosts (excludes Red Hat)'
+    )
+    @total_policies = PrometheusExporter::Metric::Gauge.new(
+      'total_policies', 'Policies'
+    )
+    @external_policies = PrometheusExporter::Metric::Gauge.new(
+      'external_policies', 'External policies'
+    )
+    @internal_policies = PrometheusExporter::Metric::Gauge.new(
+      'internal_policies', 'Internal policies'
+    )
+    @client_policies = PrometheusExporter::Metric::Gauge.new(
+      'client_policies', 'Policies from clients (excludes Red Hat)'
+    )
+    @total_systems = PrometheusExporter::Metric::Gauge.new(
+      'total_systems', 'Systems'
+    )
+    @client_systems = PrometheusExporter::Metric::Gauge.new(
+      'client_systems', 'Systems from clients (excludes Red Hat)'
+    )
+  end
+
+  def type
+    'business'
+  end
+
+  def collect
+    @total_accounts.observe Account.count
+    client_accounts = Account.distinct.where(
+      id: User.select(:account_id).where.not('email LIKE ?', '%redhat%')
+    )
+    @client_accounts.observe client_accounts.count
+    @client_accounts_with_hosts.observe(
+      Host.where(
+        account_id: client_accounts.pluck(:id)
+      ).select(:account_id).distinct.count
+    )
+    @total_policies.observe Profile.count
+    @client_policies.observe(
+      Profile.where(account_id: client_accounts.pluck(:id)).count
+    )
+    @total_systems.observe Host.count
+    @client_systems.observe(
+      Host.where(account_id: client_accounts.pluck(:id)).count
+    )
+  end
+
+  def metrics
+    collect
+    [
+      @total_accounts,
+      @client_accounts,
+      @client_accounts_with_hosts,
+      @total_policies,
+      @client_policies,
+      @total_systems,
+      @client_systems
+    ]
+  end
+end
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Metrics/AbcSize
