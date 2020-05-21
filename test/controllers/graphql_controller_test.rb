e4a94bd39fd234284f5d7f9a2fba6334c39f021b
Daniel Lobato Garcia <me@daniellobato.me>
2020-05-21 11:03:11 +0200

    Add GQLcontroller test

---
+++ test/controllers/graphql_controller_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class GraphqlControllerTest < ActionDispatch::IntegrationTest
+  test 'calls the schema executor with a query, variables, and user' do
+    GraphqlController.any_instance.expects(:authenticate_user)
+    query = 'gqlquery'
+    variables = ['samplevar']
+    User.current = users(:test)
+    Schema.expects(:execute).with(
+      query, { variables: variables, context: { current_user: users(:test) }}
+    )
+    post graphql_url, params: { variables: variables, query: query }
+    assert_response :success
+  end
+end
+
