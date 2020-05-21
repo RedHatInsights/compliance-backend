e79bff7c3ab0c0d912b0d3b09daa894b7c34ca84
Daniel Lobato García <me@daniellobato.me>
2020-05-21 10:50:35 +0200

    Update supported SSG in RHEL 8 to 0.1.48 (#478) (#479)

--- app/models/xccdf/benchmark.rb
+++ app/models/xccdf/benchmark.rb
@@ -19,7 +19,7 @@ module Xccdf
     LATEST_SUPPORTED_VERSIONS = {
       'xccdf_org.ssgproject.content_benchmark_RHEL-6': '0.1.28',
       'xccdf_org.ssgproject.content_benchmark_RHEL-7': '0.1.46',
-      'xccdf_org.ssgproject.content_benchmark_RHEL-8': '0.1.46'
+      'xccdf_org.ssgproject.content_benchmark_RHEL-8': '0.1.48'
     }.freeze
 
     class << self
