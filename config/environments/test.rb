ca29d43d4a3b679365d7c839997bea149527d098
Daniel Lobato Garcia <me@daniellobato.me>
2020-05-22 17:51:23 +0200

    Preload env for codecov tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,7 +13,11 @@ Rails.application.configure do
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
+  if ENV['CODECOV_TOKEN']
+    config.eager_load = true
+  else
+    config.eager_load = false
+  end
 
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
