3ac03bfdb83b69a76ad446e4b3ac49fed3f6b0eb
Daniel Lobato Garcia <me@daniellobato.me>
2020-06-09 13:48:22 +0200

    RHICOMPL-610 - Internal profiles are only shown by default
    Profiles#index now only shows internal profiles by default, which are
    non external, non canonical profiles.

--- test/controllers/v1/profiles_controller_test.rb
+++ test/controllers/v1/profiles_controller_test.rb
@@ -26,15 +26,74 @@ module V1
         get tailoring_file_v1_profile_url(profiles(:one).id)
         assert_response :no_content
       end
+    end
 
-      test 'tailoring_file with a noncanonical profile '\
-           'returns tailoring file' do
-        profiles(:two).update!(rules: [rules(:one), rules(:two)])
-        profiles(:one).update!(parent_profile: profiles(:two),
-                               rules: [rules(:one)])
-        get tailoring_file_v1_profile_url(profiles(:one).id)
+    class IndexTest < ProfilesControllerTest
+      test 'external profiles can be requested' do
+        profiles(:one).update! external: true
+        search_query = 'external=true'
+        get v1_profiles_url, params: { search: search_query }
         assert_response :success
-        assert_equal Mime[:xml].to_s, @response.content_type
+
+        profiles = JSON.parse(response.body)
+        assert_equal 1, profiles['data'].length
+        assert_equal profiles(:one).id, profiles['data'].first['id']
+      end
+
+      test 'canonical profiles can be requested' do
+        search_query = 'canonical=true'
+        get v1_profiles_url, params: { search: search_query }
+        assert_response :success
+
+        profiles = JSON.parse(response.body)
+        assert_equal 1, profiles['data'].length
+        assert_equal profiles(:one).id, profiles['data'].first['id']
+      end
+
+      test 'does not contain external or canonical profiles by default' do
+        get v1_profiles_url
+        assert_response :success
+
+        profiles = JSON.parse(response.body)
+        assert_empty profiles['data']
+      end
+
+      test 'only contain internal profiles by default' do
+        internal = Profile.create!(
+          account: accounts(:test), name: 'foo', ref_id: 'foo',
+          benchmark: benchmarks(:one),
+          parent_profile: profiles(:one)
+        )
+        get v1_profiles_url
+        assert_response :success
+
+        profiles = JSON.parse(response.body)
+        assert_equal internal.id, profiles['data'].first['id']
+      end
+
+      test 'all profile types can be requested at the same time' do
+        internal = Profile.create!(
+          account: accounts(:test), name: 'foo', ref_id: 'foo',
+          benchmark: benchmarks(:one),
+          parent_profile: profiles(:one)
+        )
+        external = Profile.create!(
+          account: accounts(:test), name: 'bar', ref_id: 'bar',
+          external: true,
+          benchmark: benchmarks(:one),
+          parent_profile: profiles(:one)
+        )
+        search_query = 'canonical=true or canonical=false '\
+                       'or external=false or external=true'
+        get v1_profiles_url, params: { search: search_query }
+        assert_response :success
+
+        profiles = JSON.parse(response.body)
+        returned_ids = profiles['data'].map { |profile| profile['id'] }
+        assert_equal 3, profiles['data'].length
+        assert_includes returned_ids, internal.id
+        assert_includes returned_ids, external.id
+        assert_includes returned_ids, profiles(:one).id
       end
     end
 
