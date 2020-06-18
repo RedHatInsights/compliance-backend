0265ab303ef3d2764b639b7348f30b12a0c4858e
Daniel Lobato Garcia <me@daniellobato.me>
2020-06-18 17:15:31 +0200

    Add a few more tests for system_profile

--- test/services/host_inventory_api_test.rb
+++ test/services/host_inventory_api_test.rb
@@ -14,6 +14,9 @@ class HostInventoryApiTest < ActiveSupport::TestCase
     @b64_identity = '1234abcd'
     @api = HostInventoryAPI.new(@account, @url, @b64_identity)
     @connection = mock('faraday_connection')
+    @system_profile_response = OpenStruct.new(body: {
+      results: [{ id: @host.id, system_profile: { os_release: '8.2' } }]
+    }.to_json)
     Platform.stubs(:connection).returns(@connection)
   end
 
@@ -31,15 +34,12 @@ class HostInventoryApiTest < ActiveSupport::TestCase
 
   test 'inventory_host for host already in inventory' do
     @api.expects(:host_already_in_inventory).returns(@host)
-    system_profile_response = OpenStruct.new(body: {
-      results: [{ id: @host.id, system_profile: { os_release: '8.2' } }]
-    }.to_json)
     @connection.expects(:get).with(
       "#{@url}#{ENV['PATH_PREFIX']}/inventory/v1/hosts/"\
       "#{@host.id}/system_profile",
       { per_page: 50, page: 1 },
       X_RH_IDENTITY: @b64_identity
-    ).returns(system_profile_response)
+    ).returns(@system_profile_response)
     assert_equal @host, @api.inventory_host(@host.id)
     assert_equal 8, @host.os_major
     assert_equal 2, @host.os_minor
@@ -66,5 +66,34 @@ class HostInventoryApiTest < ActiveSupport::TestCase
       @host.id,
       'find_results did not return the expected match by ID'
     )
+  end
+
+  test 'system_profile returns a hash with OS info if found' do
+    @connection.expects(:get).with(
+      "#{@url}#{ENV['PATH_PREFIX']}/inventory/v1/hosts/"\
+      "#{@host.id}/system_profile",
+      { per_page: 50, page: 1 },
+      X_RH_IDENTITY: @b64_identity
+    ).returns(@system_profile_response)
+    system_profile_results = @api.system_profile([@host.id])
+    assert_equal '8', system_profile_results.first[:os_major]
+    assert_equal '2', system_profile_results.first[:os_minor]
+    assert_equal @host.id, system_profile_results.first[:id]
+  end
+
+  test 'system_profile returns a hash without OS info if not found' do
+    wrong_system_profile_response = OpenStruct.new(body: {
+      results: [{ id: @host.id, system_profile: { os_release: '' } }]
+    }.to_json)
+    @connection.expects(:get).with(
+      "#{@url}#{ENV['PATH_PREFIX']}/inventory/v1/hosts/"\
+      "#{@host.id}/system_profile",
+      { per_page: 50, page: 1 },
+      X_RH_IDENTITY: @b64_identity
+    ).returns(wrong_system_profile_response)
+    system_profile_results = @api.system_profile([@host.id])
+    assert_equal nil, system_profile_results.first[:os_major]
+    assert_equal nil, system_profile_results.first[:os_minor]
+    assert_equal @host.id, system_profile_results.first[:id]
   end
 end
