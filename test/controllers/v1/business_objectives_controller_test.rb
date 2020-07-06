fe6744cbfb77358331d59038b85b231e00a11ec6
Andrew Kofink <akofink@redhat.com>
2020-07-06 16:07:50 -0400

    Add business_objectives V1 API for index and show
    ```js
    GET /api/compliance/business_objectives
    
    {
        "data": [
            {
                "attributes": {
                    "title": "China"
                },
                "id": "5b661819-3ecd-4597-9c98-e7e5e0f6ca23",
                "relationships": {
                    "profiles": {
                        "data": [
                            {
                                "id": "3ad85c18-6339-4e87-ace5-44625cd1b172",
                                "type": "profile"
                            }
                        ]
                    }
                },
                "type": "business_objective"
            },
            {
                "attributes": {
                    "title": "Foo"
                },
                "id": "00577e90-63ff-4e7f-8238-867f879a8605",
                "relationships": {
                    "profiles": {
                        "data": [
                            {
                                "id": "35774cad-0c1b-4139-b6c7-012109798981",
                                "type": "profile"
                            }
                        ]
                    }
                },
                "type": "business_objective"
            }
        ],
        "links": {
            "first": "/api/compliance/business_objectives?limit=10&offset=1",
            "last": "/api/compliance/business_objectives?limit=10&offset=1"
        },
        "meta": {
            "limit": 10,
            "offset": 1,
            "search": null,
            "total": 2
        }
    }
    ```
    
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

---
+++ test/controllers/v1/business_objectives_controller_test.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module V1
+  class BusinessObjectivesControllerTest < ActionDispatch::IntegrationTest
+    setup do
+      BusinessObjectivesController.any_instance.stubs(:authenticate_user)
+      User.current = users(:test)
+      users(:test).update! account: accounts(:test)
+      profiles(:one).update!(account: accounts(:test),
+                             business_objective: business_objectives(:one))
+      profiles(:two).update!(account: accounts(:one),
+                             business_objective: business_objectives(:two))
+    end
+
+    context '#index' do
+      should 'only list business objectives associated to owned profiles' do
+        get v1_business_objectives_path
+        assert_response :success
+        assert_equal business_objectives(:one).id, parsed_data&.first&.dig('id')
+      end
+    end
+
+    context '#show' do
+      should 'succeed' do
+        get v1_business_objective_path(business_objectives(:one))
+        assert_response :success
+      end
+
+      should 'not show business objectives not associated to owned profiles' do
+        get v1_business_objective_path(business_objectives(:two))
+        assert_response :not_found
+      end
+    end
+  end
+end
--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -4,5 +4,8 @@ require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
   should have_many :users
+  should have_many :hosts
+  should have_many :profiles
+  should have_many :business_objectives
   should validate_presence_of :account_number
 end
