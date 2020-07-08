7f40930e40021a33ab110198f142666ae7ebcc05
Daniel Lobato Garcia <me@daniellobato.me>
2020-07-08 17:20:54 +0200

    RHICOMPL-668 - Import os_major/minor on report parsing
    Before this change, we got the os_release fields from the inventory, but
    we didn't update the compliance database.

--- test/services/concerns/xccdf/hosts_test.rb
+++ test/services/concerns/xccdf/hosts_test.rb
@@ -14,7 +14,9 @@ module Xccdf
         @test_result_file = test_result_file
         @host = host
         @account = account
-        @inventory_host = OpenStruct.new(id: @host.id, fqdn: @host.name)
+        @inventory_host = OpenStruct.new(id: @host.id, fqdn: @host.name,
+                                         os_major_version: 8,
+                                         os_minor_version: 2)
         set_openscap_parser_data
       end
     end
@@ -43,6 +45,13 @@ module Xccdf
       assert_difference('ProfileRule.count', 0) do
         @parser.associate_rules_from_rule_results
       end
+    end
+
+    test 'saves the OS attributes upon report parsing' do
+      @parser.save_host
+      parser_host = @parser.instance_variable_get('@host')
+      assert_equal 8, parser_host.reload.os_major_version
+      assert_equal 2, parser_host.reload.os_minor_version
     end
   end
 end
