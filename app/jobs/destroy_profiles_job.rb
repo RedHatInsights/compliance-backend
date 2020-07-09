6ec6b18c468e3c3f29b9afde4008946f2823be87
Daniel Lobato Garcia <me@daniellobato.me>
2020-07-09 12:30:34 +0200

    Add more logging to Sidekiq jobs

--- app/jobs/destroy_profiles_job.rb
+++ app/jobs/destroy_profiles_job.rb
@@ -5,6 +5,8 @@ class DestroyProfilesJob
   include Sidekiq::Worker
 
   def perform(ids)
+    Sidekiq.logger.info("Destroying profiles with IDs: #{ids}...")
     Profile.where(id: ids).destroy_all
+    Sidekiq.logger.info('Finished')
   end
 end
