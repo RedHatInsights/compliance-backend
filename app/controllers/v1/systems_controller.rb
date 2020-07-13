729a733b09dd09c355469f0b56002a0f5d3e5bb6
Andrew Kofink <akofink@redhat.com>
2020-07-13 12:29:31 -0400

    Add hosts#show and add includes to hosts API
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- app/controllers/v1/systems_controller.rb
+++ app/controllers/v1/systems_controller.rb
@@ -4,17 +4,27 @@ module V1
   # API for Systems (only Hosts for the moment)
   class SystemsController < ApplicationController
     def index
-      render json: HostSerializer.new(scope_search, metadata)
+      render_json scope_search
+    end
+
+    def show
+      render_json host
     end
 
     def destroy
-      host = Host.find(params[:id])
       authorize host
-      host.destroy
-      render HostSerializer.new(host)
+      render_json host.destroy
     end
 
     private
+
+    def host
+      @host ||= pundit_scope.find(params[:id])
+    end
+
+    def serializer
+      HostSerializer
+    end
 
     def resource
       Host
--- spec/integration/systems_spec.rb
+++ spec/integration/systems_spec.rb
@@ -4,6 +4,11 @@ require 'swagger_helper'
 
 describe 'Systems API' do
   fixtures :accounts, :hosts
+
+  before do
+    hosts(:one).update!(account: accounts(:one))
+  end
+
   path "#{ENV['PATH_PREFIX']}/#{ENV['APP_NAME']}/systems" do
     get 'List all hosts' do
       tags 'host'
@@ -37,6 +42,51 @@ describe 'Systems API' do
                  }
                }
 
+        after { |e| autogenerate_examples(e) }
+
+        run_test!
+      end
+    end
+  end
+
+  path "#{ENV['PATH_PREFIX']}/#{ENV['APP_NAME']}/systems/{id}" do
+    get 'Retrieve a system' do
+      tags 'host'
+      description 'Lists all hosts requested'
+      operationId 'ListHosts'
+
+      content_types
+      auth_header
+
+      parameter name: :id, in: :path, type: :string
+      include_param
+
+      response '404', 'system not found' do
+        let(:id) { 'invalid' }
+        let(:'X-RH-IDENTITY') { encoded_header }
+        let(:include) { '' } # work around buggy rswag
+        after { |e| autogenerate_examples(e) }
+        run_test!
+      end
+
+      response '200', 'retrieves a system' do
+        let(:'X-RH-IDENTITY') { encoded_header(accounts(:one)) }
+        let(:id) { hosts(:one).id }
+        let(:include) { '' } # work around buggy rswag
+        schema type: :object,
+               properties: {
+                 meta: ref_schema('metadata'),
+                 links: ref_schema('links'),
+                 data: {
+                   type: :object,
+                   properties: {
+                     type: { type: :string },
+                     id: ref_schema('uuid'),
+                     attributes: ref_schema('host'),
+                     relationships: ref_schema('host_relationships')
+                   }
+                 }
+               }
         after { |e| autogenerate_examples(e) }
 
         run_test!
