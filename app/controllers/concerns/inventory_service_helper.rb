570a804117ef303bf824febd44e1cf8984e57fc5
Andrew Kofink <akofink@redhat.com>
2020-08-26 13:40:41 -0400

    [RHICOMPL-467] Consolidate REST and graphql inventory logic
    When a profile and host are associated but the host only exists in the
    inventory, we must create the host from the inventory in our own
    database. We must handle this in both REST and graphql. This change
    consolidates the two code paths to use the same InventoryServiceHelper.
    
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

---
+++ app/controllers/concerns/inventory_service_helper.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+# Module to support controller integrations with the host inventory service
+module InventoryServiceHelper
+  extend ActiveSupport::Concern
+
+  included do
+    def add_inventory_hosts(ids)
+      existing_hosts = ::Pundit.policy_scope(current_user, ::Host)
+                               .where(id: ids)
+      save_hosts(ids - existing_hosts.pluck(:id)) + existing_hosts
+    end
+
+    def save_hosts(ids)
+      ids.map do |id|
+        save_host(id)
+      end
+    end
+
+    def save_host(id)
+      i_host = inventory_host(id)
+      host = ::Host.find_or_initialize_by(
+        id: i_host['id'],
+        account_id: current_user.account.id
+      )
+
+      host.update!(
+        name: i_host['display_name']
+      )
+
+      host
+    end
+
+    def inventory_host(id)
+      ::HostInventoryAPI.new(
+        current_user.account,
+        ::Settings.host_inventory_url,
+        nil # infer identity from account
+      ).inventory_host(id)
+    end
+  end
+end
