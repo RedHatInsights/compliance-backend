92110d7a98e273dcde7b21503d37fb7daccbd91f
Sebastian Gräßl <mail@bastilian.me>
2020-08-27 06:53:58 +0200

    [RHICOMPL-742] Allow editing the profile description and name

--- app/graphql/mutations/profile/edit.rb
+++ app/graphql/mutations/profile/edit.rb
@@ -7,21 +7,27 @@ module Mutations
       graphql_name 'UpdateProfile'
 
       argument :id, ID, required: true
+      argument :name, String, required: false
+      argument :description, String, required: false
       argument :compliance_threshold, Float, required: false
       argument :business_objective_id, ID, required: false
+
       field :profile, Types::Profile, null: true
 
       def resolve(args = {})
-        profile = Pundit.authorize(
+        profile = authorized_profile(args)
+        profile.update(args.compact)
+        { profile: profile }
+      end
+
+      private
+
+      def authorized_profile(args)
+        Pundit.authorize(
           context[:current_user],
           ::Profile.find(args[:id]),
           :edit?
         )
-        profile.update(
-          compliance_threshold: args[:compliance_threshold],
-          business_objective_id: args[:business_objective_id]
-        )
-        { profile: profile }
       end
     end
   end
