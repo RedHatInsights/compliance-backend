45d4354a3cd483cc239dd2e3ed127a0e00499886
Andrew Kofink <akofink@redhat.com>
2020-08-31 12:49:45 -0400

    [RHICOMPL-838] Rescue remediations errors, bubble errors on import
    This change adds connection errors to the list of rescued errors when
    importing remediations. It also ensures that any exceptions that are
    raised cause the entire rake task to fail (ssg:import_latest_supported).
    It also updates the slack channel for exception notifications to
    forum-cloudservices-compliance.
    
    https://projects.engineering.redhat.com/browse/RHICOMPL-838
    
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -23,7 +23,7 @@ ExceptionNotification.configure do |config|
 
   config.add_notifier :slack, {
     webhook_url: Settings.slack_webhook,
-    channel: '#insights-compliance',
+    channel: '#forum-cloudservices-compliance',
     additional_parameters: {
       mrkdwn: true
     }
