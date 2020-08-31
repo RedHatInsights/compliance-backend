2b83667279f44ca264a4374616d21f91c9d0b667
Andrew Kofink <akofink@redhat.com>
2020-08-31 16:48:10 -0400

    [RHICOMPL-574] sync_with_inventory actually saves host
    I'm not sure how this ever worked without saving the host to the DB.
    
    https://projects.engineering.redhat.com/browse/RHICOMPL-574
    
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

---
+++ test/tasks/sync_with_inventory_test.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'rake'
+
+class SyncWithInventoryTest < ActiveSupport::TestCase
+  setup do
+    HostInventoryAPI.any_instance.stubs(:inventory_host).returns(
+      'display_name' => 'foo',
+      'os_major_version' => 7,
+      'os_minor_version' => 5
+    )
+  end
+
+  test 'sync_with_inventory' do
+    assert_not_equal hosts(:one).name, 'foo'
+    assert_nil hosts(:one).os_minor_version
+    assert_nil hosts(:one).os_major_version
+
+    Rake::Task['sync_with_inventory'].execute
+
+    assert_equal hosts(:one).reload.name, 'foo'
+    assert_equal hosts(:one).os_major_version, 7
+    assert_equal hosts(:one).os_minor_version, 5
+  end
+end
