407cc543a5c15041c9d7ed6d2ca29d29296d7012
Andrew Kofink <akofink@redhat.com>
2020-09-18 13:16:01 -0400

    [RHICOMPL-840] Include os_major_version in benchmarks API
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- app/serializers/benchmark_serializer.rb
+++ app/serializers/benchmark_serializer.rb
@@ -3,7 +3,7 @@
 # JSON API serialization for an OpenSCAP Benchmark
 class BenchmarkSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :ref_id, :title, :version, :description
+  attributes :ref_id, :title, :version, :description, :os_major_version
   has_many :rules
   has_many :profiles do |benchmark|
     Pundit.policy_scope(User.current, Profile).where(benchmark: benchmark)
