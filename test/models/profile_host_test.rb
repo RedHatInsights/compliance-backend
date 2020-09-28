441b173c7e5f3395de84e80f1f3353e8e445ce0b
Andrew Kofink <akofink@redhat.com>
2020-09-28 14:22:33 -0400

    [RHICOMPL-987] Do not delete orphaned hosts (#589)
    Before this change, deleting a profile with hosts associated may have
    deleted the hosts as well if they were not associated with any other
    profiles. After this change, we will only remove hosts from our database
    during an Inventory culling message in kafka.
    
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- test/models/profile_host_test.rb
+++ test/models/profile_host_test.rb
@@ -4,25 +4,6 @@ require 'test_helper'
 require 'sidekiq/testing'
 
 class ProfileHostTest < ActiveSupport::TestCase
-  setup do
-    DeleteHost.clear
-  end
-
-  test 'destroys associated hosts if host has no more policies assigned' do
-    profile = Profile.create(name: 'foo', benchmark: benchmarks(:one),
-                             ref_id: 'foo')
-    host = Host.create(profiles: [profile], name: 'bar',
-                       account: accounts(:one))
-    assert_equal 0, DeleteHost.jobs.size
-    profile.destroy
-    assert_equal 1, DeleteHost.jobs.size
-    assert_difference('Host.count', -1) do
-      DeleteHost.drain
-    end
-    assert_empty Host.where(id: host.id)
-    assert_equal 0, DeleteHost.jobs.size
-  end
-
   test 'destroy associated external policies if they have no more hosts' do
     internal_profile = Profile.create(name: 'foo', benchmark: benchmarks(:one),
                                       ref_id: 'foo', account: accounts(:one))
