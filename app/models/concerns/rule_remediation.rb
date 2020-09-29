343975ef03c8e7c477f3c8edb369a64c976a9ca6
Viliam Krizan <vkrizan@redhat.com>
2020-09-29 11:19:02 +0200

    [RHICOMPL-991] Refactor Profile short ref_id

--- app/models/concerns/rule_remediation.rb
+++ app/models/concerns/rule_remediation.rb
@@ -5,22 +5,10 @@ module RuleRemediation
   extend ActiveSupport::Concern
 
   def remediation_issue_id
-    profile_ref = short_profile_ref_id
-    return unless profile_ref
-
-    "ssg:rhel7|#{profile_ref}|#{ref_id}"
-  end
-
-  private
-
-  def short_profile_ref_id
     # FIXME: Nondetermenistic canonical selection
     profile = profiles.select(&:canonical?)&.first
     return unless profile
 
-    short_ref_id = profile.ref_id.downcase.split(
-      'xccdf_org.ssgproject.content_profile_'
-    )[1]
-    short_ref_id || profile.ref_id
+    "ssg:rhel7|#{profile.short_ref_id}|#{ref_id}"
   end
 end
