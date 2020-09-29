c57d9f1613c049848b90080cff845023161d6745
Viliam Krizan <vkrizan@redhat.com>
2020-09-29 11:43:15 +0200

    [RHICOMPL-991] Major OS ver for remediation issue id
    Generate remediation issue id's OS part based on its profile/benchmark.
    Do not hardcode the "rhel7" value, in order to support different major
    OS versions.

--- app/models/concerns/rule_remediation.rb
+++ app/models/concerns/rule_remediation.rb
@@ -4,11 +4,15 @@
 module RuleRemediation
   extend ActiveSupport::Concern
 
+  # It is recommended to prefetch profiles with their benchmarks:
+  #
+  #  `.includes(profiles: :benchmark)`
+  #
   def remediation_issue_id
     # FIXME: Nondetermenistic canonical selection
     profile = profiles.select(&:canonical?)&.first
     return unless profile
 
-    "ssg:rhel7|#{profile.short_ref_id}|#{ref_id}"
+    "ssg:rhel#{profile.os_major_version}|#{profile.short_ref_id}|#{ref_id}"
   end
 end
