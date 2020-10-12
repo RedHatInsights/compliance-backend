5d861a402a00f096cc99c65c8083c5d9b297ad1b
Viliam Krizan <vkrizan@redhat.com>
2020-10-12 17:20:32 +0200

    [RHICOMPL-948] Collection of supported SSGs available upstream
    Provide list of supported SSGs by RHEL minor version, that are avialable
    uptream (have ZIP package).  Note, that RHEL 6 ships very old SSG
    packages.

--- test/models/supported_ssg_test.rb
+++ test/models/supported_ssg_test.rb
@@ -10,4 +10,25 @@ class SupportedSsgTest < ActiveSupport::TestCase
   test 'provides revision' do
     assert SupportedSsg.revision
   end
+
+  context 'loaded supported SSGs' do
+    setup do
+      loaded = [
+        SupportedSsg.new(version: '0.1.50'),
+        SupportedSsg.new(version: '0.1.24'),
+        SupportedSsg.new(upstream_version: '0.1.25', version: '0.1.22'),
+        SupportedSsg.new(upstream_version: 'N/A', version: '0.1.1')
+      ]
+      SupportedSsg.stubs(:all).returns(loaded)
+    end
+
+    should 'provide models available upstream' do
+      in_upstream = SupportedSsg.available_upstream
+      versions = in_upstream.map(&:version)
+      assert_includes versions, '0.1.50'
+      assert_includes versions, '0.1.24'
+      assert_includes versions, '0.1.22' # upstream version is higher
+      assert_equal in_upstream.count, 3
+    end
+  end
 end
