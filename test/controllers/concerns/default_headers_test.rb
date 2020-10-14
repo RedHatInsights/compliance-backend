57d085cc011dc6f78422558ccbfaae4c6f012f4b
Viliam Krizan <vkrizan@redhat.com>
2020-10-14 13:20:08 +0200

    [RHICOMPL-1037] Use default headers also on API controllers
    Default headers were not being set for an API only controllers. This
    issue has been fixed in Rails 6.0.
    
    References:
    
    https://github.com/rails/rails/commit/f22bc41a92e8f51d6f6da5b840f3364474d6aaba
    https://github.com/rails/rails/pull/32484

---
+++ test/controllers/concerns/default_headers_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class DefaultHeadersTest < ActionDispatch::IntegrationTest
+  setup do
+    ApplicationController.any_instance.expects(:authenticate_user)
+    ApplicationController.any_instance.stubs(:index).returns('Response Body')
+    Rails.application.routes.draw do
+      root 'application#index'
+    end
+  end
+
+  teardown do
+    Rails.application.reload_routes!
+  end
+
+  test 'response includes default headers' do
+    get '/'
+    default_headers = Rails.application.config
+                           .action_dispatch.default_headers
+                           .keys.to_set
+    assert default_headers.subset?(response.header.keys.to_set)
+  end
+end
