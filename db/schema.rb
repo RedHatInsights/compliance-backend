aa59c49beaa344b42a718b0954386263bade6a12
Viliam Krizan <vkrizan@redhat.com>
2020-10-20 15:32:06 +0200

    [RHICOMPL-662] Migration of old profile data into new policy records (#610)
    * [RHICOMPL-662] Create a service for copying profiles into Policies
    
    The service creates new Policy models for Profiles that are internal and
    that do not have a Policy.  It copies some of the attributes from the
    profile.  It assigns the same host set from the internal profile.
    
    * [RHICOMPL-662] First pass for migration of profile data to policies
    
    This is mostly intended for testing the migration.  Production should
    be migrated only when the whole feature is complete.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_153704) do
+ActiveRecord::Schema.define(version: 2020_10_15_174517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
