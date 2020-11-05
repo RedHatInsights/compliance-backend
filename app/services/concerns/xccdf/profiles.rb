510a071352e651b7817af1154acffa2fa84316ce
Viliam Krizan <vkrizan@redhat.com>
2020-11-05 12:20:20 +0100

    Remove dead code in Xccdf report parser

--- app/services/concerns/xccdf/profiles.rb
+++ app/services/concerns/xccdf/profiles.rb
@@ -15,23 +15,6 @@ module Xccdf
         ::Profile.import!(@profiles.select(&:new_record?), ignore: true)
       end
       alias_method :save_profiles, :profiles
-
-      private
-
-      def account_profile_ref_ids
-        @account.profiles.map(&:ref_id)
-      end
-
-      def clone_and_update(new_profiles)
-        ::Profile.import!(new_profiles.map! do |profile|
-          new_profile = @account.profiles.find_by(ref_id: profile.ref_id) ||
-            profile.deep_clone
-          new_profile.account ||= @account
-          new_profile.hosts = new_profile.hosts | [@host]
-          new_profile
-        end, recursive: true)
-        new_profiles
-      end
     end
   end
 end
