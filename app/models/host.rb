f51f4f7a245f282e0e98be5e3bcc04599745b36e
Andrew Kofink <akofink@redhat.com>
2020-11-13 13:57:50 -0500

    refactor(RHICOMPL-1091): Delete duplicate test_result_profiles
    This should not affect any behavior
    
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- app/models/host.rb
+++ app/models/host.rb
@@ -18,7 +18,6 @@ class Host < ApplicationRecord
   has_many :profile_hosts, dependent: :destroy
   has_many :policy_hosts, dependent: :destroy
   has_many :test_results, dependent: :destroy
-  has_many :test_result_profiles, through: :test_results, dependent: :destroy
   include SystemLike
 
   has_many :profile_host_profiles, through: :profile_hosts, source: :profile
