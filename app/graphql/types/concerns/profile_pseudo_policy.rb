8e6a7081bcf90a9afd827138c964802ccd3e4f6d
Viliam Krizan <vkrizan@redhat.com>
2020-11-18 12:03:31 +0100

    fix(Profile): RHICOMPL-1077 GraphQL to return policy name and desc
    Initial (main) policy profile should return name and the description
    from the policy on GraphQL, which are user defined.
    Profile and pseudo policy (profile) would return same name and the
    description on GraphQL, if they are the same objects.
    
    This will fix the problem of caching on UI, where the same GQL objects
    (identified by id) had a different values depending on context.
    It should also improve and fix filtering and displaying in UI.

--- app/graphql/types/concerns/profile_pseudo_policy.rb
+++ app/graphql/types/concerns/profile_pseudo_policy.rb
@@ -5,18 +5,19 @@ module Types
   module ProfilePseudoPolicy
     extend ActiveSupport::Concern
 
+    def name
+      object == policy ? object.policy_object.name : object.name
+    end
+
+    def description
+      object == policy ? object.policy_object.description : object.description
+    end
+
     # Pseudo policy with a Profile type
-    # inheriting most of the attributes form the policy.
     def policy
       return if object.canonical? || object.policy_object.nil?
 
-      policy = object.policy_object
-      policy_profile = object.policy_object.initial_profile
-      policy_profile.assign_attributes(
-        name: policy.name,
-        description: policy.description
-      )
-      policy_profile
+      object.policy_object.initial_profile
     end
 
     # policy profiles
