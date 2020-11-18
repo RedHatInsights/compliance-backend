0fc1aa33ad9b78bd6e6204a4a4cfdfbdda2d3181
Viliam Krizan <vkrizan@redhat.com>
2020-11-18 12:03:31 +0100

    fix(Profile): RHICOMPL-1077 GraphQL to return policy name and desc
    Initial (main) policy profile should return name and the description
    from the policy on GraphQL, which are user defined.
    Profile and pseudo policy (profile) would return same name and the
    description on GraphQL, if they are the same objects.
    
    This will fix the problem of caching on UI, where the same GQL objects
    (identified by id) had a different values depending on context.
    It should also improve and fix filtering and displaying in UI.

--- test/graphql/queries/profile_query_test.rb
+++ test/graphql/queries/profile_query_test.rb
@@ -101,95 +101,147 @@ class ProfileQueryTest < ActiveSupport::TestCase
     end
   end
 
-  test 'query profile with a policy owned by the user' do
-    query = <<-GRAPHQL
-      query Profile($id: String!){
-          profile(id: $id) {
-              id
-              name
-              refId
-              policy {
+  context 'policy profiles' do
+    setup do
+      policies(:one).update!(account: accounts(:test),
+                             hosts: [hosts(:one), hosts(:two)])
+
+      (parent = profiles(:one).dup).update!(account: nil, hosts: [])
+      profiles(:one).update!(policy_object: policies(:one),
+                             external: false,
+                             parent_profile: parent)
+      profiles(:two).update!(account: accounts(:test),
+                             external: true,
+                             parent_profile: parent,
+                             policy_object: policies(:one))
+    end
+
+    should 'query profile with a policy owned by the user' do
+      query = <<-GRAPHQL
+        query Profile($id: String!){
+            profile(id: $id) {
                 id
                 name
                 refId
-              }
-          }
-      }
-    GRAPHQL
+                policy {
+                  id
+                  name
+                  refId
+                }
+            }
+        }
+      GRAPHQL
 
-    policies(:one).update!(account: accounts(:test),
-                           hosts: [hosts(:one), hosts(:two)])
+      result = Schema.execute(
+        query,
+        variables: { id: profiles(:two).id },
+        context: { current_user: users(:test) }
+      )
 
-    (parent = profiles(:one).dup).update!(account: nil, hosts: [])
-    profiles(:one).update!(policy_object: policies(:one),
-                           parent_profile: parent)
-    profiles(:two).update!(account: accounts(:test),
-                           external: true,
-                           parent_profile: parent,
-                           policy_object: policies(:one))
+      assert_equal profiles(:two).name, result['data']['profile']['name']
+      assert_equal profiles(:two).ref_id, result['data']['profile']['refId']
 
-    result = Schema.execute(
-      query,
-      variables: { id: profiles(:two).id },
-      context: { current_user: users(:test) }
-    )
+      assert_equal profiles(:one).id,
+                   result['data']['profile']['policy']['id']
+      assert_equal profiles(:one).ref_id,
+                   result['data']['profile']['policy']['refId']
+      assert_equal policies(:one).name,
+                   result['data']['profile']['policy']['name']
+    end
 
-    assert_equal profiles(:two).name, result['data']['profile']['name']
-    assert_equal profiles(:two).ref_id, result['data']['profile']['refId']
-
-    assert_equal profiles(:one).id,
-                 result['data']['profile']['policy']['id']
-    assert_equal profiles(:one).ref_id,
-                 result['data']['profile']['policy']['refId']
-    assert_equal policies(:one).name,
-                 result['data']['profile']['policy']['name']
-  end
-
-  test 'query profile with a policy policy profiles owned by the user' do
-    query = <<-GRAPHQL
+    should 'query profile with a policy profiles using first policy profile' \
+    ' owned by the user' do
+      query = <<-GRAPHQL
       query Profile($id: String!){
           profile(id: $id) {
               id
               name
+              description
               refId
               policy {
                 profiles {
                   id
                   refId
+                  name
+                  description
                 }
               }
           }
       }
-    GRAPHQL
+      GRAPHQL
 
-    policies(:one).update!(account: accounts(:test),
-                           hosts: [hosts(:one), hosts(:two)])
+      result = Schema.execute(
+        query,
+        variables: { id: profiles(:one).id },
+        context: { current_user: users(:test) }
+      )
 
-    (parent = profiles(:one).dup).update!(account: nil, hosts: [])
-    profiles(:one).update!(policy_object: policies(:one),
-                           parent_profile: parent)
-    profiles(:two).update!(account: accounts(:test),
-                           external: true,
-                           parent_profile: parent,
-                           policy_object: policies(:one))
+      assert_equal policies(:one).name, result['data']['profile']['name']
+      assert_equal policies(:one).description,
+                   result['data']['profile']['description']
+      assert_equal profiles(:one).ref_id, result['data']['profile']['refId']
 
-    result = Schema.execute(
-      query,
-      variables: { id: profiles(:two).id },
-      context: { current_user: users(:test) }
-    )
+      returned_profiles = result['data']['profile']['policy']['profiles']
+      assert_equal returned_profiles.count, 2
 
-    assert_equal profiles(:two).name, result['data']['profile']['name']
-    assert_equal profiles(:two).ref_id, result['data']['profile']['refId']
+      policy_profile =
+        returned_profiles.find { |rp| rp['id'] == profiles(:one).id }
+      assert_equal profiles(:one).ref_id, policy_profile['refId']
+      assert_equal policies(:one).name, policy_profile['name']
+      assert_equal policies(:one).description, policy_profile['description']
 
-    returned_profiles = result['data']['profile']['policy']['profiles']
-    assert_equal returned_profiles.count, 2
-    assert_includes returned_profiles.map { |rp| rp['id'] }, profiles(:one).id
-    assert_includes returned_profiles.map { |rp| rp['refId'] },
-                    profiles(:one).ref_id
-    assert_includes returned_profiles.map { |rp| rp['id'] }, profiles(:two).id
-    assert_includes returned_profiles.map { |rp| rp['refId'] },
-                    profiles(:two).ref_id
+      second_profile =
+        returned_profiles.find { |rp| rp['id'] == profiles(:two).id }
+      assert_equal profiles(:two).ref_id, second_profile['refId']
+      assert_equal profiles(:two).name, second_profile['name']
+      assert_equal profiles(:two).description, second_profile['description']
+    end
+
+    should 'query profile with a policy profiles using any policy profile' \
+           ' owned by the user' do
+      query = <<-GRAPHQL
+        query Profile($id: String!){
+            profile(id: $id) {
+                id
+                name
+                description
+                refId
+                policy {
+                  profiles {
+                    id
+                    refId
+                    name
+                    description
+                  }
+                }
+            }
+        }
+      GRAPHQL
+
+      result = Schema.execute(
+        query,
+        variables: { id: profiles(:two).id },
+        context: { current_user: users(:test) }
+      )
+
+      assert_equal profiles(:two).name, result['data']['profile']['name']
+      assert_equal profiles(:two).ref_id, result['data']['profile']['refId']
+
+      returned_profiles = result['data']['profile']['policy']['profiles']
+      assert_equal returned_profiles.count, 2
+
+      policy_profile =
+        returned_profiles.find { |rp| rp['id'] == profiles(:one).id }
+      assert_equal profiles(:one).ref_id, policy_profile['refId']
+      assert_equal policies(:one).name, policy_profile['name']
+      assert_equal policies(:one).description, policy_profile['description']
+
+      second_profile =
+        returned_profiles.find { |rp| rp['id'] == profiles(:two).id }
+      assert_equal profiles(:two).ref_id, second_profile['refId']
+      assert_equal profiles(:two).name, second_profile['name']
+      assert_equal profiles(:two).description, second_profile['description']
+    end
   end
 
   test 'query all profiles' do
