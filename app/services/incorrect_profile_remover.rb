d9489bcf3dff75545617c47418b61b7d760c00ab
Andrew Kofink <akofink@redhat.com>
2021-01-07 10:33:56 -0500

    fix(data): RHICOMPL-1301 remove incorrect profiles from policies
    A bug introduced in RHICOMPL-1295 caused some profiles to be incorrectly
    associated to policies. The initial profile on a policy essentially
    determines the policy's policy type. If any profile ref_id does not
    match this initial profile's ref_id, it does not belong on the policy
    and will be removed along with any test results.
    
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

---
+++ app/services/incorrect_profile_remover.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# A class to remove profiles on policies that do not have a matching ref_id
+class IncorrectProfileRemover
+  def self.run!
+    counts = Policy.includes(:profiles).find_each.flat_map do |policy|
+      next unless policy.profiles.length > 1
+
+      find_incorrect_profiles(policy).destroy_all.count
+    end.compact
+
+    Logger.new(STDOUT).info "#{counts.sum} profiles removed from policies with"\
+                            ' mismatched ref_ids'
+  end
+
+  def self.find_incorrect_profiles(policy)
+    Profile.where(policy_object: policy)
+           .where.not(ref_id: policy.initial_profile.ref_id)
+  end
+end
