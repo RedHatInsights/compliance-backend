bb3c99dde42da62738d0544f2723e468fb7a25f1
Andrew Kofink <akofink@redhat.com>
2021-01-12 17:22:17 -0500

    chore(rails): RHICOMPL-1317 run rails app:upgrade
    Signed-off-by: Andrew Kofink <akofink@redhat.com>
    (cherry picked from commit eb95323d3fa00732600269755e8b6d1eef89d033)

--- config/puma.rb
+++ config/puma.rb
@@ -22,8 +22,11 @@ end
 #
 environment ENV.fetch('RAILS_ENV') { 'development' }
 
+# Specifies the `pidfile` that Puma will use.
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
 # Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked webserver processes. If using threads and workers together
+# Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
