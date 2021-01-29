bcc0ca75646a9db4216a039210a2461f6b53ed27
Andrew Kofink <akofink@redhat.com>
2021-01-29 17:37:07 -0500

    feat(metrics): RHICOMPL-1285 for dangling host associations
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

---
+++ lib/prometheus/engineering_collector.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+unless defined? Rails
+  require File.expand_path('../../config/environment', __dir__)
+end
+
+# rubocop:disable Metrics/MethodLength
+# Collects stats relevent for the engineers from the database
+# when /metrics is called. This runs directly on prometheus_exporter.
+class EngineeringCollector < PrometheusExporter::Server::TypeCollector
+  def initialize
+    @dangling_accounts = PrometheusExporter::Metric::Gauge.new(
+      'dangling_accounts', 'Accounts without hosts'
+    )
+    @dangling_test_results = PrometheusExporter::Metric::Gauge.new(
+      'dangling_test_results', 'TestResults without hosts'
+    )
+    @dangling_rule_results = PrometheusExporter::Metric::Gauge.new(
+      'dangling_rule_results', 'RuleResults without hosts'
+    )
+    @dangling_policy_hosts = PrometheusExporter::Metric::Gauge.new(
+      'dangling_policy_hosts', 'PolicyHosts without hosts'
+    )
+  end
+
+  def type
+    'engineering'
+  end
+
+  def dangling(model)
+    model.where.not(host_id: Host.select(:id))
+  end
+
+  def collect
+    @dangling_accounts.observe(
+      Account.where.not(account_number: Host.select(:account)).count
+    )
+    @dangling_test_results.observe(dangling(TestResult).count)
+    @dangling_rule_results.observe(dangling(RuleResult).count)
+    @dangling_policy_hosts.observe(dangling(PolicyHost).count)
+  end
+
+  def metrics
+    collect
+
+    [
+      @dangling_accounts,
+      @dangling_test_results,
+      @dangling_rule_results,
+      @dangling_policy_hosts
+    ]
+  end
+end
+# rubocop:enable Metrics/MethodLength
