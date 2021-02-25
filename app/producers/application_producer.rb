becb1b0fb2751785079357c4ef6e5309d08b6190
Andrew Kofink <akofink@redhat.com>
2021-02-25 14:45:54 -0500

    feat(Kafka Producers): RHICOMPL-1242 now with SSL
    Our kafka producers will now allow SSL with the same config the
    consumers use to connect to kafka. This is fairly difficult to test
    in devel without setting up kafka with SSL, but I intend to test this
    thoroughly in stage. In any case, it should be able to use plaintext
    mode with the change of the racecar environment variables.
    
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- app/producers/application_producer.rb
+++ app/producers/application_producer.rb
@@ -4,6 +4,8 @@
 # https://www.rubydoc.info/gems/ruby-kafka/Kafka/Producer
 class ApplicationProducer < Kafka::Client
   BROKERS = [ENV['KAFKAMQ']].compact.freeze
+  SSL_CA_LOCATION = ENV['RACECAR_SSL_CA_LOCATION']
+  SECURITY_PROTOCOL = ENV['RACECAR_SECURITY_PROTOCOL']
   CLIENT_ID = 'compliance-backend'
   SERVICE = 'compliance'
   # Define TOPIC in the inherited class.
@@ -26,8 +28,19 @@ class ApplicationProducer < Kafka::Client
       Rails.logger
     end
 
+    def kafka_ca_cert
+      File.read(self::SSL_CA_LOCATION) if self::SECURITY_PROTOCOL == 'ssl'
+    end
+
+    def kafka_config
+      {}.tap do |config|
+        config[:client_id] = self::CLIENT_ID
+        config[:ssl_ca_cert] = kafka_ca_cert if kafka_ca_cert
+      end
+    end
+
     def kafka
-      @kafka ||= Kafka.new(BROKERS, client_id: CLIENT_ID) if BROKERS.any?
+      @kafka ||= Kafka.new(self::BROKERS, **kafka_config) if self::BROKERS.any?
     end
   end
 end
