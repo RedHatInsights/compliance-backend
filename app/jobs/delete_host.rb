57647c63d512061a617a7fe85f0b92b17c8144ca
Viliam Krizan <vkrizan@redhat.com>
2021-03-01 17:29:58 +0100

    refactor(jobs): RHICOMPL-1474 DeleteHost captures number of rows removed

--- app/jobs/delete_host.rb
+++ app/jobs/delete_host.rb
@@ -4,12 +4,23 @@
 class DeleteHost
   include Sidekiq::Worker
 
+  MODELS = [RuleResult, TestResult, PolicyHost].freeze
+
   def perform(message)
+    host_id = message['id']
+    remove_related(host_id)
+  end
+
+  private
+
+  def remove_related(host_id)
+    num_removed = 0
     Host.transaction do
-      Sidekiq.logger.info("Deleting related records for host #{message['id']}")
-      [RuleResult, TestResult, PolicyHost].each do |model|
-        model.where(host_id: message['id']).delete_all
+      Sidekiq.logger.info("Deleting related records for host #{host_id}")
+      MODELS.each do |model|
+        num_removed += model.where(host_id: host_id).delete_all
       end
     end
+    num_removed
   end
 end
