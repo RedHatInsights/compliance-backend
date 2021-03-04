238b31dcac25f994a47f99c5c58fba883b8b4177
Dávid Halász <dhalasz@redhat.com>
2021-03-04 10:02:12 +0100

    chore(tests): RHICOMPL-1482 prevent constant redefinition in tests
    Constant redefinitions cause warnings that flood the test output

--- test/services/datastream_importer_test.rb
+++ test/services/datastream_importer_test.rb
@@ -5,18 +5,18 @@ require 'test_helper'
 # A class to test importing from a Datastream file
 class DatastreamImporterTest < ActiveSupport::TestCase
   setup do
-    DATASTREAM_FILE = file_fixture('ssg-rhel7-ds.xml')
+    @datastream_file = file_fixture('ssg-rhel7-ds.xml')
   end
 
   test 'datastream import' do
-    importer = DatastreamImporter.new(DATASTREAM_FILE)
+    importer = DatastreamImporter.new(@datastream_file)
     importer.expects(:save_all_benchmark_info)
     ENV['JOBS_ACCOUNT_NUMBER'] ||= Account.first.account_number
     importer.import!
   end
 
   test 'works without any accounts' do
-    importer = DatastreamImporter.new(DATASTREAM_FILE)
+    importer = DatastreamImporter.new(@datastream_file)
     importer.expects(:save_all_benchmark_info)
     ENV['JOBS_ACCOUNT_NUMBER'] ||= nil
     importer.import!
