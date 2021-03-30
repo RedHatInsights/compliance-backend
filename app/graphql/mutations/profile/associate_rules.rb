a384271a5b91db9c8927dc492ac7bdf27a50f92b
Dávid Halász <dhalasz@redhat.com>
2021-03-30 12:27:18 +0200

    feat(EditPolicy): RHICOMPL-1576 assigning rules via ruleRefIds

--- app/graphql/mutations/profile/associate_rules.rb
+++ app/graphql/mutations/profile/associate_rules.rb
@@ -9,16 +9,16 @@ module Mutations
       graphql_name 'associateRules'
 
       argument :id, ID, required: true
-      argument :rule_ids, [ID], required: true
+      argument :rule_ids, [ID], required: false
+      argument :rule_ref_ids, [String], required: false
       field :profile, Types::Profile, null: true
 
       def resolve(args = {})
+        rules = prepare_rules(args)
         ::Profile.transaction do
           profile = find_profile(args[:id])
           if profile
-            rules_added, rules_removed = profile.update_rules(
-              ids: args[:rule_ids]
-            )
+            rules_added, rules_removed = profile.update_rules(rules)
             audit_mutation(profile, rules_added, rules_removed)
           end
           { profile: profile }
@@ -32,6 +32,16 @@ module Mutations
           "Updated rule assignments of profile #{profile.id}," \
           " #{added} rules added, #{removed} rules removed"
         )
+      end
+
+      def prepare_rules(args)
+        if args[:rule_ids]
+          { ids: args[:rule_ids] }
+        elsif args[:rule_ref_ids]
+          { ref_ids: args[:rule_ref_ids] }
+        else
+          raise ArgumentError, 'Missing argument identifying rules'
+        end
       end
     end
   end
