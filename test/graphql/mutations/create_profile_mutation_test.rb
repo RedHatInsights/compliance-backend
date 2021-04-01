7678295054598944d648d3d2a55ad494da8224b9
Andrew Kofink <akofink@redhat.com>
2021-04-01 11:14:47 -0400

    feat(createProfile): RHICOMPL-1607 no longer requires rule ref IDs
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- test/graphql/mutations/create_profile_mutation_test.rb
+++ test/graphql/mutations/create_profile_mutation_test.rb
@@ -27,6 +27,26 @@ class CreateProfileMutationTest < ActiveSupport::TestCase
         refId: 'xccdf-customized',
         name: 'customized profile',
         description: 'abcdf',
+        complianceThreshold: 90.0
+      } },
+      context: { current_user: users(:test) }
+    )['data']['createProfile']['profile']
+
+    cloned_profile = ::Profile.find(result['id'])
+    assert cloned_profile.ref_id, 'xccdf-customized'
+    assert_equal cloned_profile.rules, @original_profile.rules
+    assert_equal cloned_profile.parent_profile, @original_profile
+  end
+
+  test 'provide all required arguments with selectedRuleRefIds' do
+    result = Schema.execute(
+      @query,
+      variables: { input: {
+        benchmarkId: @original_profile.benchmark_id,
+        cloneFromProfileId: @original_profile.id,
+        refId: 'xccdf-customized',
+        name: 'customized profile',
+        description: 'abcdf',
         complianceThreshold: 90.0,
         selectedRuleRefIds: @original_profile.rules.map(&:ref_id)
       } },
