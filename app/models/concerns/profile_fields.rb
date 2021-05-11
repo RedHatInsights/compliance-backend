d6d388e571e29758fefcde83234cce5b23c724b6
Viliam Krizan <vkrizan@redhat.com>
2021-05-11 12:30:45 +0200

    refactor(Profile): RHICOMPL-1782 split computed fields

---
+++ app/models/concerns/profile_fields.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+# Computed Fields of a Profile
+module ProfileFields
+  extend ActiveSupport::Concern
+
+  included do
+    def ssg_version
+      benchmark.version
+    end
+
+    def policy_type
+      (parent_profile || self).name
+    end
+
+    def major_os_version
+      benchmark&.inferred_os_major_version
+    end
+    alias_method :os_major_version, :major_os_version
+
+    def os_version
+      if os_minor_version.present?
+        "#{os_major_version}.#{os_minor_version}"
+      else
+        os_major_version.to_s
+      end
+    end
+
+    def short_ref_id
+      ref_id.downcase.split(
+        'xccdf_org.ssgproject.content_profile_'
+      )[1] || ref_id
+    end
+
+    def canonical?
+      parent_profile_id.blank?
+    end
+  end
+end
