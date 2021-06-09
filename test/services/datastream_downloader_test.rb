c9e6a6a89c16b367d4287acc1c8dffc67d413550
Viliam Krizan <vkrizan@redhat.com>
2021-06-09 15:26:53 +0200

    feat(ssg): RHICOMPL-1594 downstream datastream downloader
    Intends to download downstream datastream (ds) files from the
    compliance-ssg server from predefined directory/URL structure.

---
+++ test/services/datastream_downloader_test.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+# A class to test importing from a Datastream file
+class DatastreamImporterTest < ActiveSupport::TestCase
+  setup do
+    @ssgs = [
+      SupportedSsg.new(
+        id: 'rhel-7.9-scap-security-guide-0.1.49-13.el7',
+        package: 'scap-security-guide-0.1.49-13.el7',
+        version: '0.1.49',
+        os_major_version: '7',
+        os_minor_version: '9'
+      )
+    ]
+  end
+
+  test 'dowlnoads ssg datastream files' do
+    @ds_file = file_fixture('ssg-rhel7-ds.xml')
+    downloaded = mock
+    downloaded.expects(:path).returns(@ds_file.to_s)
+    downloaded.expects(:close)
+
+    SafeDownloader.expects(:download).returns(downloaded).with do |url|
+      url.include?('/rhel7') && url =~ %r{/ssg-rhel7-ds.xml$}
+    end
+
+    downloader = DatastreamDownloader.new(@ssgs)
+    yielded = 0
+    downloader.download_datastreams do |file|
+      assert @ds_file.size, File.size(file)
+      yielded += 1
+    end
+
+    assert_equal 1, yielded
+  end
+
+  test 'uses default supported ssgs' do
+    assert DatastreamDownloader.new.instance_variable_get('@ssgs')
+  end
+end
