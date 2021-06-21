1b0f7a1be685ee6877c567caa8f89307febe2f81
Dávid Halász <dhalasz@redhat.com>
2021-06-21 10:38:15 +0200

    fix(migration): RHICOMPL-1956 score profiles only when available

--- db/migrate/20210609081621_add_score_to_profiles.rb
+++ db/migrate/20210609081621_add_score_to_profiles.rb
@@ -4,21 +4,23 @@ class AddScoreToProfiles < ActiveRecord::Migration[5.2]
 
     Profile.transaction do
       scores = TestResult.latest.joins(:profile).group(:profile_id).average(:score)
-
       # Profile.canonical(false).where(score: nil).joins(:test_results).distinct.find_each(&:calculate_score!)
+
       # TODO: replace this with upsert after upgrading to Rails 6
-      values = scores.map do |k, v|
-        "(#{ActiveRecord::Base.connection.quote(k)}, #{ActiveRecord::Base.connection.quote(v)})"
-      end.join(', ')
+      if scores.any?
+        values = scores.map do |k, v|
+          "(#{ActiveRecord::Base.connection.quote(k)}, #{ActiveRecord::Base.connection.quote(v)})"
+        end.join(', ')
 
-      query = %{
-        UPDATE profiles P
-        SET score = uv.new_score
-        FROM (VALUES #{values}) AS uv (id, new_score)
-        WHERE P.id = uv.id::uuid
-      }.gsub(/\s+/, " ").strip
+        query = %{
+          UPDATE profiles P
+          SET score = uv.new_score
+          FROM (VALUES #{values}) AS uv (id, new_score)
+          WHERE P.id = uv.id::uuid
+        }.gsub(/\s+/, " ").strip
 
-      ActiveRecord::Base.connection.execute(query)
+        ActiveRecord::Base.connection.execute(query)
+      end
     end
   end
 
