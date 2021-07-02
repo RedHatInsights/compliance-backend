e600fb63556b32ffad0eef26374e138e54296ea2
Viliam Krizan <vkrizan@redhat.com>
2021-07-02 16:59:01 +0200

    fix(puma): require clowder lib in puma config
    This fixes run of puma in devel enviroment.
    The library is likely not preloaded.

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'clowder-common-ruby'
 
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
