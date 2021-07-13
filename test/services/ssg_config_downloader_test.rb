4a25bb5896b90c2447bfc9fe24dc71b24e42e5b0
Dávid Halász <dhalasz@redhat.com>
2021-07-13 14:49:58 +0200

    fix(ssg): RHICOMPL-2030 do not override default SSGs file, use two
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- test/services/ssg_config_downloader_test.rb
+++ test/services/ssg_config_downloader_test.rb
@@ -4,34 +4,66 @@ require 'test_helper'
 
 # A class to test the SsgConfigDownloader service
 class ConfigDownloaderTest < ActiveSupport::TestCase
-  setup do
-    @ds_config_file = File.new('config/supported_ssg.yaml')
+  context 'fallback file' do
+    setup do
+      if File.exist?(SsgConfigDownloader::FILE_PATH)
+        File.delete(SsgConfigDownloader::FILE_PATH)
+      end
+
+      @ds_config_file = File.new(SsgConfigDownloader::FALLBACK_PATH)
+    end
+
+    should 'ssg_ds returns SSG datastream fallback file from disk' do
+      assert_equal @ds_config_file.read, SsgConfigDownloader.ssg_ds
+    end
+
+    should 'ssg_ds_checksum' do
+      assert_equal Digest::MD5.file(@ds_config_file).hexdigest,
+                   SsgConfigDownloader.ssg_ds_checksum
+    end
   end
 
-  test 'ssg_ds returns SSG datastream config file from disk' do
-    assert_equal @ds_config_file.read, SsgConfigDownloader.ssg_ds
-  end
+  context 'downloaded file' do
+    setup do
+      FileUtils.cp(
+        SsgConfigDownloader::FALLBACK_PATH,
+        SsgConfigDownloader::FILE_PATH
+      )
 
-  test 'ssg_ds_checksum' do
-    assert_equal Digest::MD5.file(@ds_config_file).hexdigest,
-                 SsgConfigDownloader.ssg_ds_checksum
-  end
+      @ds_config_file = File.new(SsgConfigDownloader::FILE_PATH)
+    end
 
-  test 'update_ssg_ds success' do
-    SafeDownloader.expects(:download)
+    should 'ssg_ds returns SSG datastream file from disk' do
+      assert_equal @ds_config_file.read, SsgConfigDownloader.ssg_ds
+    end
 
-    SsgConfigDownloader.update_ssg_ds
-    assert_audited 'Downloaded config'
+    should 'ssg_ds_checksum' do
+      assert_equal Digest::MD5.file(@ds_config_file).hexdigest,
+                   SsgConfigDownloader.ssg_ds_checksum
+    end
 
-    assert_equal @ds_config_file.read, SsgConfigDownloader.ssg_ds
-  end
+    should 'update_ssg_ds success' do
+      SafeDownloader.expects(:download)
 
-  test 'update_ssg_ds handles failure gracefully' do
-    SafeDownloader.expects(:download).raises(StandardError)
+      SsgConfigDownloader.update_ssg_ds
+      assert_audited 'Downloaded config'
 
-    SsgConfigDownloader.update_ssg_ds
-    assert_audited 'Failed to download config'
+      assert_equal @ds_config_file.read, SsgConfigDownloader.ssg_ds
+    end
 
-    assert_equal @ds_config_file.read, SsgConfigDownloader.ssg_ds
+    should 'update_ssg_ds handles failure gracefully' do
+      SafeDownloader.expects(:download).raises(StandardError)
+
+      SsgConfigDownloader.update_ssg_ds
+      assert_audited 'Failed to download config'
+
+      assert_equal @ds_config_file.read, SsgConfigDownloader.ssg_ds
+    end
+
+    teardown do
+      if File.exist?(SsgConfigDownloader::FILE_PATH)
+        File.delete(SsgConfigDownloader::FILE_PATH)
+      end
+    end
   end
 end
