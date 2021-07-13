8943ab6109cc7b97d51c66c3ab9d5cee2d0609a2
Dávid Halász <dhalasz@redhat.com>
2021-07-13 14:49:58 +0200

    fix(ssg): RHICOMPL-2030 do not override default SSGs file, use two
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- app/services/ssg_config_downloader.rb
+++ app/services/ssg_config_downloader.rb
@@ -2,20 +2,21 @@
 
 # Service for dowloading SSG configuration files
 class SsgConfigDownloader
-  class << self
-    SSG_DS_FILE = File.new('config/supported_ssg.yaml')
+  FILE_PATH = 'config/supported_ssg.yaml'
+  FALLBACK_PATH = 'config/supported_ssg.default.yaml'
 
+  class << self
     def ssg_ds
-      File.read(SSG_DS_FILE)
+      File.read(ssg_ds_file)
     end
 
     def ssg_ds_checksum
-      Digest::MD5.file(SSG_DS_FILE).hexdigest
+      Digest::MD5.file(ssg_ds_file).hexdigest
     end
 
     def update_ssg_ds
       ssg_content = download(ssg_datastream_config_url)&.read
-      ssg_content && File.open(SSG_DS_FILE, 'w') do |f|
+      ssg_content && File.open(FILE_PATH, 'w') do |f|
         f.write(ssg_content)
       end
     end
@@ -28,6 +29,10 @@ class SsgConfigDownloader
     end
 
     private
+
+    def ssg_ds_file
+      File.new(File.exist?(FILE_PATH) ? FILE_PATH : FALLBACK_PATH)
+    end
 
     def download(url)
       file = SafeDownloader.download(url)
