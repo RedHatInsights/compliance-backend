8cdc196728de3d80a9888cd75068335c0fb6ebbe
Andrew Kofink <akofink@redhat.com>
2021-08-23 15:20:36 -0400

    devel: Build and push the image during pr_check
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- pr_check.sh
+++ pr_check.sh
@@ -2,6 +2,21 @@
 
 set -exv
 
+# Install bonfire repo/initialize
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+
+# --------------------------------------------
+# Options that must be configured by app owner
+# --------------------------------------------
+export APP_NAME="compliance"  # name of app-sre "application" folder this component lives in
+export COMPONENT_NAME="compliance"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+export IMAGE="quay.io/cloudservices/compliance-backend"
+cat /etc/redhat-release
+
+# build the PR commit image
+source $CICD_ROOT/build.sh
+
 # Make directory for artifacts
 mkdir -p artifacts
 
