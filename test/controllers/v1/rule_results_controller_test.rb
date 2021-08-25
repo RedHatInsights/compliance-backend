86b6349fb9615549e3ce69dbbe93abd678e9272a
Dávid Halász <dhalasz@redhat.com>
2021-08-25 15:17:38 +0200

    fix(sorting): RHICOMPL-2180 allow sorting rule results

--- test/controllers/v1/rule_results_controller_test.rb
+++ test/controllers/v1/rule_results_controller_test.rb
@@ -6,18 +6,36 @@ module V1
   class RuleResultsControllerTest < ActionDispatch::IntegrationTest
     setup do
       RuleResultsController.any_instance.expects(:authenticate_user).yields
+      User.current = FactoryBot.create(:user)
     end
 
     test 'index lists all rule results' do
-      User.current = FactoryBot.create(:user)
       FactoryBot.create_list(:rule_result, 10)
+      RuleResultsController.any_instance.expects(:policy_scope)
+                           .with(RuleResult)
+                           .returns(RuleResult.all).at_least_once
+
+      get v1_rule_results_url
+
+      assert_response :success
+    end
+
+    test 'rule results can be sorted' do
+      rr1 = FactoryBot.create(:rule_result, result: 'pass')
+      rr2 = FactoryBot.create(:rule_result, result: 'fail')
 
       RuleResultsController.any_instance.expects(:policy_scope)
                            .with(RuleResult)
                            .returns(RuleResult.all).at_least_once
-      get v1_rule_results_url
 
+      get v1_rule_results_url, params: {
+        sort_by: %w[result]
+      }
       assert_response :success
+
+      rule_results = response.parsed_body['data']
+
+      assert_equal(rule_results.map { |rr| rr['id'] }, [rr2, rr1].map(&:id))
     end
   end
 end
