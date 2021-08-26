f1aa83cda8c5ca7f6b306f1a9163f0b29e52d7fd
Andrew Kofink <akofink@redhat.com>
2021-08-26 14:18:05 -0400

    fix(clowder): Harden clowder-config against nil calls
    Add a bunch of safe navigation to the clowder config
    
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- config/initializers/0_clowder-config.rb
+++ config/initializers/0_clowder-config.rb
@@ -3,41 +3,41 @@ if ClowderCommonRuby::Config.clowder_enabled?
 
   config = ClowderCommonRuby::Config.load
 
-  remediations_config = config.dependency_endpoints.dig('remediations', 'api')
+  remediations_config = config.dependency_endpoints&.dig('remediations', 'api')
   remediations_url = "http://#{remediations_config&.hostname}:#{remediations_config&.port}"
 
   # compliance-ssg
-  compliance_ssg_config = config.private_dependency_endpoints.dig('compliance-ssg', 'service')
-  compliance_ssg_url = "http://#{compliance_ssg_config.hostname}:#{compliance_ssg_config.port}"
+  compliance_ssg_config = config.private_dependency_endpoints&.dig('compliance-ssg', 'service')
+  compliance_ssg_url = "http://#{compliance_ssg_config&.hostname}:#{compliance_ssg_config&.port}"
 
   # RBAC
-  rbac_config = config.dependency_endpoints['rbac']['service']
-  rbac_url = "http://#{rbac_config.hostname}:#{rbac_config.port}"
+  rbac_config = config.dependency_endpoints.dig('rbac', 'service')
+  rbac_url = "http://#{rbac_config&.hostname}:#{rbac_config&.port}"
 
   # Prometheus
-  prometheus_exporter_config = config.private_dependency_endpoints.dig('compliance', 'prometheus-exporter')
+  prometheus_exporter_config = config.private_dependency_endpoints&.dig('compliance', 'prometheus-exporter')
 
   # Inventory
-  host_inventory_config = config.dependency_endpoints['host-inventory']['service']
-  host_inventory_url = "http://#{host_inventory_config.hostname}:#{host_inventory_config.port}"
+  host_inventory_config = config.dependency_endpoints&.dig('host-inventory', 'service')
+  host_inventory_url = "http://#{host_inventory_config&.hostname}:#{host_inventory_config&.port}"
 
   # Redis (in-memory db)
-  redis_url = "#{config.inMemoryDb.hostname}:#{config.inMemoryDb.port}"
+  redis_url = "#{config.dig('inMemoryDb', 'hostname')}:#{config.dig('inMemoryDb', 'port')}"
 
   clowder_config = {
     compliance_ssg_url: compliance_ssg_url,
     kafka: {
-      brokers: config.kafka.brokers.map { |b| "#{b.hostname}:#{b.port}" }.join(','),
+      brokers: config.dig('kafka', 'brokers')&.map { |b| "#{b&.hostname}:#{b&.port}" }.join(','),
       # Not provided by clowder, not sure which of the following should be: [:plaintext, :ssl, :sasl_plaintext, :sasl_ssl]
       security_protocol: 'plaintext'
     },
     kafka_consumer_topics: {
-      inventory_events: config.kafka_topics['platform.inventory.events'].name
+      inventory_events: config.kafka_topics&.dig('platform.inventory.events', 'name')
     },
     kafka_producer_topics: {
-      upload_validation: config.kafka_topics['platform.upload.compliance'].name,
-      payload_tracker: config.kafka_topics['platform.payload-status'].name,
-      remediation_updates: config.kafka_topics['platform.remediation-updates.compliance'].name
+      upload_validation: config.kafka_topics&.dig('platform.upload.compliance', 'name'),
+      payload_tracker: config.kafka_topics&.dig('platform.payload-status', 'name'),
+      remediation_updates: config.kafka_topics&.dig('platform.remediation-updates.compliance', 'name')
     },
     prometheus_exporter_host: prometheus_exporter_config&.hostname,
     prometheus_exporter_port: prometheus_exporter_config&.port,
