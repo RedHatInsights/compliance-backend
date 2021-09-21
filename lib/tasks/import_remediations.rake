f3078bf710a5d37d11a2c21b82d1e26d328ad833
Andrew Kofink <akofink@redhat.com>
2021-09-21 17:03:50 -0400

    fix: RHICOMPL-2147 Use PlaybookDownloader to import_remediations
    This also updates records using 'update_all' instead of update for each
    record to decrease the number of updates to the DB.
    
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- lib/tasks/import_remediations.rake
+++ lib/tasks/import_remediations.rake
@@ -15,11 +15,22 @@ task import_remediations: :environment do
   begin
     start_time = Time.now.utc
     puts "Starting import_remediations job at #{start_time}"
-    RemediationsAPI.new(
-      Account.new(account_number: ENV['JOBS_ACCOUNT_NUMBER'])
-    ).import_remediations
+
+    playbook_status_by_rule_id = PlaybookDownloader.playbooks_exist?(
+      Rule.with_profiles.includes(:benchmark)
+    )
+    ids_with_playbooks = playbook_status_by_rule_id.filter { |_, v| v }.keys
+    ids_sans_playbooks = playbook_status_by_rule_id.filter { |_, v| !v }.keys
+
+    # rubocop:disable Rails/SkipsModelValidations
+    Rule.where(id: ids_sans_playbooks).update_all(remediation_available: false)
+    Rule.where(id: ids_with_playbooks).update_all(remediation_available: true)
+    # rubocop:enable Rails/SkipsModelValidations
+
     end_time = Time.now.utc
     duration = end_time - start_time
+    puts "Updated #{ids_with_playbooks.count} rules with remediations"
+    puts "Updated #{ids_sans_playbooks.count} rules without remediations"
     puts "Finishing import_remediations job at #{end_time} "\
          "and last #{duration} seconds "
   rescue StandardError => e
--- test/tasks/import_remediations_test.rb
+++ test/tasks/import_remediations_test.rb
@@ -4,16 +4,39 @@ require 'test_helper'
 require 'rake'
 
 class ImportRemediationsTest < ActiveSupport::TestCase
-  test 'import_remediations fails on error' do
-    ENV['JOBS_ACCOUNT_NUMBER'] = FactoryBot.create(:account).account_number
+  setup do
+    @rules = FactoryBot.create_list(:rule, 10)
+  end
 
-    RemediationsAPI.any_instance.expects(:import_remediations)
-                   .raises(StandardError)
+  test 'import_remediations fails on error' do
+    PlaybookDownloader.expects(:playbooks_exist?).raises(StandardError)
 
     assert_raises StandardError do
       capture_io do
         Rake::Task['import_remediations'].execute
       end
+    end
+  end
+
+  test 'import_remediations updates all relevant rules' do
+    # rubocop:disable Rails/SkipsModelValidations
+    Rule.where(id: @rules).update_all(remediation_available: false)
+    # rubocop:enable Rails/SkipsModelValidations
+
+    remediation_available = {}
+    @rules.each do |rule|
+      remediation_available[rule.id] = rand(2).zero?
+    end
+
+    PlaybookDownloader.expects(:playbooks_exist?).returns(remediation_available)
+
+    capture_io do
+      Rake::Task['import_remediations'].execute
+    end
+
+    @rules.each do |rule|
+      assert_equal(remediation_available[rule.id],
+                   rule.reload.remediation_available)
     end
   end
 end
