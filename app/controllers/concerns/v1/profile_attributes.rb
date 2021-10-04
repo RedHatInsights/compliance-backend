e6fef68286a73c3de1ae23393da57b8164b62aa5
Marley Stipich <mstipich@redhat.com>
2021-10-04 14:24:01 -0500

    fix: RHICOMPL-1113 Removes name from updatable REST policy attributes

--- app/controllers/concerns/v1/profile_attributes.rb
+++ app/controllers/concerns/v1/profile_attributes.rb
@@ -6,14 +6,18 @@ module V1
     extend ActiveSupport::Concern
 
     included do
-      POLICY_ATTRIBUTES = %i[name description
-                             compliance_threshold business_objective].freeze
+      POLICY_CREATE_ATTRIBUTES = %i[name description
+                                    compliance_threshold
+                                    business_objective].freeze
+      POLICY_UPDATE_ATTRIBUTES = %i[description
+                                    compliance_threshold
+                                    business_objective].freeze
       ALLOWED_CREATE_ATTRIBUTES = %i[parent_profile_id].freeze
 
       private
 
       def policy_create_attributes
-        resource_attributes.to_h.slice(*POLICY_ATTRIBUTES)
+        resource_attributes.to_h.slice(*POLICY_CREATE_ATTRIBUTES)
                            .merge(account_id: current_user.account_id)
                            .tap do |attrs|
           if business_objective
@@ -32,12 +36,17 @@ module V1
       end
 
       def policy_update_attributes
-        resource_attributes.to_h.slice(*POLICY_ATTRIBUTES).tap do |attrs|
+        policy_update_params.to_h.slice(*POLICY_UPDATE_ATTRIBUTES)
+                            .tap do |attrs|
           if business_objective
             attrs.except! :business_objective
             attrs[:business_objective_id] = business_objective.id
           end
         end
+      end
+
+      def policy_update_params
+        resource_params[:attributes]&.permit(*POLICY_UPDATE_ATTRIBUTES)
       end
     end
   end
