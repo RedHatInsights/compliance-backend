803ed2d35c65a3a6af673079f1f9e25bd9dc6c02
Andrew Kofink <akofink@redhat.com>
2021-10-18 15:50:11 -0400

    feat(GraphQL): RHICOMPL-2378 Add tags to the system connection
    This allows getting all tags for the given host filter via the GraphQL
    system connection. This builds on RHICOMPL-866/#1027
    
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

--- app/graphql/connections/system_connection.rb
+++ app/graphql/connections/system_connection.rb
@@ -4,9 +4,14 @@ module Connections
   # Contains fields and methods related with system connection fields
   class SystemConnection < BaseConnection
     field :os_versions, [::Types::OperatingSystem], null: false
+    field :tags, [::Types::Tag], null: false
 
     def os_versions
       object.nodes.limit(nil).available_os_versions
+    end
+
+    def tags
+      object.nodes.limit(nil).available_tags
     end
   end
 end
