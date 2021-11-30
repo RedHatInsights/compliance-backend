f9fadd1faf51425156d693a16c358c24b825095c
Dávid Halász <dhalasz@redhat.com>
2021-11-30 11:07:30 +0100

    fix(racecar): RHICOMPL-2496 explicit symbolization for config

--- config/racecar.rb
+++ config/racecar.rb
@@ -3,9 +3,9 @@ Racecar.configure do |config|
   config.group_id_prefix = 'compliance'
 
   if Settings.kafka.security_protocol
-    config.security_protocol = Settings.kafka.security_protocol
+    config.security_protocol = Settings.kafka.security_protocol.try(:to_sym)
 
-    if config.security_protocol == 'sasl_ssl'
+    if config.security_protocol == :sasl_ssl
       config.ssl_ca_location = Settings.kafka.ssl_ca_location
       config.sasl_username = Settings.kafka.sasl_username
       config.sasl_password = Settings.kafka.sasl_password
