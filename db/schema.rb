e55d8f36f92c7a34c611dbdd470d7a2c0c8b320b
Dávid Halász <dhalasz@redhat.com>
2022-01-03 14:19:16 +0100

    fix(migrations): reset revisions again for force import

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_11_074855) do
+ActiveRecord::Schema.define(version: 2022_01_03_131739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "dblink"
