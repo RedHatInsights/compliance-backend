e0794cbe97d6bb974de10ac79524677400fe92aa
Roman Blanco <rblanco@redhat.com>
2022-02-09 11:11:15 +0000

    chore(config): RHICOMPL-2704 Replacing CODECOV_TOKEN by CI
    Replacing dependency on CODECOV_TOKEN by CI environment variable

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
-  if ENV['CODECOV_TOKEN']
+  if ENV['CI']
     config.eager_load = true
   else
     config.eager_load = false
