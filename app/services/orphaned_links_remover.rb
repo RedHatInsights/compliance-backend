6372b97d04753d608442b3c5af32e73ea35c506a
Dávid Halász <dhalasz@redhat.com>
2022-02-14 13:45:38 +0100

    chore(db): RHICOMPL-2720 clean up orphaned profile/rule/result links

---
+++ app/services/orphaned_links_remover.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Service for removing orphaned links
+class OrphanedLinksRemover
+  class << self
+    # rubocop:disable Metrics/AbcSize
+    def run!
+      ProfileRule.left_outer_joins(:profile).left_outer_joins(:rule).where(profiles: { id: nil }).or(
+        ProfileRule.where(rules: { id: nil })
+      ).destroy_all
+
+      RuleResult.left_outer_joins(:rule).left_outer_joins(:host).where(rules: { id: nil }).or(
+        RuleResult.where('inventory.hosts': { id: nil })
+      ).destroy_all
+
+      TestResult.left_outer_joins(:profile).left_outer_joins(:host).where(profiles: { id: nil }).or(
+        RuleResult.where('inventory.hosts': { id: nil })
+      ).destroy_all
+    end
+    # rubocop:enable Metrics/AbcSize
+  end
+end
