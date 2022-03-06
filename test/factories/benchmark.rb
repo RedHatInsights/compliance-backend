6fd6b847bdcc13e60187f459b7a930b37f766591
Dávid Halász <dhalasz@redhat.com>
2022-03-06 08:13:41 +0100

    chore(tests): do not create 0.y.z benchmarks using default factory

--- test/factories/benchmark.rb
+++ test/factories/benchmark.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     title { Faker::Lorem.sentence }
     description { Faker::Lorem.paragraph }
     ref_id { "xccdf_org.ssgproject.content_benchmark_RHEL-#{os_major_version}" }
-    sequence(:version) { |n| "0.#{(n / 50).floor}.#{n % 50}" }
+    sequence(:version) { |n| "100.#{(n / 50).floor}.#{n % 50}" }
 
     transient do
       os_major_version { '7' }
