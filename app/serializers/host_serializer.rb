41fc3405d0c0a168969a44774db879e149855679
Dávid Halász <dhalasz@redhat.com>
2022-03-10 17:55:40 +0100

    feat(API): RHICOMPL-2306 expose updated field for hosts in GQL+REST

--- app/serializers/host_serializer.rb
+++ app/serializers/host_serializer.rb
@@ -5,7 +5,7 @@ class HostSerializer < ApplicationSerializer
   set_type :host
   attributes :name, :os_major_version, :os_minor_version, :last_scanned,
              :rules_passed, :rules_failed, :has_policy, :culled_timestamp,
-             :stale_timestamp, :stale_warning_timestamp, :insights_id
+             :stale_timestamp, :stale_warning_timestamp, :updated, :insights_id
   attribute :compliant do |host|
     host.compliant.values.all?
   end
