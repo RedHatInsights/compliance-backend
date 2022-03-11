548e9e63d95471d99c33dfac40275062ebc8552b
Victor M <vmugicag@redhat.com>
2022-03-11 16:13:33 +0100

    patch(RHICOMPL-2815): Patch init container command
    - Moves init container commands into separate scripts
    - Adds active wait loops to all initial containers, with configurable
      timeout before crashing
    - Refactor of some liveness probes
    - Reduced deployment timeout value to 10 minutes

---
+++ scripts/abort_if_pending_migrations.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+MAX_INIT_TIMEOUT_SECONDS=${MAX_INIT_TIMEOUT_SECONDS:-120}
+
+for ((i=1;i<=$MAX_INIT_TIMEOUT_SECONDS;i++)); do
+    if bundle exec rake --trace db:abort_if_pending_migrations; then
+        exit 0
+    else
+        sleep 1
+    fi
+done
+
+exit 1
