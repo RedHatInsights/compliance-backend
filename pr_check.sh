2128dbb81e37133ff212578e48a321ee15a29dba
vmugicag <vmugicag@redhat.com>
2022-04-22 00:09:19 +0200

    checking sh command

--- pr_check.sh
+++ pr_check.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+which sh
+ls -lrt $(which sh)
+sh --version
+
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
