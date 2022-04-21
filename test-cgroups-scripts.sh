03ec84204377459c1a820cd8be5986eaf99e75b7
Victor M <vmugicag@redhat.com>
2022-04-21 20:34:09 +0200

    refactor

--- test-cgroups-scripts.sh
+++ test-cgroups-scripts.sh
@@ -2,9 +2,21 @@
 
 COMPLIANCE_IMAGE_TAG="${1:-29ac074}"
 
-expected_output=$(podman run --rm  quay.io/cloudservices/compliance-backend:${COMPLIANCE_IMAGE_TAG} "cgroup-limits" 2>/dev/null)
-actual_output=$(podman run --rm  quay.io/cloudservices/compliance-backend:${COMPLIANCE_IMAGE_TAG} /bin/bash -c 'echo $(env -i bash -c "scripts/set_cgroup_limits.sh  2>/dev/null && env")')
 
-formatted_actual_output=$(sed -e 's/\s/\n/g' <<<"$actual_output" | sed -e '/^\(PWD\|SHLVL\|_\)=.*$/d')
+
+_check_command_is_present() {
+    command -v $1 >/dev/null
+}
+
+if _check_command_is_present "docker"; then
+    CONTAINER_ENGINE="docker"
+else
+    CONTAINER_ENGINE="podman"
+fi
+
+expected_output=$("$CONTAINER_ENGINE" run --rm  quay.io/cloudservices/compliance-backend:${COMPLIANCE_IMAGE_TAG} "cgroup-limits" 2>/dev/null | sort -u)
+actual_output=$("$CONTAINER_ENGINE" run --rm  quay.io/cloudservices/compliance-backend:${COMPLIANCE_IMAGE_TAG} /bin/bash -c 'echo $(env -i bash -c "scripts/set_cgroup_limits.sh  2>/dev/null && env")')
+
+formatted_actual_output=$(sed -e 's/\s/\n/g' <<<"$actual_output" | sed -e '/^\(PWD\|SHLVL\|_\)=.*$/d' | sort -u)
 
 diff <(echo "$expected_output") <(echo "$formatted_actual_output")
