e548980246a8f2ec4fbb721243641cf5bdaecda0
Victor M <vmugicag@redhat.com>
2022-04-22 17:35:05 +0200

    Using pod template, first draft

--- run-e2e-cgroup-limits-test.sh
+++ run-e2e-cgroup-limits-test.sh
@@ -2,34 +2,34 @@
 
 TARGET_IMAGE_TAG=${1:-latest}
 
-REFERENCE_IMAGE='registry.access.redhat.com/ubi8/ruby-30'
+POD_TEMPLATE='./custom-template.yaml'
 TARGET_IMAGE="quay.io/cloudservices/compliance-backend:${TARGET_IMAGE_TAG}"
 REFERENCE_POD_NAME='reference'
 TARGET_POD_NAME='target'
+POD_NAME='e2e-test-for-compliance-cgroups-limit-script'
 TEARDOWN_RAN=0
+DIFF_STATUS=1
 EXIT_CODE=0
 
 function teardown {
 
-    oc delete pod --now "$REFERENCE_POD_NAME" "$TARGET_POD_NAME" --namespace "$TEST_NAMESPACE"
-    bonfire namespace release "$TEST_NAMESPACE"
+    oc delete pod --now "$POD_NAME" --namespace "$TEST_NAMESPACE"
+#    bonfire namespace release "$TEST_NAMESPACE"
 
     exit $EXIT_CODE
 }
 
-trap teardown EXIT ERR SIGINT SIGTERM
+trap teardown EXIT SIGINT SIGTERM
 
-set -e
+#TEST_NAMESPACE=$(bonfire namespace reserve)
+TEST_NAMESPACE='ephemeral-62w6lz'
 
-TEST_NAMESPACE=$(bonfire namespace reserve)
+oc create -f "$POD_TEMPLATE" --namespace "$TEST_NAMESPACE"
+oc wait --for=condition=Ready --namespace "$TEST_NAMESPACE" "pod/${POD_NAME}" 
 
-oc run "$REFERENCE_POD_NAME" --namespace "$TEST_NAMESPACE" --image "$REFERENCE_IMAGE" --command -- "/bin/bash" "-c" "cgroup-limits 2>/dev/null && sleep infinity"
-oc run "$TARGET_POD_NAME" --namespace "$TEST_NAMESPACE" --image "$TARGET_IMAGE" --command -- "/bin/bash" "-c" 'echo $(env -i bash -c "scripts/set_cgroup_limits.sh  2>/dev/null && env") && sleep infinity' 
-oc wait --for=condition=Ready --namespace "$TEST_NAMESPACE" "pod/${REFERENCE_POD_NAME}" 
-oc wait --for=condition=Ready --namespace "$TEST_NAMESPACE" "pod/${TARGET_POD_NAME}" 
-EXPECTED_OUTPUT=$(oc logs --namespace "$TEST_NAMESPACE" "$REFERENCE_POD_NAME" | sort -u) 
-ACTUAL_OUTPUT=$(oc logs --namespace "$TEST_NAMESPACE" "$TARGET_POD_NAME")
-FORMATTED_ACTUAL_OUTPUT=$(sed -e 's/\s/\n/g' <<<"$ACTUAL_OUTPUT" | sed -e '/^\(PWD\|SHLVL\|_\)=.*$/d' | sort -u)
+EXPECTED_OUTPUT=$(oc logs --namespace "$TEST_NAMESPACE" "$POD_NAME" -c 'reference' | sort) 
+ACTUAL_OUTPUT=$(oc logs --namespace "$TEST_NAMESPACE" "$POD_NAME" -c 'target')
+FORMATTED_ACTUAL_OUTPUT=$(sed -e 's/\s/\n/g' <<<"$ACTUAL_OUTPUT" | sed -e '/^\(PWD\|SHLVL\|_\)=.*$/d' | sort)
 
 diff -q <(echo "$EXPECTED_OUTPUT") <(echo "$FORMATTED_ACTUAL_OUTPUT") 
 
