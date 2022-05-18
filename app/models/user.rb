df766019b0277f77ff36243bea13dabe99d44cea
Dávid Halász <dhalasz@redhat.com>
2022-05-18 11:31:08 +0200

    cleanup(model): drop dead code from the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,27 +30,5 @@ class User < ApplicationRecord
     def current=(user)
       Thread.current[:user] = user
     end
-
-    # rubocop:disable Metrics/AbcSize
-    # rubocop:disable Metrics/MethodLength
-    def from_x_rh_identity(identity)
-      new(
-        account: Account.find_by(
-          account_number: identity['account_number']
-        ),
-        # redhat_id: identity['id'] || identity['user_id'],
-        redhat_org_id: (identity['internal']['org_id'] if identity['internal']),
-        email: identity['user']['email'],
-        first_name: identity['user']['first_name'],
-        last_name: identity['user']['last_name'],
-        active: identity['user']['is_active'],
-        org_admin: identity['user']['is_org_admin'],
-        locale: identity['user']['locale'],
-        username: identity['user']['username'],
-        internal: identity['user']['is_internal']
-      )
-    end
-    # rubocop:enable Metrics/AbcSize
-    # rubocop:enable Metrics/MethodLength
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,32 +8,6 @@ class UserTest < ActiveSupport::TestCase
   should validate_presence_of :username
   should belong_to :account
 
-  test 'can be created from a X-RH-IDENTITY JSON' do
-    FactoryBot.create(:account, account_number: '1333331')
-
-    user = User.from_x_rh_identity(
-      JSON.parse(
-        <<~X_RH_IDENTITY
-          {
-            "account_number":"1333331",
-            "type": "User",
-            "user":  {
-              "username":"foobar@redhat.com",
-              "email":"foobar@redhat.com",
-              "first_name":"Foo",
-              "last_name":"Bar",
-              "locale":"en_US"
-            },
-            "internal": {
-              "org_id": "29329"
-            }
-          }
-        X_RH_IDENTITY
-      )
-    )
-    assert user.valid?
-  end
-
   test 'can test RBAC resources authorization' do
     account = FactoryBot.create(:account)
     current_user = FactoryBot.create(:user, account: account)
