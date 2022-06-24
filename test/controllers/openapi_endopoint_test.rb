936cb8b1a25888d34a1f48d7c21f1ee0aa68fe40
Roman Blanco <rblanco@redhat.com>
2022-06-24 13:34:24 +0000

    fix(RBAC): RHICOMPL-3053 fix access key constant

---
+++ test/controllers/openapi_endopoint_test.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class OpenapiEndpointTest < ActionDispatch::IntegrationTest
+  setup do
+    stub_rbac_permissions(Rbac::COMPLIANCE_VIEWER, Rbac::INVENTORY_VIEWER)
+  end
+
+  test 'openapi success' do
+    account = FactoryBot.create(:account)
+    encoded_header = Base64.encode64(
+      {
+        'identity': {
+          'account_number': account.account_number,
+          'auth_type': 'basic-auth',
+          'org_id': account.org_id
+        },
+        'entitlements':
+        {
+          'insights': {
+            'is_entitled': true
+          }
+        }
+      }.to_json
+    )
+    get '/api/compliance/openapi.json', headers: { 'X-RH-IDENTITY': encoded_header }
+    assert_response :success
+  end
+end
