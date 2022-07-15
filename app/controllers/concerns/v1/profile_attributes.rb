1604c4ba3def0c53470c3c0f1b4218bbd117aedb
Dávid Halász <dhalasz@redhat.com>
2022-07-15 14:41:33 +0200

    refactor(Profiles): RHICOMPL-478 contextualize params handling

--- app/controllers/concerns/v1/profile_attributes.rb
+++ app/controllers/concerns/v1/profile_attributes.rb
@@ -5,6 +5,8 @@ module V1
   module ProfileAttributes
     extend ActiveSupport::Concern
 
+    ParamType = ActionController::Parameters
+
     included do
       POLICY_CREATE_ATTRIBUTES = %i[name description
                                     compliance_threshold
@@ -13,6 +15,11 @@ module V1
                                     compliance_threshold
                                     business_objective].freeze
       ALLOWED_CREATE_ATTRIBUTES = %i[parent_profile_id].freeze
+
+      RELATIONSHIP_TYPE = ParamType.map(
+        id: ParamType.string,
+        type: ParamType.string
+      )
 
       private
 
@@ -47,6 +54,41 @@ module V1
 
       def policy_update_params
         resource_params[:attributes]&.permit(*POLICY_UPDATE_ATTRIBUTES)
+      end
+
+      def resource_params
+        params.permit(data: ParamType.map(
+          attributes: ParamType.map,
+          relationships: ParamType.map
+        ))
+        params.require(:data).permit(attributes: {}, relationships: {})
+      end
+
+      def resource_attributes
+        resource_params[:attributes]&.permit(ProfileSerializer.attributes_to_serialize.keys)
+      end
+
+      def resource_relationships
+        resource_params[:relationships]&.permit(relationship_types)
+      end
+
+      def new_relationship_ids(model)
+        relationship = model.to_s.pluralize.downcase.to_sym
+        ids = resource_relationships.to_h.dig(relationship, :data)&.map do |res|
+          res[:id]
+        end
+
+        # Pundit implicitly returns with an empty array when the input is nil.
+        # This is unwanted as it would unassign all the systems...
+        ::Pundit.policy_scope(current_user, model).where(id: ids).pluck(:id) unless ids.nil?
+      end
+
+      def relationship_types
+        ProfileSerializer.relationships_to_serialize.keys.each_with_object({}) do |relationship, h|
+          h[relationship] = ParamType.map(
+            data: ParamType.array(RELATIONSHIP_TYPE) | RELATIONSHIP_TYPE
+          )
+        end
       end
     end
   end
