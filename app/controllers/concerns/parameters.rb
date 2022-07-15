a0ff42a1fa75ef0b5a42743cfe218ccd4e57689e
Dávid Halász <dhalasz@redhat.com>
2022-07-15 14:41:33 +0200

    refactor(Profiles): RHICOMPL-478 contextualize params handling

--- app/controllers/concerns/parameters.rb
+++ app/controllers/concerns/parameters.rb
@@ -6,59 +6,16 @@ module Parameters
 
   ParamType = ActionController::Parameters
 
-  RELATIONSHIP_TYPE = ParamType.map(
-    id: ParamType.string,
-    type: ParamType.string
-  )
-
   included do
+    private
+
     def relationships_enabled?
       params.permit(relationships: ParamType.boolean)
             .with_defaults(relationships: true)[:relationships]
     end
 
-    def resource_params
-      params.permit(data: ParamType.map(
-        attributes: ParamType.map,
-        relationships: ParamType.map
-      ))
-      params.require(:data).permit(attributes: {}, relationships: {})
-    end
-
-    def resource_attributes
-      resource_params[:attributes]&.permit(
-        serializer.attributes_to_serialize.keys
-      )
-    end
-
-    def resource_relationships
-      resource_params[:relationships]&.permit(relationship_types)
-    end
-
-    def new_relationship_ids(model)
-      relationship = model.to_s.pluralize.downcase.to_sym
-      ids = resource_relationships.to_h.dig(relationship, :data)&.map do |res|
-        res[:id]
-      end
-
-      # Pundit implicitly returns with an empty array when the input is nil.
-      # This is unwanted as it would unassign all the systems...
-      ::Pundit.policy_scope(current_user, model).where(id: ids).pluck(:id) unless ids.nil?
-    end
-
     def include_params
       params.permit(include: ParamType.regexp(/^[a-zA-Z0-9,_]*$/))[:include]
-    end
-
-    private
-
-    def relationship_types
-      serializer.relationships_to_serialize.keys
-                .each_with_object({}) do |relationship, h|
-        h[relationship] = ParamType.map(
-          data: ParamType.array(RELATIONSHIP_TYPE) | RELATIONSHIP_TYPE
-        )
-      end
     end
   end
 end
