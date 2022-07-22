79894697764c010e05aaa7f2b9ec550b5a9be2ba
Dávid Halász <dhalasz@redhat.com>
2022-07-22 17:03:06 +0200

    fix(REST): RHICOMPL-3124 remove branch_id parameter coming from Sat

--- lib/satellite_compensation/middleware.rb
+++ lib/satellite_compensation/middleware.rb
@@ -5,17 +5,21 @@ module Insights
   module API
     module Common
       module SatelliteCompensation
-        # Rack middleware to set content-type for Satellite-forwarded requests
+        # Rack middleware to compensate issues in Satellite-forwarded requests
         class Middleware
           def initialize(app)
             @app = app
           end
 
           def call(env)
-            # Sometimes Satellite forwards the client requests with an empty string
-            # as content-type and Rails does not like it.
-            if env['HTTP_USER_AGENT'] =~ /foreman/i && env['CONTENT_TYPE'] == ''
-              env['CONTENT_TYPE'] = nil
+            if env['HTTP_USER_AGENT'] =~ /foreman/i
+              # Sometimes Satellite forwards the client requests with an empty string
+              # as content-type and Rails does not like it.
+              env['CONTENT_TYPE'] = nil if env['CONTENT_TYPE'] == ''
+
+              # There is an additional branch_id parameter coming from Satellite that
+              # fails on the stricter checking of params in our REST API.
+              Rack::Request.new(env).delete_param('branch_id')
             end
 
             @app.call(env)
