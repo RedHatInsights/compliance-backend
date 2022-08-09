c717a4386e34069a9d57098ca6c468b7c2f7cca1
Dávid Halász <dhalasz@redhat.com>
2022-08-09 11:42:54 +0200

    fix(migrations): comment out some org_id related migrations

--- db/migrate/20211104070445_delete_upstream_profiles.rb
+++ db/migrate/20211104070445_delete_upstream_profiles.rb
@@ -1,7 +1,7 @@
 class DeleteUpstreamProfiles < ActiveRecord::Migration[5.2]
   def up
     UpstreamProfileRemover.run!
-    DanglingAccountRemover.run!
+    # DanglingAccountRemover.run!
   end
 
   def down
