b06e80441c2ebe3557f60b49b8ebf77e9c14d3d3
Dávid Halász <dhalasz@redhat.com>
2022-08-11 10:58:01 +0200

    fix(GraphQL): RHICOMPL-3036 move and adjust the SystemConnection

--- app/graphql/types/system_connection.rb
+++ app/graphql/types/system_connection.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-module Connections
+module Types
   # Contains fields and methods related with system connection fields
   class SystemConnection < BaseConnection
     field :os_versions, [::Types::OperatingSystem], null: false
     field :tags, [::Types::Tag], null: false
 
     def os_versions
-      object.nodes.limit(nil).available_os_versions
+      object.items.limit(nil).available_os_versions
     end
 
     def tags
-      object.nodes.limit(nil).available_tags
+      object.items.limit(nil).available_tags
     end
   end
 end
