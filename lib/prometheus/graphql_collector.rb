a54cf487a1181fa4f6bd5caac2bf0217a8c27e7c
Dávid Halász <dhalasz@redhat.com>
2022-08-17 16:05:11 +0200

    cleanup(metrics): RHICOMPL-3219 drop not working collectors

--- lib/prometheus/graphql_collector.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'graphql/tracing'
-require 'graphql/tracing/prometheus_tracing/graphql_collector'
-
-class GraphQLCollector < GraphQL::Tracing::PrometheusTracing::GraphQLCollector
-end
