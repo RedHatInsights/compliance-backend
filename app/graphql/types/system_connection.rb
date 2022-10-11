225d2f5cbedbe0e05d917fdbf4d68663a52f2b76
Dávid Halász <dhalasz@redhat.com>
2022-10-11 14:10:03 +0200

    cleanup(GraphQL): RHICOMPL-3339 drop unused systemConnection.tags

--- app/graphql/types/system_connection.rb
+++ app/graphql/types/system_connection.rb
@@ -4,14 +4,9 @@ module Types
   # Contains fields and methods related with system connection fields
   class SystemConnection < BaseConnection
     field :os_versions, GraphQL::Types::JSON, null: false
-    field :tags, GraphQL::Types::JSON, null: false
 
     def os_versions
       object.items.limit(nil).available_os_versions
-    end
-
-    def tags
-      context[:wide_scope].available_tags
     end
   end
 end
