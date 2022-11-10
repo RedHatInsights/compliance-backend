4701a4d2469493410903fc8263bb6f7482273004
Victor M <vmugicag@redhat.com>
2022-11-10 14:06:52 +0100

    refactor(build): amend container login variable name

--- build_deploy.sh
+++ build_deploy.sh
@@ -6,6 +6,5 @@ source 'scripts/deploy/build-deploy-common.sh'
 
 IMAGE_NAME="${IMAGE_NAME:-quay.io/cloudservices/compliance-backend}"
 ADDITIONAL_TAGS="latest"
-#PREFER_CONTAINER_ENGINE="docker"
 
 build_deploy_main || exit 1
