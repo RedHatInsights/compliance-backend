b294745f530f2d8faf501cb2c5efe8b3930d61f6
Dávid Halász <dhalasz@redhat.com>
2022-11-24 08:43:06 +0100

    fix(Notifications): RHICOMPL-3442 adjust the slack channel name

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -23,7 +23,7 @@ ExceptionNotification.configure do |config|
 
   config.add_notifier :slack, {
     webhook_url: Settings.slack_webhook[%r{^https://.*$}],
-    channel: '#compliance-alarms',
+    channel: '#team-clouddot-compliance-alarms',
     additional_parameters: {
       mrkdwn: true
     }
