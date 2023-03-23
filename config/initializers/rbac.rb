2ec91b94b138bd0d8dcdd95890ab5337f0b300b3
Dávid Halász <dhalasz@redhat.com>
2023-03-23 12:49:09 +0100

    fix(RBAC): RHINENG-101 set the URL scheme for RBAC requests

--- config/initializers/rbac.rb
+++ config/initializers/rbac.rb
@@ -1,5 +1,6 @@
 RBACApiClient.configure do |config|
   config.host = Settings.rbac_url
+  config.scheme = Settings.rbac_url.split('://').first
   if Settings.platform_basic_auth_username.present?
     config.username = Settings.platform_basic_auth_username
     config.password = Settings.platform_basic_auth_password
