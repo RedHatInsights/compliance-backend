777efa84944e3ddbecc5a098bdc5ae8beb5aeaaf
Dávid Halász <dhalasz@redhat.com>
2023-05-03 08:54:54 +0200

    fix(Logging): RHICOMPL-3738 limit tagged logging into blocks

--- lib/logger_with_audit.rb
+++ lib/logger_with_audit.rb
@@ -6,11 +6,15 @@ module Insights
       # Additional helper methods for audit logging
       class LoggerWithAudit < ActiveSupport::Logger
         def audit_success(message)
-          tagged('AUDIT - SUCCESS').info(message)
+          tagged('AUDIT - SUCCESS') do |logger|
+            logger.info(message)
+          end
         end
 
         def audit_fail(message)
-          tagged('AUDIT - FAIL').info(message)
+          tagged('AUDIT - FAIL') do |logger|
+            logger.info(message)
+          end
         end
       end
     end
