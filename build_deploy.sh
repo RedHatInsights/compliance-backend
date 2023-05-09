e4c7c4bd35d010f00e1be0ddbb2a603898d31123
Victor M <vmugicag@redhat.com>
2023-05-09 16:04:59 +0200

    Add additional tag for security

--- build_deploy.sh
+++ build_deploy.sh
@@ -5,6 +5,8 @@ set -exv
 source 'scripts/deploy/build-deploy-common.sh'
 
 IMAGE_NAME="${IMAGE_NAME:-quay.io/cloudservices/compliance-backend}"
-ADDITIONAL_TAGS="latest"
+SECURITY_COMPLIANCE_TAG="sc-$(date +%Y%m%d)"
+ADDITIONAL_TAGS="$SECURITY_COMPLIANCE_TAG"
+REQUIRED_REGISTRIES_LOCAL=''
 
 build_deploy_main || exit 1
