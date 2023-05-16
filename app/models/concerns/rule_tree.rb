8d0fa1ee560006a4ad07729cb670f723b524f595
Dávid Halász <dhalasz@redhat.com>
2023-05-16 16:10:50 +0200

    perf(API): RHICOMPL-3754 optimize and minimalize RuleTree

--- app/models/concerns/rule_tree.rb
+++ app/models/concerns/rule_tree.rb
@@ -4,17 +4,19 @@
 module RuleTree
   extend ActiveSupport::Concern
 
-  RULE_ATTRIBUTES = %i[id ref_id title description rationale identifier references severity values precedence].freeze
+  RULE_ATTRIBUTES = %i[id ref_id].freeze
 
-  RULE_GROUP_ATTRIBUTES = %i[id ref_id title description rationale].freeze
+  RULE_GROUP_ATTRIBUTES = %i[id ref_id title].freeze
 
   included do
     def rule_tree(graphql = false)
-      rule_groups.includes(:rules_with_references).arrange_serializable do |group, children|
+      cached_rules = rules.select(*RULE_ATTRIBUTES, :rule_group_id).group_by(&:rule_group_id)
+
+      rule_groups.arrange_serializable do |group, children|
         serialize(group, RULE_GROUP_ATTRIBUTES, graphql).merge(
-          children: children + group.rules_with_references.map do |rule|
+          children: children + (cached_rules[group.id]&.map do |rule|
             serialize(rule, RULE_ATTRIBUTES, graphql)
-          end
+          end || [])
         )
       end
     end
