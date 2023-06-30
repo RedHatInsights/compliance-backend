cac196314e2b8053a4e8c95c077af3524cfe4e08
Dávid Halász <dhalasz@redhat.com>
2023-06-30 13:58:18 +0200

    feat(CICD): RHICOMPL-3877 set IMAGE_TAG label on the first stage

--- build_deploy.sh
+++ build_deploy.sh
@@ -7,5 +7,6 @@ source 'scripts/deploy/build-deploy-common.sh'
 IMAGE_NAME="${IMAGE_NAME:-quay.io/cloudservices/compliance-backend}"
 ADDITIONAL_TAGS="latest"
 REQUIRED_REGISTRIES_LOCAL=''
+BUILD_ARGS=("IMAGE_TAG")
 
 build_deploy_main || exit 1
