7664c1b9e753f02e8b7b4ec35b9a9354e2f0b7ed
Dávid Halász <dhalasz@redhat.com>
2023-07-25 09:52:19 +0200

    refactor(RBAC): RHICOMPL-3851 extract groups into the RBAC service

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,33 +18,10 @@ class User < ApplicationRecord
   end
 
   def inventory_groups
-    rbac_permissions.each_with_object([]) do |permission, ids|
-      next unless Rbac.verify(permission.permission, Rbac::INVENTORY_HOSTS_READ)
-      # Empty array on 'resource_definitions' symbolizes a global access to the permitted resource.
-      # In such case, the method returns Rbac::ANY and skips parsing of attributeFilter.
-      return Rbac::ANY if permission.resource_definitions == []
-
-      permission.resource_definitions.each do |filter|
-        next unless valid_inventory_groups_definition?(filter[:attributeFilter])
-
-        ids.append(*inventory_groups_definition_value(filter[:attributeFilter]))
-      end
-    end
+    @inventory_groups ||= Rbac.load_inventory_groups(rbac_permissions)
   end
 
   private
-
-  def valid_inventory_groups_definition?(definition)
-    definition[:value].instance_of?(Array) &&
-      definition[:operation] == 'in' &&
-      definition[:key] == 'group.id'
-  end
-
-  def inventory_groups_definition_value(definition)
-    # Received '[nil]' symbolizes access to ungrouped entries.
-    # In output represtented with an empty array.
-    definition[:value].map { |dv| dv || [] }
-  end
 
   def rbac_permissions
     @rbac_permissions ||= Rbac.load_user_permissions(account.identity_header.raw)
