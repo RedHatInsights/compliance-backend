ecc0d5c62f684c2d6d363341e0bf5c067655f6b6
Dávid Halász <dhalasz@redhat.com>
2023-08-03 13:08:37 +0200

    fix(DB): RHICOMPL-3930 rename name to title in canonical_profiles

---
+++ db/views/canonical_profiles_v02.sql
@@ -0,0 +1,11 @@
+SELECT
+  "profiles"."id",
+  "profiles"."name" "title",
+  "profiles"."ref_id",
+  "profiles"."created_at",
+  "profiles"."updated_at",
+  "profiles"."description",
+  "profiles"."benchmark_id",
+  "profiles"."upstream",
+  "profiles"."value_overrides"
+FROM "profiles" WHERE "profiles"."parent_profile_id" IS NULL;
