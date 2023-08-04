c2bc373a3d6d7690f88a625a3751c09052f5f80e
Dávid Halász <dhalasz@redhat.com>
2023-08-04 07:30:42 +0200

    fix(CI): RHICOMPL-3933 unify codecov configuration

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,24 +4,10 @@ require 'rails'
 
 unless Rails.env.production?
   require 'simplecov'
-  SimpleCov.start do
-    add_filter 'config'
-    add_filter 'db'
-    add_filter 'spec'
-    add_filter 'test'
-
-    add_group 'Consumers', 'app/consumers'
-    add_group 'Controllers', 'app/controllers'
-    add_group 'GraphQL', 'app/graphql'
-    add_group 'Jobs', 'app/jobs'
-    add_group 'Models', 'app/models'
-    add_group 'Policies', 'app/policies'
-    add_group 'Serializers', 'app/serializers'
-    add_group 'Services', 'app/services'
-  end
 
   if ENV['GITHUB_ACTIONS']
     require 'simplecov-cobertura'
+    SimpleCov.command_name 'spec'
     SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
   end
 
