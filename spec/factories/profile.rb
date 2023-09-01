cbdefd7f2d459d5acb34203844505689056d9cad
Dávid Halász <dhalasz@redhat.com>
2023-09-01 23:24:55 +0200

    fix(factories): do not preload models when loading factories

--- spec/factories/profile.rb
+++ spec/factories/profile.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :v2_profile, class: V2::Profile do
+  factory :v2_profile, class: 'V2::Profile' do
     id { SecureRandom.uuid }
     title { Faker::Lorem.sentence }
     description { Faker::Lorem.paragraph }
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :v2_user, class: User do
+  factory :v2_user, class: 'User' do
     sequence(:username) { |n| "testuser_#{n}@redhat.com" }
     association :account, factory: :v2_account
   end
