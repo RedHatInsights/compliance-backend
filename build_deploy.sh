eeb03e4a16c570da67a621ea22f823a5d3b060b7
Casey Williams <caswilli@redhat.com>
2023-10-03 15:39:50 -0400

    chore(build_deploy): adding Security-Compliance Branch Image Tagging Support

--- build_deploy.sh
+++ build_deploy.sh
@@ -5,7 +5,19 @@ set -exv
 source 'scripts/deploy/build-deploy-common.sh'
 
 IMAGE_NAME="${IMAGE_NAME:-quay.io/cloudservices/compliance-backend}"
-ADDITIONAL_TAGS="latest"
+
+# Check if the current Git branch is 'origin/security-compliance'.
+if [[ "$GIT_BRANCH" == "origin/security-compliance" ]]; then
+    # Generate a tag for the Docker image based on the current date and Git commit short hash.
+    SECURITY_COMPLIANCE_TAG="sc-$(date +%Y%m%d)-$(git rev-parse --short=7 HEAD)"
+    
+    # Set ADDITIONAL_TAGS to the generated security compliance tag.
+    ADDITIONAL_TAGS="$SECURITY_COMPLIANCE_TAG"
+else
+    # If the current Git branch is not 'origin/security-compliance':
+    ADDITIONAL_TAGS="latest"
+fi
+
 REQUIRED_REGISTRIES_LOCAL=''
 BUILD_ARGS=("IMAGE_TAG")
 
