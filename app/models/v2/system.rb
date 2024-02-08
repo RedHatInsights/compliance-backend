e4c4b1418b7d24e41bbf3ed03a576e661afdd2ff
Janos Laszlo Vasik <jvasik@redhat.com>
2024-02-08 16:22:45 +0100

    fix(APIv2): RHINENG-1986 connect hosts table to systems model

--- app/models/v2/system.rb
+++ app/models/v2/system.rb
@@ -3,6 +3,9 @@
 module V2
   # Class representing read-only systems syndicated from the host-based inventory
   class System < ApplicationRecord
+    self.table_name = 'inventory.hosts'
+    self.primary_key = 'id'
+
     belongs_to :account, class_name: 'V2::Account', foreign_key: :org_id, inverse_of: :systems
     has_many :policy_systems, class_name: 'V2::PolicySystem', dependent: nil
     has_many :policies, through: :policy_systems
