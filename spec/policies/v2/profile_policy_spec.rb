3f9a966933b49417357b7c3e50e3397800413700
Dávid Halász <dhalasz@redhat.com>
2024-02-09 16:42:43 +0100

    fix(tests): make policy specs use the right user factory

--- spec/policies/v2/profile_policy_spec.rb
+++ spec/policies/v2/profile_policy_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 describe V2::ProfilePolicy do
-  let(:user) { FactoryBot.create(:user) }
+  let(:user) { FactoryBot.create(:v2_user) }
   let(:items) { FactoryBot.create_list(:v2_profile, 20) }
 
   it_behaves_like 'globally available'
--- spec/policies/v2/rule_policy_spec.rb
+++ spec/policies/v2/rule_policy_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 describe V2::RulePolicy do
-  let(:user) { FactoryBot.create(:user) }
+  let(:user) { FactoryBot.create(:v2_user) }
   let(:items) { FactoryBot.create_list(:v2_rule, 20) }
 
   it_behaves_like 'globally available'
