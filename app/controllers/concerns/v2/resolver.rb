0ebce1071ff7c60543da432e15b95450de048d75
Dávid Halász <dhalasz@redhat.com>
2024-02-15 14:52:35 +0100

    feat(V2): implemented aggregated_attribute for resolver/serializer

--- app/controllers/concerns/v2/resolver.rb
+++ app/controllers/concerns/v2/resolver.rb
@@ -5,54 +5,95 @@ module V2
   module Resolver
     extend ActiveSupport::Concern
 
-    def expand_resource
-      # Get the list of fields to be selected from the serializer
-      fields = serializer.fields(permitted_params[:parents], resource.one_to_one)
-      # Append a list of additional fields required to render the response, usually RBAC related
-      fields[nil] += extra_fields
+    private
 
+    # Building the query that returns all the required data for serialization
+    def expand_resource
       # Join with the parents assumed from the route
       scope = join_parents(pundit_scope, permitted_params[:parents])
-      # Join with the additional 1:1 relationships required by the serializer,
-      # select only the fields that are really necessary for the rendering.
-      join_one_to_ones(scope, fields).select(*select_fields(fields))
+      # Join with the additional 1:1 relationships required by the serializer, select only the
+      # dependencies that are really necessary for the rendering.
+      join_aggregated(join_associated(scope)).select(*select_fields)
     end
 
-    # Reduce through all the parents of the `relation` and join+scope them or return the
-    # `relation˙ untouched if it does not have any parents.
-    def join_parents(relation, parents)
-      parents.to_a.reduce(relation) do |scope, parent|
-        ref = scope.reflect_on_association(parent)
+    # Reduce through all the associations of the `relation` and join+scope them or return the
+    # `relation˙ untouched if it does not have any associations.
+    def join_parents(relation, associations)
+      associations.to_a.reduce(relation) do |scope, association|
+        ref = scope.reflect_on_association(association)
         klass = ref.klass
 
-        scope.joins(parent)
-             .where(parent => { klass.primary_key => permitted_params[ref.foreign_key] })
+        scope.joins(association)
+             .where(association => { klass.primary_key => permitted_params[ref.foreign_key] })
              .merge(Pundit.policy_scope(current_user, klass))
       end
     end
 
     # Select the 1:1 associations that can be satisfied without any additional WHERE clause,
-    # then join them to the scope.
-    def join_one_to_ones(scope, fields)
+    # then join them with the relation.
+    def join_associated(relation)
       # Do not join with the already joined parents assumed from the (nested) route
-      associations = fields.keys.excluding(*permitted_params[:parents]).compact
-      scope.where.associated(*associations)
+      associations = dependencies.keys.excluding(*permitted_params[:parents]).compact
+      relation.where.associated(*associations)
+    end
+
+    # Self-join with the requested aggregations built using 1:n associations, also select
+    # the aggregated field from the self-joined subquery.
+    def join_aggregated(relation)
+      aggregations.reduce(relation) do |scope, (association, aggregation)|
+        scope.joins(subquery_fragment(association, aggregation))
+             .select(aggregation.alias)
+      end
+    end
+
+    # Builds a subquery with the `resource` left outer joined with the `association`, grouped
+    # by the primary key of the `resource` and returning the result with the `aggregation`.
+    # This subquery is then further self-joined with the `resource` and the joining fragment
+    # is extracted from the arel tree.
+    #
+    # The resulting fragment ends up in the following format:
+    # ```
+    # INNER JOIN (
+    #   SELECT "resource"."id", AGG("association"."XY") FROM "resource" LEFT OUTER JOIN "association" ...
+    # ) "aggregate_association" ON "aggregate_association"."id" = "resource"."id";
+    # ```
+    #
+    def subquery_fragment(association, aggregation)
+      sq = resource.left_outer_joins(association)
+                   .group(resource.primary_key)
+                   .select(resource.primary_key, aggregation)
+      resource.arel_join_fragment(sq.arel.as(association.to_s))
     end
 
     # Iterate through the (nested) fields to be selected and set their names accordingly
     # so it is understood by SQL. Furthermore, alias any field that is coming from a joined
     # table to avoid any possible hash key collision.
-    def select_fields(fields)
-      fields.flat_map do |(association, columns)|
-        columns.map do |column|
+    def select_fields
+      dependencies.flat_map do |(association, fields)|
+        fields.map do |field|
           if association
-            "#{association}.#{column} AS #{association}__#{column}"
+            "#{association}.#{field} AS #{association}__#{field}"
           else
             # FIXME: this is just temporary until we figure out aggregations as they will be also arel-based
-            column.is_a?(ApplicationRecord::AN::Node) ? column.to_sql : [resource.table_name, column].join('.')
+            field.is_a?(ApplicationRecord::AN::Node) ? field.to_sql : [resource.table_name, field].join('.')
           end
         end
       end
+    end
+
+    # Get the list of dependent (relations => [fields]) to be selected from the serializer and append the
+    # list of additional fields required to render the response, usually RBAC related.
+    def dependencies
+      @dependencies ||= begin
+        deps = serializer.dependencies(permitted_params[:parents], resource.one_to_one)
+        deps[nil] += extra_fields
+        deps
+      end
+    end
+
+    # Retrieve the list of aggregations on any one-to-many associations specified by the serializer
+    def aggregations
+      @aggregations ||= serializer.aggregations(resource.one_to_many)
     end
   end
 end
--- app/models/v2/application_record.rb
+++ app/models/v2/application_record.rb
@@ -24,5 +24,22 @@ module V2
         obj << key.to_sym if reflection.has_one? || reflection.belongs_to?
       end
     end
+
+    # Returns with a list of symbols describing one-to-many relationships
+    def self.one_to_many
+      reflections.each_with_object([]) do |(key, reflection), obj|
+        obj << key.to_sym unless reflection.has_one? || reflection.belongs_to?
+      end
+    end
+
+    # Creates an Arel-fragment from a self-joined subquery that can be passed as an argument
+    # to the `ActiveRecord::Base.joins` method.
+    # rubocop:disable Metrics/AbcSize
+    def self.arel_join_fragment(subquery)
+      select(primary_key).arel.join(subquery)
+                         .on(subquery[primary_key].eq(arel_table[primary_key]))
+                         .ast.cores.first.source.right.first
+    end
+    # rubocop:enable Metrics/AbcSize
   end
 end
