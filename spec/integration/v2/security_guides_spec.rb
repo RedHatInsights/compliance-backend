f6d24b1b50e3880cb00876d2913a9cc6d251d8fe
Dávid Halász <dhalasz@redhat.com>
2024-03-15 07:47:44 +0100

    feat(APIv2): RHINENG-2752 swagger specs for Security Guides

--- spec/integration/v2/security_guides_spec.rb
+++ spec/integration/v2/security_guides_spec.rb
@@ -3,154 +3,95 @@
 require 'swagger_helper'
 
 describe 'Security Guides', swagger_doc: 'v2/openapi.json' do
-  let(:identity_header) do
-    OpenStruct.new(
-      cert_based?: false,
-      valid?: true,
-      org_id: '1234',
-      identity: { org_id: '1234' },
-      raw: nil
-    )
-  end
+  let(:'X-RH-IDENTITY') { FactoryBot.create(:v2_user).account.identity_header.raw }
 
-  before do
-    allow(Insights::Api::Common::IdentityHeader).to receive(:new).and_return(identity_header)
-    stub_rbac_permissions(Rbac::COMPLIANCE_ADMIN, Rbac::INVENTORY_HOSTS_READ)
-  end
+  before { stub_rbac_permissions(Rbac::COMPLIANCE_ADMIN, Rbac::INVENTORY_HOSTS_READ) }
 
   path '/security_guides' do
-    get 'List all Security Guides' do
-      before do
-        SupportedSsg.all.map(&:os_major_version).uniq.each do |os_version|
-          FactoryBot.create(
-            :v2_security_guide,
-            title: 'Guide to the Secure Configuration of Red Hat Enterprise ' \
-                   "Linux #{os_version}",
-            description: 'This guide presents a catalog of security-relevant ' \
-                         "configuration settings for Red Hat Enterprise Linux #{os_version}.",
-            os_major_version: os_version
-          )
-        end
-      end
+    before { FactoryBot.create_list(:v2_security_guide, 25) }
 
+    get 'Request Security Guides' do
+      v2_auth_header
       tags 'security_guide'
-      description 'Lists all Security guides'
-      operationId 'ListSecurityGuides'
+      description 'Lists Security Guides'
+      operationId 'SecurityGuides'
       content_types
       pagination_params_v2
       sort_params_v2(V2::SecurityGuide)
       search_params_v2(V2::SecurityGuide)
 
-      response '200', 'Lists all Security Guides requested' do
-        schema type: :object,
-               properties: {
-                 meta: ref_schema('metadata'),
-                 links: ref_schema('links'),
-                 data: {
-                   type: :array,
-                   items: {
-                     properties: {
-                       type: { type: :string },
-                       id: ref_schema('id'),
-                       attributes: ref_schema('security_guides')
-                     }
-                   }
-                 }
-               }
+      response '200', 'Lists Security Guides' do
+        v2_collection_schema 'security_guide'
 
         after { |e| autogenerate_examples(e, 'List of Security Guides') }
 
         run_test!
       end
 
-      response '200', 'Lists all Security Guides requested' do
+      response '200', 'Lists Security Guides' do
         let(:sort_by) { ['os_major_version'] }
-        schema type: :object,
-               properties: {
-                 meta: ref_schema('metadata'),
-                 links: ref_schema('links'),
-                 data: {
-                   type: :array,
-                   items: {
-                     properties: {
-                       type: { type: :string },
-                       id: ref_schema('id'),
-                       attributes: ref_schema('security_guides')
-                     }
-                   }
-                 }
-               }
+        v2_collection_schema 'security_guide'
 
         after { |e| autogenerate_examples(e, 'List of Security Guides sorted by "os_major_verision:asc"') }
 
         run_test!
       end
 
-      response '200', 'Lists all Security Guides requested' do
+      response '200', 'Lists Security Guides' do
         let(:filter) { '(os_major_version=8)' }
-        schema type: :object,
-               properties: {
-                 meta: ref_schema('metadata'),
-                 links: ref_schema('links'),
-                 data: {
-                   type: :array,
-                   items: {
-                     properties: {
-                       type: { type: :string },
-                       id: ref_schema('id'),
-                       attributes: ref_schema('security_guides')
-                     }
-                   }
-                 }
-               }
+        v2_collection_schema 'security_guide'
 
         after { |e| autogenerate_examples(e, 'List of Security Guides filtered by "(os_major_version=8)"') }
 
         run_test!
       end
 
-      response '422', 'Returns error if wrong parameters are used' do
+      response '422', 'Returns with Unprocessable Content' do
         let(:sort_by) { ['description'] }
-        schema type: :object,
-               properties: {
-                 meta: ref_schema('metadata'),
-                 links: ref_schema('links'),
-                 data: {
-                   type: :array,
-                   items: {
-                     properties: {
-                       type: { type: :string },
-                       id: ref_schema('id'),
-                       attributes: ref_schema('security_guides')
-                     }
-                   }
-                 }
-               }
+        schema ref_schema('errors')
 
         after { |e| autogenerate_examples(e, 'Description of an error when sorting by incorrect parameter') }
 
         run_test!
       end
 
-      response '422', 'Returns error if wrong parameters are used' do
+      response '422', 'Returns with Unprocessable Content' do
         let(:limit) { 103 }
-        schema type: :object,
-               properties: {
-                 meta: ref_schema('metadata'),
-                 links: ref_schema('links'),
-                 data: {
-                   type: :array,
-                   items: {
-                     properties: {
-                       type: { type: :string },
-                       id: ref_schema('id'),
-                       attributes: ref_schema('security_guides')
-                     }
-                   }
-                 }
-               }
+        schema ref_schema('errors')
 
         after { |e| autogenerate_examples(e, 'Description of an error when requesting higher limit than supported') }
+
+        run_test!
+      end
+    end
+  end
+
+  path '/security_guides/{id}' do
+    let(:item) { FactoryBot.create(:v2_security_guide) }
+
+    get 'Request Security Guide' do
+      v2_auth_header
+      tags 'security_guide'
+      description 'Returns a Security Guide'
+      operationId 'SecurityGuide'
+      content_types
+
+      parameter name: :id, in: :path, type: :string, required: true
+
+      response '200', 'Returns Security Guide' do
+        let(:id) { item.id }
+        v2_item_schema('security_guide')
+
+        after { |e| autogenerate_examples(e, 'Returns a Security Guide') }
+
+        run_test!
+      end
+
+      response '404', 'Returns with Not Found' do
+        let(:id) { Faker::Internet.uuid }
+        schema ref_schema('errors')
+
+        after { |e| autogenerate_examples(e, 'Returns a Security Guide') }
 
         run_test!
       end
