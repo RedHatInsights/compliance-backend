1dea5aa3a00163b7e20c989a5a101189103cd469
Dávid Halász <dhalasz@redhat.com>
2024-03-24 08:55:55 +0100

    chore(deps): update Rails default configuration to 7.0

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,10 @@ Dotenv::Rails.load unless Rails.env.production?
 module ComplianceBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
+
+    # Partial inserts are necessary for writing into views with read-only fields
+    config.active_record.partial_inserts = true # FIXME: clean up after the remodel
 
     # Load platform modules
     require 'insights'
