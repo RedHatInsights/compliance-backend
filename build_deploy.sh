0cc03ea0eaecd4f1a9d178c19a14f1fa74ce22ad
Dávid Halász <dhalasz@redhat.com>
2024-03-26 14:59:47 +0100

    bump

--- build_deploy.sh
+++ build_deploy.sh
@@ -7,7 +7,7 @@ CICD_TOOLS_URL="https://raw.githubusercontent.com/RedHatInsights/cicd-tools/main
 source <(curl -sSL "$CICD_TOOLS_URL") image_builder
 
 export CICD_IMAGE_BUILDER_IMAGE_NAME='quay.io/cloudservices/compliance-backend'
-export CICD_IMAGE_BUILDER_BUILD_ARGS=("IMAGE_TAG=$(cicd::image_builder::get_image_tag)")
+export CICD_IMAGE_BUILDER_BUILD_ARGS=("IMAGE_TAG=$(cicd::image_builder::get_image_tag)" "gemMirror=$GEM_MIRROR" "httpProxy=$HTTP_PROXY")
 # Check if the current Git branch is 'origin/security-compliance'.
 if [[ "$GIT_BRANCH" == "origin/security-compliance" ]]; then
     # Generate a tag for the Docker image based on the current date and Git commit short hash.
