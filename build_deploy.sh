aaf5f343f5cf32a765b5d8a897c1493d79003a50
Casey Williams <caswilli@redhat.com>
2024-04-22 11:45:33 -0400

    fix(build_deploy): only create sc tag when building sc branch

--- build_deploy.sh
+++ build_deploy.sh
@@ -7,16 +7,15 @@ CICD_TOOLS_URL="https://raw.githubusercontent.com/RedHatInsights/cicd-tools/main
 source <(curl -sSL "$CICD_TOOLS_URL") image_builder
 
 export CICD_IMAGE_BUILDER_IMAGE_NAME='quay.io/cloudservices/compliance-backend'
-export CICD_IMAGE_BUILDER_BUILD_ARGS=("IMAGE_TAG=$(cicd::image_builder::get_image_tag)")
+
 # Check if the current Git branch is 'origin/security-compliance'.
 if [[ "$GIT_BRANCH" == "origin/security-compliance" ]]; then
-    # Generate a tag for the Docker image based on the current date and Git commit short hash.
+    # Generate a tag for the container image based on the current date and Git commit short hash.
     SECURITY_COMPLIANCE_TAG="sc-$(date +%Y%m%d)-$(git rev-parse --short=7 HEAD)"
-    
-    # Set ADDITIONAL_TAGS to the generated security compliance tag.
-    export CICD_IMAGE_BUILDER_ADDITIONAL_TAGS=("$SECURITY_COMPLIANCE_TAG")
+    export CICD_IMAGE_BUILDER_BUILD_ARGS=("IMAGE_TAG=$SECURITY_COMPLIANCE_TAG")
 else
     # If the current Git branch is not 'origin/security-compliance':
+    export CICD_IMAGE_BUILDER_BUILD_ARGS=("IMAGE_TAG=$(cicd::image_builder::get_image_tag)")
     export CICD_IMAGE_BUILDER_ADDITIONAL_TAGS=("latest")
 fi
 
