52b2a48c12889ea2dae442edc7918df44b2c84ce
Dávid Halász <dhalasz@redhat.com>
2024-05-10 18:41:20 +0200

    fix(APIv2): RHINENG-10010 do not allow duplicate policy creation

--- app/models/v2/policy.rb
+++ app/models/v2/policy.rb
@@ -22,6 +22,7 @@ module V2
     validates :compliance_threshold, numericality: {
       greater_than_or_equal_to: 0, less_than_or_equal_to: 100
     }
+    validate :not_duplicate?, on: :create
 
     sortable_by :title
     sortable_by :os_major_version, 'security_guide.os_major_version'
@@ -83,6 +84,16 @@ module V2
       self.title ||= profile&.title
       self.description ||= profile&.description
       self.compliance_threshold ||= 100.0
+    end
+
+    def not_duplicate?
+      policies = self.class.joins(profile: :security_guide).where(
+        security_guide: { os_major_version: profile&.security_guide&.os_major_version },
+        profile: { ref_id: profile&.ref_id },
+        account_id: account_id
+      ).count
+
+      errors.add(:profile, 'duplication is not supported') unless policies.zero?
     end
   end
 end
