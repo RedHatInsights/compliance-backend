ce24161595ad165da3e85f7b4b98cbc4b73abd87
Roman Blanco <rblanco@redhat.com>
2025-03-21 12:44:37 +0000

    revert(API): RHINENG-16382 remove SSG ID for Test Result
    This reverts commit 378641e0f415ad6cbb5c8ff694c4f4b675b95e46.
    
    The security_guide_id would need to be introduced to other models for
    consistency, adding unnecessary information to the API

--- app/models/v2/test_result.rb
+++ app/models/v2/test_result.rb
@@ -2,7 +2,6 @@
 
 module V2
   # Database model representing latest results of compliance scans
-  # rubocop:disable Metrics/ClassLength
   class TestResult < ApplicationRecord
     # FIXME: clean up after the remodel
     self.table_name = :v2_test_results
@@ -124,10 +123,6 @@ module V2
       !score.nil? && score >= threshold.to_f
     end
 
-    def security_guide_id
-      attributes['security_guide__id'] || try(:security_guide)&.id
-    end
-
     def security_guide_version
       attributes['security_guide__version'] || try(:security_guide)&.version
     end
@@ -148,5 +143,4 @@ module V2
         .uniq # using this instead of `.distinct` to properly handle sorting of versions. It should still perform fine.
     end
   end
-  # rubocop:enable Metrics/ClassLength
 end
