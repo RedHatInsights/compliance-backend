3eb98be974ed75eca55e7ac371e5c691b14ae564
Janos Laszlo Vasik <jvasik@redhat.com>
2025-04-10 15:56:34 +0200

    refactor(Kafka): RHINENG-17355 move host remover wrapper to service

--- app/consumers/compliance_consumer.rb
+++ app/consumers/compliance_consumer.rb
@@ -1,18 +1,26 @@
 # frozen_string_literal: true
 
-# Receives messages from the Kafka topic, converts them into jobs for processing
+# Receives messages from the Kafka topic, dispatches them to the appropriate service
 class ComplianceConsumer < ApplicationConsumer
+  def consume_one
+    if message_type == 'delete'
+      Kafka::HostRemover.new(payload, logger).remove_host
+    else
+      logger.debug "Skipped message of type '#{message_type}'"
+    end
+  end
+
   private
 
   def payload
     JSON.parse(@message.raw_payload)
   end
 
-  def account
-    payload.dig('platform_metadata', 'account')
+  def service
+    payload.dig('platform_metadata', 'service')
   end
 
-  def org_id
-    payload.dig('platform_metadata', 'org_id')
+  def message_type
+    payload.dig('type')
   end
 end
