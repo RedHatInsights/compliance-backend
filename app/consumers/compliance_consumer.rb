393874f391dde745bdf7feef577591ff83b5085e
Roman Blanco <rblanco@redhat.com>
2025-05-20 17:44:44 +0000

    revert(Kafka): RHINENG-8908 auto-assign Systems created to Policies
    This reverts commit 3c8817e7e06b78e78a8d812616da4014a30bfed6.

--- app/consumers/compliance_consumer.rb
+++ app/consumers/compliance_consumer.rb
@@ -3,9 +3,7 @@
 # Receives messages from the Kafka topic, dispatches them to the appropriate service
 class ComplianceConsumer < ApplicationConsumer
   def consume_one
-    if message_type == 'created' && policy_id
-      Kafka::PolicySystemImporter.new(payload, logger).import
-    elsif message_type == 'delete'
+    if message_type == 'delete'
       Kafka::HostRemover.new(payload, logger).remove_host
     else
       logger.debug "Skipped message of type '#{message_type}'"
@@ -24,9 +22,5 @@ class ComplianceConsumer < ApplicationConsumer
 
   def message_type
     payload.dig('type')
-  end
-
-  def policy_id
-    payload.dig('host', 'system_profile', 'image_builder', 'compliance_policy_id')
   end
 end
