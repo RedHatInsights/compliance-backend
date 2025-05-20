e92f59148d4f15a46b201b34022a0793ec056b7a
Roman Blanco <rblanco@redhat.com>
2025-05-20 17:45:26 +0000

    revert(Kafka): RHINENG-17355 move host remover wrapper to service
    This reverts commit 22c7b40945512ae91a8f690b525aca4eaface032.

--- app/consumers/compliance_consumer.rb
+++ app/consumers/compliance_consumer.rb
@@ -1,26 +1,18 @@
 # frozen_string_literal: true
 
-# Receives messages from the Kafka topic, dispatches them to the appropriate service
+# Receives messages from the Kafka topic, converts them into jobs for processing
 class ComplianceConsumer < ApplicationConsumer
-  def consume_one
-    if message_type == 'delete'
-      Kafka::HostRemover.new(payload, logger).remove_host
-    else
-      logger.debug "Skipped message of type '#{message_type}'"
-    end
-  end
-
   private
 
   def payload
     JSON.parse(@message.raw_payload)
   end
 
-  def service
-    payload.dig('platform_metadata', 'service')
+  def account
+    payload.dig('platform_metadata', 'account')
   end
 
-  def message_type
-    payload.dig('type')
+  def org_id
+    payload.dig('platform_metadata', 'org_id')
   end
 end
