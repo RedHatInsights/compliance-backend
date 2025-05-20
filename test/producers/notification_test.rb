f748dbc62b113a0034f2c6b9a71c05faf3c8f349
Roman Blanco <rblanco@redhat.com>
2025-05-20 18:05:49 +0000

    revert(Kafka): return Racecar back in stage
    reverting changes related to switching Kafka client from Racecar to
    Karafka:
    
    2ef36478d0410068e1e5635cd72db2b706bad014...5c1ec2e611cba43e698bfc91ff405156bab01717
    
    This PR commits squashed changes included in
    https://github.com/RedHatInsights/compliance-backend/pull/2447

---
+++ test/producers/notification_test.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class NofiticationTest < ActiveSupport::TestCase
+  class MockNotification < Notification
+    def self.build_context(**_kwargs)
+      {}
+    end
+
+    def self.build_events(**_kwargs)
+      []
+    end
+  end
+
+  setup do
+    @acc = FactoryBot.create(:account)
+  end
+
+  test 'handles missing kafka config' do
+    MockNotification.stubs(:kafka).returns(nil)
+    assert_nil MockNotification.deliver(org_id: @acc.org_id)
+  end
+
+  test 'delivers messages to the notifications topic' do
+    kafka = mock('kafka')
+    MockNotification.stubs(:kafka).returns(kafka)
+    kafka.expects(:produce).with(anything)
+    MockNotification.deliver(org_id: @acc.org_id)
+  end
+
+  test 'handles delivery issues' do
+    kafka = mock('kafka')
+    MockNotification.stubs(:kafka).returns(kafka)
+    kafka.expects(:produce).with(anything).raises(Rdkafka::RdkafkaError.new(1))
+
+    assert_nothing_raised do
+      MockNotification.deliver(org_id: @acc.org_id)
+    end
+  end
+end
