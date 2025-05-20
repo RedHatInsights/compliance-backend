3265b199e9f8e8890e7713252114f0e3165bff56
Roman Blanco <rblanco@redhat.com>
2025-05-20 17:42:17 +0000

    revert(Kafka): RHINENG-17355 implement report parser in Karafka
    This reverts commit 8d72b362f80cf5406e378619beb479ed2190fce8.

--- app/consumers/compliance_consumer.rb
+++ app/consumers/compliance_consumer.rb
@@ -3,9 +3,7 @@
 # Receives messages from the Kafka topic, dispatches them to the appropriate service
 class ComplianceConsumer < ApplicationConsumer
   def consume_one
-    if service == 'compliance'
-      Kafka::ReportParser.new(payload, logger).parse_reports
-    elsif message_type == 'created' && policy_id
+    if message_type == 'created' && policy_id
       Kafka::PolicySystemImporter.new(payload, logger).import
     elsif message_type == 'delete'
       Kafka::HostRemover.new(payload, logger).remove_host
