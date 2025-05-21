c4d331f5d4f8c17092c47654111362caaca31a2d
Roman Blanco <rblanco@redhat.com>
2025-05-21 12:59:51 +0000

    style(Rubocop): match Rubocop rules

--- app/services/concerns/xccdf/test_result.rb
+++ app/services/concerns/xccdf/test_result.rb
@@ -3,6 +3,8 @@
 module Xccdf
   # Methods related to saving TestResult from openscap_parser
   module TestResult
+    delegate :score, to: :@op_test_result
+
     def save_test_result
       @test_result = ::TestResult.create!(
         host: @host, profile: @host_profile,
@@ -23,10 +25,6 @@ module Xccdf
                          host: @host)
                   .where.not(id: @test_result.id)
                   .destroy_all
-    end
-
-    def score
-      @op_test_result.score
     end
 
     def supported?
