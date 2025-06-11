2000f8e45e963500bd889cc8d84aa222f583dcad
Janos Laszlo Vasik <jvasik@redhat.com>
2025-06-11 11:45:55 +0200

    fix(logging): RHINENG-18683 enhance kafka logging with lag and reduce noise

--- app/consumers/application_consumer.rb
+++ app/consumers/application_consumer.rb
@@ -7,25 +7,21 @@ class ApplicationConsumer < Karafka::BaseConsumer
   # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
   def consume
     logger.info "Processing #{messages.metadata.topic}/#{messages.metadata.partition} " \
-                "#{messages.metadata.first_offset}-#{messages.metadata.last_offset}"
+                "#{messages.metadata.first_offset}-#{messages.metadata.last_offset} " \
+                "with processing lag #{messages.metadata.processing_lag}"
     messages.each do |message|
       @message = message
 
-      if retrying?
-        logger.debug 'Retrying message'
-
-        if attempt > 3
-          logger.error 'Discarded message'
-          mark_as_consumed(message)
-        end
+      if attempt > 3
+        logger.error 'Discarded message'
+        mark_as_consumed(message)
       end
 
       consume_one
-
-      logger.info 'Consumed message'
-
       mark_as_consumed(message)
     end
+
+    logger.info 'Batch consumed'
   end
   # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
