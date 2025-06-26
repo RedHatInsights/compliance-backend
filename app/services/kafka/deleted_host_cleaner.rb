9c589abdaae153746afea1d57841bcad1041e3b5
Janos Laszlo Vasik <jvasik@redhat.com>
2025-06-26 15:30:39 +0200

    refactor(Kafka): RHINENG-18559 use V2 models in DeletedHostCleanup

--- app/services/kafka/deleted_host_cleaner.rb
+++ app/services/kafka/deleted_host_cleaner.rb
@@ -13,7 +13,7 @@ module Kafka
 
     def cleanup_host
       num_removed = remove_related
-      audit_success(num_removed) if num_removed.positive?
+      audit_success if num_removed.positive?
     rescue StandardError => e
       audit_fail(e)
       raise
@@ -23,47 +23,37 @@ module Kafka
 
     def remove_related
       [
-        remove_related_rule_results,
         remove_related_test_results,
-        remove_related_policy_hosts
+        remove_related_policy_systems
       ].sum
     end
 
-    def remove_related_rule_results
-      RuleResult.where(host_id: @id).delete_all
-    end
-
     def remove_related_test_results
-      to_remove = TestResult.where(host_id: @id)
-      profiles_to_adjust = Profile.where(id: to_remove.pluck(:profile_id).uniq)
+      to_remove = V2::HistoricalTestResult.where(system_id: @id)
 
       num_removed = to_remove.delete_all
-
-      profiles_to_adjust.find_each do |profile|
-        profile.calculate_score!
-        profile.policy.update_counters!
-      end
 
       num_removed
     end
 
-    def remove_related_policy_hosts
-      to_remove = PolicyHost.where(host_id: @id)
-      policies_to_adjust = Policy.where(id: to_remove.pluck(:policy_id).uniq)
+    def remove_related_policy_systems
+      to_remove = V2::PolicySystem.where(system_id: @id)
+      policies_to_adjust = V2::Policy.where(id: to_remove.pluck(:policy_id).uniq)
 
       num_removed = to_remove.delete_all
 
-      policies_to_adjust.find_each(&:update_counters!)
+      # FIXME: V1 model compatiblity method - remove after remodel
+      policies_to_adjust.find_each(&:__v1_update_total_system_count)
 
       num_removed
     end
 
     def audit_fail(error)
-      @logger.audit_fail("[#{@org_id}] Failed to delete related records for Host #{@id}: #{error.message}")
+      @logger.audit_fail("[#{@org_id}] Failed to delete related records for System #{@id}: #{error.message}")
     end
 
-    def audit_success(num_removed)
-      @logger.audit_success("[#{@org_id}] Deleted #{num_removed} related records for Host #{@id}")
+    def audit_success
+      @logger.audit_success("[#{@org_id}] Deleted related records for System #{@id}")
     end
   end
 end
