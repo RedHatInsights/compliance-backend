ce1e371ca3c6c92ec1a251a8bc95448355e37f72
Janos Laszlo Vasik <jvasik@redhat.com>
2025-06-26 16:02:02 +0200

    refactor(tests): from DeleteHost to DeletedHostCleaner

--- spec/services/kafka/deleted_host_cleaner_spec.rb
+++ spec/services/kafka/deleted_host_cleaner_spec.rb
@@ -3,52 +3,79 @@
 require 'rails_helper'
 
 describe Kafka::DeletedHostCleaner do
-  after do
-    DeleteHost.clear
-  end
-
   let(:service) { Kafka::DeletedHostCleaner.new(message, Karafka.logger) }
 
   let(:type) { 'delete' }
   let(:user) { FactoryBot.create(:v2_user) }
   let(:org_id) { user.org_id }
-  let(:system) do
-    FactoryBot.create(
-      :system,
-      account: user.account
-    )
-  end
+  let(:policy) { FactoryBot.create(:v2_policy, account: user.account, supports_minors: [0]) }
+  let(:system) { FactoryBot.create(:system, account: user.account, policy_id: policy.id, os_minor_version: 0) }
+  let!(:test_result) { FactoryBot.create(:v2_test_result, system: system, policy_id: policy.id) }
   let(:message) do
     {
       'type' => type,
       'id' => system.id,
-      'timestamp' => DateTime.now.iso8601(6),
       'org_id' => org_id
     }
   end
 
-  it 'enqueues system deletion' do
+  it 'performs and logs cleanup' do
     expect(Karafka.logger).to receive(:audit_success).with(
-      "[#{org_id}] Enqueued DeleteHost job for host #{system.id}"
+      "[#{org_id}] Deleted related records for System #{system.id}"
     )
 
-    service.remove_host
-
-    expect(DeleteHost.jobs.size).to eq(1)
+    expect do
+      service.cleanup_host
+    end.to change { V2::HistoricalTestResult.where(system_id: system.id).count }.from(1).to(0)
+       .and change { policy.systems.count }.from(1).to(0)
   end
 
-  context 'enqueued system deletion fails' do
-    before do
-      allow(DeleteHost).to receive(:perform_async).and_raise(StandardError)
-    end
+  context 'with multiple systems under a policy' do
+    let(:extra_system) { FactoryBot.create(:system, account: user.account, policy_id: policy.id, os_minor_version: 0) }
+    let!(:extra_test_result) { FactoryBot.create(:v2_test_result, system: extra_system, policy_id: policy.id) }
 
-    it 'handles error gracefully' do
-      expect(Karafka.logger).to receive(:audit_fail).with(
-        "[#{org_id}] Failed to enqueue DeleteHost job for host #{system.id}: StandardError"
+    it 'performs and logs cleanup for the specific system' do
+      expect(Karafka.logger).to receive(:audit_success).with(
+        "[#{org_id}] Deleted related records for System #{system.id}"
       )
 
-      expect { service.remove_host }.to raise_error(StandardError)
-      expect(DeleteHost.jobs.size).to eq(0)
+      expect do
+        service.cleanup_host
+      end.to change { V2::HistoricalTestResult.where(system_id: system.id).count }.from(1).to(0)
+         .and change { policy.systems.count }.from(2).to(1)
+
+      expect(V2::HistoricalTestResult.where(system_id: extra_system.id).count).to eql(1)
+    end
+  end
+
+  context 'when there are no records tied to the deleted system' do
+    let(:lonely_system) { FactoryBot.create(:system, account: user.account, os_minor_version: 0) }
+    let(:message) do
+      {
+        'type' => type,
+        'id' => lonely_system.id,
+        'org_id' => org_id
+      }
+    end
+
+    it 'does not perform cleanup and does not log' do
+      expect(Karafka.logger).not_to receive(:audit_success)
+
+      service.cleanup_host
+    end
+  end
+
+  context 'when deletion is not possible' do
+    before do
+      allow(service).to receive(:remove_related).and_raise(StandardError)
+    end
+
+    it 'logs failure and raises and error' do
+      expect(Karafka.logger).to receive(:audit_fail).with(
+        "[#{org_id}] Failed to delete related records for System #{system.id}: StandardError"
+      )
+
+      expect { service.cleanup_host }.to raise_error(StandardError)
     end
   end
 end
