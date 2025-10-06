5726b8ef982a7ed7f22d7e2a2b31cc3854e3ba2c
Josejulio Martínez <jmartine@redhat.com>
2025-10-06 09:34:28 -0600

    fix(kessel): use ruby client to fetch default workspace

--- app/services/kessel_utils.rb
+++ app/services/kessel_utils.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
+require 'kessel-sdk'
+
 # Utilities used by Kessel
 class KesselUtils
   class << self
+    include Kessel::RBAC::V2
+    include Kessel::Auth
+
     def get_default_workspace_id(auth, identity_header)
       parsed_identity = Insights::Api::Common::IdentityHeader.new(identity_header)
       org_id = parsed_identity.org_id
@@ -10,52 +15,12 @@ class KesselUtils
 
       return @workspace_cache[cache_key] if @workspace_cache&.key?(cache_key)
 
-      workspace_id = fetch_default_workspace(auth, Settings.endpoints.rbac.url, org_id)
+      workspace = fetch_default_workspace(Settings.endpoints.rbac.url, org_id, auth: oauth2_auth_request(auth),
+                                                                               http_client: nil)
       @workspace_cache ||= {}
-      @workspace_cache[cache_key] = workspace_id
+      @workspace_cache[cache_key] = workspace.id
 
-      workspace_id
-    end
-
-    private
-
-    def fetch_default_workspace(auth, rbac_base_endpoint, org_id)
-      access_token = auth.get_token.access_token
-
-      workspace_response = make_workspace_request(rbac_base_endpoint, access_token, org_id)
-      workspace_id = extract_workspace_id(workspace_response)
-
-      raise "No default workspace found for org id: #{org_id}" unless workspace_id
-
-      workspace_id
-    end
-
-    def make_workspace_request(rbac_base_endpoint, access_token, org_id)
-      conn = build_faraday_connection(rbac_base_endpoint)
-
-      conn.get('/api/rbac/v2/workspaces/') do |req|
-        req.params['type'] = 'default'
-        req.headers['authorization'] = "Bearer #{access_token}"
-        req.headers['x-rh-rbac-org-id'] = org_id
-        req.headers['Content-Type'] = 'application/json'
-      end
-    end
-
-    def build_faraday_connection(rbac_base_endpoint)
-      Faraday.new(url: rbac_base_endpoint) do |f|
-        f.request :url_encoded
-        f.response :json
-        f.adapter Faraday.default_adapter
-        # Do we need to add ENV['SSL_CERT_FILE'] ?
-      end
-    end
-
-    def extract_workspace_id(response)
-      raise "RBAC API error: #{response.status} - #{response.body}" unless response.success?
-
-      workspaces = response.body
-      workspace = workspaces.dig('data')&.first
-      workspace&.dig('id')
+      workspace.id
     end
   end
 end
