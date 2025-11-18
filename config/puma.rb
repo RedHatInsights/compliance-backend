7842db677e1fd57c0e78a24158c114aa09c26e41
Roman Blanco <rblanco@redhat.com>
2025-11-18 22:29:06 +0000

    cleanup(Unleash): remove Unleash

--- config/puma.rb
+++ config/puma.rb
@@ -43,19 +43,6 @@ workers(concurrency)
 #
 preload_app! if concurrency > 0
 
-on_worker_boot do
-  if ENV['UNLEASH_URL'].present?
-    Rails.configuration.unleash =  = Unleash::Client.new
-    Rails.logger.info "Unleash client initialized: URL=#{ENV['UNLEASH_URL']}, App=#{Rails.application.class.module_parent_name}"
-  end
-end
-
-on_worker_shutdown do
-  if ENV['UNLEASH_URL'].present?
-    Rails.configuration.unleash.shutdown
-  end
-end
-
 # Allow puma to be restarted by `rails restart` command.
 # plugin :tmp_restart
 
