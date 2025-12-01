717e49de384f9bcea14b839b0b4da855ffcd4922
LightOfHeaven1994 <eshamard@redhat.com>
2025-12-01 10:22:33 +0100

    fix(API): RHINENG-20744 count never reported number correctly

--- app/models/v2/report.rb
+++ app/models/v2/report.rb
@@ -91,7 +91,11 @@ module V2
       system_test_results = arel_table.join(V2::System.arel_table, AN::InnerJoin)
                                       .on(V2::System.arel_table[:id].eq(V2::ReportSystem.arel_table[:system_id]))
                                       .join(V2::TestResult.arel_table, AN::OuterJoin)
-                                      .on(V2::TestResult.arel_table[:system_id].eq(V2::System.arel_table[:id]))
+                                      .on(
+                                        V2::TestResult.arel_table[:system_id].eq(V2::System.arel_table[:id])
+                                        .and(V2::TestResult.arel_table[:report_id]
+                                        .eq(V2::ReportSystem.arel_table[:report_id]))
+                                      )
                                       .join_sources
 
       # aggregation to prevent optimizer (possibly buggy) filtering out join
