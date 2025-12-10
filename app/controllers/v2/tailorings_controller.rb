5a4ed9e113ef11cc76e3295f4e55d9cd2dfdcff4
Roman Blanco <rblanco@redhat.com>
2025-12-10 23:32:31 +0000

    revert(V2): preload associations for tailoring_file endpoint
    This reverts commit ce86990c918f1970dc862193bfaea1226d411d3e.

--- app/controllers/v2/tailorings_controller.rb
+++ app/controllers/v2/tailorings_controller.rb
@@ -66,19 +66,6 @@ module V2
 
     private
 
-    def expand_resource
-      scope = super
-
-      # preloading associations to prevent N+1
-      scope.includes(
-        :rules,
-        profile: :rules,
-        policy: :profile,
-        rules: :rule_group,
-        security_guide: :value_definitions
-      )
-    end
-
     def tailorings
       @tailorings ||= authorize(fetch_collection)
     end
