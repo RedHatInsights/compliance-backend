e550f48309f26f9c6eec71608b883916dc5fdcee
Roman Blanco <rblanco@redhat.com>
2026-01-08 10:53:07 +0000

    test(TestResults): add threshold unit test

--- spec/models/v2/test_result_spec.rb
+++ spec/models/v2/test_result_spec.rb
@@ -33,4 +33,69 @@ describe V2::TestResult do
       expect(subject.os_versions.to_set { |version| version.delete('"') }).to eq(versions.to_set)
     end
   end
+  
+  describe '#compliant' do
+    let(:account) { FactoryBot.create(:v2_account) }
+    let(:policy) do
+      FactoryBot.create(:v2_policy, :for_tailoring, account: account, 
+                        compliance_threshold: threshold,
+                        os_major_version: 7,
+                        supports_minors: [0])
+    end
+
+    context 'score == threshold' do
+      let(:threshold) { 90.0 }
+
+      it 'returns true when score equals threshold' do
+        test_result = FactoryBot.create(:v2_test_result, policy_id: policy.id,
+                                        account: account, score: 90.0, supported: true)
+        allow(test_result).to receive(:report).and_return(policy)
+        expect(test_result.compliant).to eq(true)
+      end
+    end
+
+    context 'score comparison' do
+      let(:threshold) { 90.0 }
+
+      it 'returns true when score > threshold' do
+        test_result = FactoryBot.create(:v2_test_result, policy_id: policy.id,
+                                        account: account, score: 90.01, supported: true)
+        allow(test_result).to receive(:report).and_return(policy)
+        expect(test_result.compliant).to eq(true)
+      end
+
+      it 'returns false when score < threshold' do
+        test_result = FactoryBot.create(:v2_test_result, policy_id: policy.id,
+                                        account: account, score: 89.99, supported: true)
+        allow(test_result).to receive(:report).and_return(policy)
+        expect(test_result.compliant).to eq(false)
+      end
+    end
+
+    context 'threshold changes' do
+      let(:threshold) { 95.0 }
+
+      it 'threshold change updates compliant status' do
+        test_result = FactoryBot.create(:v2_test_result, policy_id: policy.id,
+                                        account: account, score: 90.0, supported: true)
+        allow(test_result).to receive(:report).and_return(policy)
+        expect(test_result.compliant).to eq(false)
+
+        policy.update!(compliance_threshold: 85.0)
+        test_result.reload
+        expect(test_result.compliant).to eq(true)
+      end
+    end
+
+    context 'nil score' do
+      let(:threshold) { 90.0 }
+
+      it 'returns false when score is nil' do
+        test_result = FactoryBot.create(:v2_test_result, policy_id: policy.id,
+                                        account: account, score: nil, supported: false)
+        allow(test_result).to receive(:report).and_return(policy)
+        expect(test_result.compliant).to eq(false)
+      end
+    end
+  end
 end
