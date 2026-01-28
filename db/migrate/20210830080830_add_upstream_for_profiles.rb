ea53650bb7be5f1251618a33555537f256051c22
Janos Laszlo Vasik <janosl.vasik@gmail.com>
2026-01-28 16:55:07 +0100

    fix(DB): remove model references from old migrations

--- db/migrate/20210830080830_add_upstream_for_profiles.rb
+++ db/migrate/20210830080830_add_upstream_for_profiles.rb
@@ -2,8 +2,11 @@ class AddUpstreamForProfiles < ActiveRecord::Migration[5.2]
   def up
     add_column :profiles, :upstream, :boolean
     add_index :profiles, :upstream
+
+    # Removed so we're able to change the table name of Profiles
+
     # Initially all canonical profiles are marked as upstream
-    Profile.canonical.update_all(upstream: true)
+    # Profile.canonical.update_all(upstream: true)
   end
 
   def down
