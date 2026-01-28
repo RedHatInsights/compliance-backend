519c2a741d78f10bb4e75679d2c3fa72c5655aaa
Janos Laszlo Vasik <janosl.vasik@gmail.com>
2026-01-28 16:55:07 +0100

    fix(DB): remove model references from old migrations

--- db/migrate/20210609081621_add_score_to_profiles.rb
+++ db/migrate/20210609081621_add_score_to_profiles.rb
@@ -2,26 +2,27 @@ class AddScoreToProfiles < ActiveRecord::Migration[5.2]
   def up
     add_column :profiles, :score, :decimal
 
-    Profile.transaction do
-      scores = TestResult.latest.joins(:profile).group(:profile_id).average(:score)
-      # Profile.canonical(false).where(score: nil).joins(:test_results).distinct.find_each(&:calculate_score!)
+    # Removed so we're able to change the table name of Profiles
+    # Profile.transaction do
+    #   scores = TestResult.latest.joins(:profile).group(:profile_id).average(:score)
+    #   # Profile.canonical(false).where(score: nil).joins(:test_results).distinct.find_each(&:calculate_score!)
 
-      # TODO: replace this with upsert after upgrading to Rails 6
-      if scores.any?
-        values = scores.map do |k, v|
-          "(#{ActiveRecord::Base.connection.quote(k)}, #{ActiveRecord::Base.connection.quote(v)})"
-        end.join(', ')
+    #   # TODO: replace this with upsert after upgrading to Rails 6
+    #   if scores.any?
+    #     values = scores.map do |k, v|
+    #       "(#{ActiveRecord::Base.connection.quote(k)}, #{ActiveRecord::Base.connection.quote(v)})"
+    #     end.join(', ')
 
-        query = %{
-          UPDATE profiles P
-          SET score = uv.new_score
-          FROM (VALUES #{values}) AS uv (id, new_score)
-          WHERE P.id = uv.id::uuid
-        }.gsub(/\s+/, " ").strip
+    #     query = %{
+    #       UPDATE profiles P
+    #       SET score = uv.new_score
+    #       FROM (VALUES #{values}) AS uv (id, new_score)
+    #       WHERE P.id = uv.id::uuid
+    #     }.gsub(/\s+/, " ").strip
 
-        ActiveRecord::Base.connection.execute(query)
-      end
-    end
+    #     ActiveRecord::Base.connection.execute(query)
+    #   end
+    # end
   end
 
   def down
