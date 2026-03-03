134254a3e64bf963aa746e617f2321cf3dc02c09
Roman Blanco <rblanco@redhat.com>
2026-03-03 15:29:46 +0000

    fix(RBAC): remove duplicated checks for Test Results and Reports

--- app/controllers/v2/test_results_controller.rb
+++ app/controllers/v2/test_results_controller.rb
@@ -19,7 +19,6 @@ module V2
       render json: test_results.os_versions, status: :ok
     end
     permission_for_action :os_versions, Rbac::SYSTEM_READ
-    kessel_permission_for_action :os_versions, KesselRbac::SYSTEM_VIEW
     permitted_params_for_action :os_versions, { filter: ParamType.string }
 
     def security_guide_versions
