c1b9c4aa8ce699b9597eb59c3e27ad41ce933969
Janos Laszlo Vasik <janosl.vasik@gmail.com>
2026-03-16 12:00:05 +0100

    feat(Config): guard RBAC client init with feature flag

--- config/initializers/rbac.rb
+++ config/initializers/rbac.rb
@@ -1,9 +1,13 @@
-RBACApiClient.configure do |config|
-  config.host = Settings.endpoints.rbac.host
-  config.scheme = Settings.endpoints.rbac.scheme
-  config.ssl_ca_cert = ENV['SSL_CERT_FILE'].presence
-  if Settings.platform_basic_auth_username.present?
-    config.username = Settings.platform_basic_auth_username
-    config.password = Settings.platform_basic_auth_password
+# frozen_string_literal: true
+
+unless ActiveModel::Type::Boolean.new.cast(Settings.disable_rbac)
+  RBACApiClient.configure do |config|
+    config.host = Settings.endpoints.rbac.host
+    config.scheme = Settings.endpoints.rbac.scheme
+    config.ssl_ca_cert = ENV['SSL_CERT_FILE'].presence
+    if Settings.platform_basic_auth_username.present?
+      config.username = Settings.platform_basic_auth_username
+      config.password = Settings.platform_basic_auth_password
+    end
   end
 end
