46d6e205fe8d08f6a465d6709e94af0e8f50716d
Janos Laszlo Vasik <janosl.vasik@gmail.com>
2026-03-16 10:27:07 +0100

    fix(Config): provide defaults for env vars in Foreman

--- config/initializers/yabeda.rb
+++ config/initializers/yabeda.rb
@@ -13,6 +13,8 @@ end
 if %w[sidekiq karafka].any? { |p| $0.include?(p) }
   if ClowderCommonRuby::Config.clowder_enabled?
     ENV['PROMETHEUS_EXPORTER_PORT'] = ClowderCommonRuby::Config.load.metricsPort.to_s
+  else
+    ENV['PROMETHEUS_EXPORTER_PORT'] ||= '9090'
   end
 
   Yabeda::ActiveJob.install!
