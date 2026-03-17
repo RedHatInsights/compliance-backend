25c0645373271740e1bd0a4422877ea42d1b988c
Roman Blanco <rblanco@redhat.com>
2026-03-17 19:19:43 +0000

    perf(API): improve performance for os_versions endpoints

--- app/controllers/v2/systems_controller.rb
+++ app/controllers/v2/systems_controller.rb
@@ -54,7 +54,7 @@ module V2
     permitted_params_for_action :destroy, { id: ID_TYPE }
 
     def os_versions
-      render json: systems.os_versions, status: :ok
+      render json: authorize(filtered_base_scope).os_versions, status: :ok
     end
     permission_for_action :os_versions, Rbac::SYSTEM_READ
     permitted_params_for_action :os_versions, { filter: ParamType.string }
--- app/controllers/v2/test_results_controller.rb
+++ app/controllers/v2/test_results_controller.rb
@@ -16,7 +16,7 @@ module V2
     kessel_permission_for_action :show, KesselRbac::REPORT_VIEW
 
     def os_versions
-      render json: test_results.os_versions, status: :ok
+      render json: authorize(filtered_base_scope).os_versions, status: :ok
     end
     permission_for_action :os_versions, Rbac::SYSTEM_READ
     permitted_params_for_action :os_versions, { filter: ParamType.string }
