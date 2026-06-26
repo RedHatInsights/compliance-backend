9f30441da4c5fe4d16f27311a89077b4d484ff9a
Petr Blaho <petrblaho@redhat.com>
2026-06-26 15:55:25 +0200

    chore(scripts): remove manual timestamps since oc logs handles it

--- scripts/check_migration_status_and_ssg_synced.sh
+++ scripts/check_migration_status_and_ssg_synced.sh
@@ -2,41 +2,34 @@
 
 MAX_INIT_TIMEOUT_SECONDS=${MAX_INIT_TIMEOUT_SECONDS:-120}
 
-# Fetch the container name or hostname so we know who is logging
-POD_ID=$(hostname)
-
-log_time() {
-    echo "[$(date +'%Y-%m-%d %H:%M:%S.%3N')][$POD_ID] $1"
-}
-
 for ((i=1;i<=MAX_INIT_TIMEOUT_SECONDS;i++)); do
-    log_time "=== Loop iteration $i ==="
+    echo "=== Loop iteration $i ==="
     
-    log_time "START db:debug"
+    echo "START db:debug"
     bundle exec rake --trace db:debug
     DB_DEBUG_EXIT=$?
-    log_time "DONE db:debug (exit $DB_DEBUG_EXIT)"
+    echo "DONE db:debug (exit $DB_DEBUG_EXIT)"
     
-    log_time "START db:migrate"
+    echo "START db:migrate"
     bundle exec rake --trace db:migrate
     DB_MIGRATE_EXIT=$?
-    log_time "DONE db:migrate (exit $DB_MIGRATE_EXIT)"
+    echo "DONE db:migrate (exit $DB_MIGRATE_EXIT)"
 
     if [ $DB_MIGRATE_EXIT -eq 0 ]; then
-        log_time "START ssg:check_synced"
+        echo "START ssg:check_synced"
         bundle exec rake --trace ssg:check_synced
         SSG_EXIT=$?
-        log_time "DONE ssg:check_synced (exit $SSG_EXIT)"
+        echo "DONE ssg:check_synced (exit $SSG_EXIT)"
         
         if [ $SSG_EXIT -eq 0 ]; then
-            log_time "All init tasks succeeded. Exiting."
+            echo "All init tasks succeeded. Exiting."
             exit 0
         fi
     fi
 
-    log_time "Tasks not fully synced/ready. Sleeping 1 second..."
+    echo "Tasks not fully synced/ready. Sleeping 1 second..."
     sleep 1
 done
 
-log_time "MAX_INIT_TIMEOUT_SECONDS reached. Exiting 1."
+echo "MAX_INIT_TIMEOUT_SECONDS reached. Exiting 1."
 exit 1
