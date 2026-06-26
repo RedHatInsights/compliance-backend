6d520c7ed5409f21532678403bf4400f7adb660f
Petr Blaho <petrblaho@redhat.com>
2026-06-26 15:05:21 +0200

    chore(scripts): wrap rake tasks with precise timestamps and pod id for timeline merging

--- scripts/check_migration_status_and_ssg_synced.sh
+++ scripts/check_migration_status_and_ssg_synced.sh
@@ -2,28 +2,41 @@
 
 MAX_INIT_TIMEOUT_SECONDS=${MAX_INIT_TIMEOUT_SECONDS:-120}
 
+# Fetch the container name or hostname so we know who is logging
+POD_ID=$(hostname)
+
+log_time() {
+    echo "[$(date +'%Y-%m-%d %H:%M:%S.%3N')][$POD_ID] $1"
+}
+
 for ((i=1;i<=MAX_INIT_TIMEOUT_SECONDS;i++)); do
-    # Check if DB is pre-populated by verifying schema_migrations table
-    DB_STATE=$(bundle exec rails runner "
-      begin
-        if ActiveRecord::Base.connection.table_exists?('schema_migrations')
-          puts '[DB_STATE_CHECK] PRE_POPULATED_DB'
-        else
-          puts '[DB_STATE_CHECK] FRESH_DB'
-        end
-      rescue StandardError => e
-        puts '[DB_STATE_CHECK] PENDING_CONNECTION'
-      end
-    " 2>/dev/null)
+    log_time "=== Loop iteration $i ==="
+    
+    log_time "START db:debug"
+    bundle exec rake --trace db:debug
+    DB_DEBUG_EXIT=$?
+    log_time "DONE db:debug (exit $DB_DEBUG_EXIT)"
+    
+    log_time "START db:migrate"
+    bundle exec rake --trace db:migrate
+    DB_MIGRATE_EXIT=$?
+    log_time "DONE db:migrate (exit $DB_MIGRATE_EXIT)"
 
-    echo "$DB_STATE"
-
-    if [[ "$DB_STATE" != "[DB_STATE_CHECK] PENDING_CONNECTION" ]]; then
-        if bundle exec rake --trace db:debug db:migrate ssg:check_synced; then
+    if [ $DB_MIGRATE_EXIT -eq 0 ]; then
+        log_time "START ssg:check_synced"
+        bundle exec rake --trace ssg:check_synced
+        SSG_EXIT=$?
+        log_time "DONE ssg:check_synced (exit $SSG_EXIT)"
+        
+        if [ $SSG_EXIT -eq 0 ]; then
+            log_time "All init tasks succeeded. Exiting."
             exit 0
         fi
     fi
+
+    log_time "Tasks not fully synced/ready. Sleeping 1 second..."
     sleep 1
 done
 
+log_time "MAX_INIT_TIMEOUT_SECONDS reached. Exiting 1."
 exit 1
