d659ec3fc701ac558953a7811c5006170090a0a4
Petr Blaho <petrblaho@redhat.com>
2026-06-26 16:09:57 +0200

    chore(scripts): change init containers to wait for db migration rather than executing it

--- scripts/check_migration_status_and_ssg_synced.sh
+++ scripts/check_migration_status_and_ssg_synced.sh
@@ -3,7 +3,7 @@
 MAX_INIT_TIMEOUT_SECONDS=${MAX_INIT_TIMEOUT_SECONDS:-120}
 
 for ((i=1;i<=MAX_INIT_TIMEOUT_SECONDS;i++)); do
-    if bundle exec rake --trace db:debug db:migrate ssg:check_synced; then
+    if bundle exec rake --trace db:debug db:abort_if_pending_migrations ssg:check_synced; then
         exit 0
     else
         sleep 1
