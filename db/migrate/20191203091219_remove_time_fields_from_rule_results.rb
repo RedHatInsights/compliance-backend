56ad471721be4fc9756c985ea54442bc6231889b
Janos Laszlo Vasik <janosl.vasik@gmail.com>
2026-07-13 14:23:16 +0200

    fix(migration): comment out model references in old migrations

--- db/migrate/20191203091219_remove_time_fields_from_rule_results.rb
+++ db/migrate/20191203091219_remove_time_fields_from_rule_results.rb
@@ -7,11 +7,11 @@ class RemoveTimeFieldsFromRuleResults < ActiveRecord::Migration[5.2]
   def down
     add_column :rule_results, :start_time, :datetime
     add_column :rule_results, :end_time, :datetime
-    TestResult.find_each do |test_result|
-      rule_result_ids = test_result.rule_results.pluck(:id)
-      RuleResult.where(id: rule_result_ids).update_all(
-        start_time: test_result.start_time, end_time: test_result.end_time
-      )
-    end
+    # TestResult.find_each do |test_result|
+    #   rule_result_ids = test_result.rule_results.pluck(:id)
+    #   RuleResult.where(id: rule_result_ids).update_all(
+    #     start_time: test_result.start_time, end_time: test_result.end_time
+    #   )
+    # end
   end
 end
