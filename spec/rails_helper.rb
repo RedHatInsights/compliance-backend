cfb155451f69343f1bbb546c5d19c10ffc7e314e
Janos Laszlo Vasik <janosl.vasik@gmail.com>
2026-07-14 15:45:57 +0200

    test(metrics): assert system write failure counters increment correctly

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ if Rails.env.production?
 end
 require 'spec_helper'
 require 'rspec/rails'
+require 'yabeda/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
