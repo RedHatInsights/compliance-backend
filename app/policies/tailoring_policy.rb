84591ba92184ec28b6b0169567c30ff65e26a9d4
Janos Laszlo Vasik <janosl.vasik@gmail.com>
2026-07-21 16:30:17 +0200

    fix(auth): rewrite TailoringPolicy scope to use subquery over aliased join

--- app/policies/tailoring_policy.rb
+++ app/policies/tailoring_policy.rb
@@ -25,11 +25,9 @@ class TailoringPolicy < ApplicationPolicy
   # Only show tailoring in our user account
   class Scope < ApplicationPolicy::Scope
     def resolve
-      resolve scope.none if user&.account_id.blank?
+      return scope.none if user&.account_id.blank?
 
-      scope.where
-           .associated(:policy)
-           .where(policy: { account_id: user.account_id })
+      scope.where(policy_id: Policy.where(account_id: user.account_id).select(:id))
     end
   end
 end
