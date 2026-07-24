dfb1d679ff7bdf4f127d10a8ef6aa19f87b0634b
Petr Blaho <petrblaho@redhat.com>
2026-07-24 19:35:17 +0200

    ci(build): restore format oci flag

--- build_deploy.sh
+++ build_deploy.sh
@@ -72,6 +72,7 @@ if [[ "$IS_MASTER_BRANCH" == "true" ]]; then
 
     # On master: build fresh layers without using older cache, and populate remote cache in Quay
     cicd::image_builder::build_and_push --layers --no-cache \
+        --format oci \
         --timestamp 0 \
         --cache-to "$CACHE_REPO" \
         --log-level=debug
@@ -80,6 +81,7 @@ else
 
     # On PRs: build using remote layer cache from Quay
     cicd::image_builder::build_and_push --layers \
+        --format oci \
         --timestamp 0 \
         --cache-from "$CACHE_REPO" \
         --cache-to "$CACHE_REPO" \
